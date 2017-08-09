594c69900a903eaf40896db347a3fce50b204c4a
Dave Cozens <davecozens.defra@gmail.com>
2017-08-09 16:00:36 +0100

    further adventures in postgres

--- src/licences.js
+++ src/licences.js
@@ -2,8 +2,23 @@ const fs = require('fs')
 const baseFilePath = __dirname + '/../public/data/licences/'
 const Helpers = require('./helpers')
 
-function getLicences () {
+function getLicences (cb) {
   var alllicences = []
+
+  const { Client } = require('pg')
+  const client = new Client()
+
+  client.connect()
+
+  client.query('SELECT * from licence', [], (err, res) => {
+    console.log(err ? err.stack : res.rows[0]) // Hello World!
+    client.end()
+    console.log('return data')
+    console.log(res.rows)
+    cb(res.rows)
+  })
+
+/**
   fs.readdirSync(baseFilePath).forEach(function (file) {
     if (file.indexOf('.json') > -1) {
       var licenceInfo = getLicenceSummary(file)
@@ -14,6 +29,7 @@ function getLicences () {
     }
   })
   return alllicences
+  **/
 }
 
 function getLicence (ref) {
@@ -24,6 +40,9 @@ function getLicence (ref) {
 }
 
 function updateLicence (id, data) {
+  //TODO: Post licence data in friendly format and transform to queries
+  // TLDR: use licence id to get licence org & type, then build empty structure then iterate friendly data into it...
+
   const thisFile = baseFilePath + id + '.json'
   if (fs.existsSync(thisFile)) {
     fs.writeFileSync(thisFile, JSON.stringify(data))
@@ -34,6 +53,7 @@ function updateLicence (id, data) {
 }
 
 function createLicence (data) {
+  //TODO: Post new licence handler
   const id = Helpers.createGUID()
   const thisFile = baseFilePath + id + '.json'
   fs.writeFileSync(thisFile, JSON.stringify(data))
@@ -41,12 +61,14 @@ function createLicence (data) {
 }
 
 function deleteLicence (id) {
+  //TODO: delete status
   const thisFile = baseFilePath + id
   fs.unlinkSync(thisFile)
   return 'OK'
 }
 
 function getLicenceSummary (filename) {
+  //todo: licence summary functions...
   var fileInfo = {}
   const licenceObject = getLicence(filename)
 
@@ -61,12 +83,68 @@ function getLicenceSummary (filename) {
   }
 }
 
+function getLicenceByOrgTypeID (data, cb) {
+  const { Client } = require('pg')
+  const client = new Client()
+  console.log(data)
+  client.connect()
+  var queryParams = [data.orgId, data.typeId, data.licenceId]
+  //swanky query to get the licence data
+  var query = `select * from (
+	SELECT licence_id,array_to_json(array_agg(attributes)) as attributeData
+	from
+		(
+		select
+		licence_id,field_nm, licence_data_value,ld.type_fields_id,tf.field_id
+		from licence_data ld
+		inner join type_fields tf on ld.type_fields_id = tf.type_fields_id
+		inner join field f on tf.field_id = f.field_id
+        ) attributes
+	group by licence_id
+) a,
+licence l
+where a.licence_id=l.licence_id and l.licence_id=$3 and l.licence_org_id=$1 and l.licence_type_id=$2`
+  console.log('perform query')
+
+
+  client.query(query, queryParams, (err, res) => {
+    if (err) {
+      console.log(err ? err.stack : 'No query errors')
+    }
+
+    client.end()
+
+/**
+convert licence data to nice friendly format, separating core values (common to all licences regardless of type) and licence/org type specific attributes
+**/
+    var licenceData = {}
+    licenceData.licence_id = res.rows[0].licence_id
+    licenceData.licence_ref = res.rows[0].licence_ref[0]
+    licenceData.licence_start_dt = res.rows[0].licence_start_dt
+    licenceData.licence_end_dt = res.rows[0].licence_end_dt
+    licenceData.licence_status_id = res.rows[0].licence_status_id
+    licenceData.licence_type_id = res.rows[0].licence_type_id
+    licenceData.licence_org_id = res.rows[0].licence_org_id
+    licenceData.attributes = []
+    for (attribute in res.rows[0].attributedata) {
+      var newAttribute = {}
+      newAttribute.name = res.rows[0].attributedata[attribute].field_nm
+      newAttribute.value = res.rows[0].attributedata[attribute].licence_data_value
+      licenceData.attributes.push(newAttribute)
+    }
+
+    console.log(licenceData)
+    cb(licenceData)
+  })
+}
+
 module.exports = {
   list: getLicences,
   get: getLicence,
   update: updateLicence,
   create: createLicence,
   summary: getLicenceSummary,
-  delete: deleteLicence
+  delete: deleteLicence,
+  getLicenceByOrgTypeID: getLicenceByOrgTypeID
 
 }
--- src/routes/API.js
+++ src/routes/API.js
@@ -2,45 +2,112 @@
 API page, pending real back end - uses fs to read and write to lkocal json files...
 
 */
-const helpers = require('../helpers')
-const licences = require('../licences')
+const Licences = require('../licences')
+const Orgs = require('../orgs')
+
 
 const version = '1.0'
 
 function licencesPostHandler(request, reply) {
 // create a new licence
-  var newLicence = licences.create(request.payload)
+  var newLicence = Licences.create(request.payload)
   reply(newLicence)
 }
 
-function licencesGetHandler(request, reply) {
+function licencesListHandler(request, reply) {
 // return all licences
   var data = {}
-  var allLicences = licences.list()
-  reply(allLicences)
+  var allLicences = Licences.list((data)=>{
+    console.log('got data!')
+    console.log(data)
+    reply(data)
+
+  })
 }
 
 function licenceGetHandler(request, reply) {
 // return all licences
   var data = {}
-  var licence = licences.get(request.params.id + '.json')
+  var licence = Licences.get(request.params.id + '.json')
   reply(licence)
 }
 
 function licencePutHandler(request, reply) {
 // update specific licence
   var data = {}
-  var update = licences.update(request.params.id, request.payload)
+  var update = Licences.update(request.params.id, request.payload)
   reply(update)
 }
 
+function orgsListHandler(request, reply) {
+// return all licences
+  var data = {}
+  var licence = Orgs.list((data)=>{
+    console.log('got data!')
+    console.log(data)
+    reply(data)
+
+  })
+}
+
+function getLicencesByOrgandType(request,reply){
+  // return all licences for supplied type and org
+    var data = {}
+    var licence = Orgs.listLicencesByOrgAndType(request.params,(data)=>{
+      console.log('got data!')
+      console.log(data)
+      reply(data)
+
+    })
+}
+
+function getLicenceByOrgTypeID(request,reply){
+  // return all licences for supplied type and org
+    var data = {}
+    var licence = Licences.getLicenceByOrgTypeID(request.params,(data)=>{
+      console.log('got data!')
+      reply(data)
+
+    })
+}
+
+function getLicenceDefinitionByOrgType(request,reply){
+  // return all licences for supplied type and org
+    var data = {}
+    var licence = Orgs.getLicenceDefinitionByOrgType(request.params,(data)=>{
+      console.log('got data!')
+      reply(data)
+
+    })
+}
+function test(request,reply){
+
+  const { Client } = require('pg')
+  const client = new Client()
+
+  client.connect()
+
+  client.query('SELECT * from licences', [], (err, res) => {
+    if(err){
+    console.log(err ? err.stack : 'No query errors') // Hello World!
+    }
+
+    reply(JSON.stringify(res.rows[0]));
+    client.end()
+  })
+
+}
+
 module.exports = [
+  { method: 'GET', path: '/API/' + version + '/orgs', handler: orgsListHandler },
+  { method: 'GET', path: '/API/' + version + '/orgs/{orgId}/types/{typeId}/licences', handler: getLicencesByOrgandType },
+  { method: 'GET', path: '/API/' + version + '/orgs/{orgId}/types/{typeId}/licences/{licenceId}', handler: getLicenceByOrgTypeID },
+  { method: 'GET', path: '/API/' + version + '/orgs/{orgId}/types/{typeId}/definition', handler: getLicenceDefinitionByOrgType },
 
+  { method: 'GET', path: '/API/' + version + '/test', handler: test },
   { method: 'POST', path: '/API/' + version + '/licences', handler: licencesPostHandler },
-
-  { method: 'GET', path: '/API/' + version + '/licences', handler: licencesGetHandler },
-
+  { method: 'GET', path: '/API/' + version + '/licences', handler: licencesListHandler },
   { method: 'GET', path: '/API/' + version + '/licences/{id}', handler: licenceGetHandler },
-
   { method: 'PUT', path: '/API/' + version + '/licences/{id}', handler: licencePutHandler  }
+
 ]

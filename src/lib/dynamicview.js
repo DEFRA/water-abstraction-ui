3ee19003bfadebb7b986c3976a703cdaae46adc9
Dave Cozens <davecozens.defra@gmail.com>
2017-08-23 15:40:05 +0100

    heroku file path investigations....

--- src/lib/dynamicview.js
+++ src/lib/dynamicview.js
@@ -1,5 +1,18 @@
 
+console.log(__dirname)
 const pathToTemplates=__dirname+'/../views/partials/jsPartials/'
+
+/**
+dynamically require all .js files in views/partials/jsPartials/, for use in
+
+  {{#dynamicView viewType="example" viewData=this}}{{/dynamicView}}
+
+  Usage:
+
+    {{#dynamicView viewType="[name of .js file]" viewData=[data to send to function]}}{{/dynamicView}}
+
+handlebars templates
+**/
 
 require('fs').readdirSync(pathToTemplates).forEach(function(file) {
   if (file.match(/\.js$/) !== null && file !== 'index.js') {
@@ -8,33 +21,3 @@ require('fs').readdirSync(pathToTemplates).forEach(function(file) {
     exports[name] = require(pathToTemplates+file);
   }
 });
-
-/**
-
-function wr () {
-  var wrType = arguments[0].type
-  var wrSubtype = arguments[0].subtype
-  var wrAttributes = arguments[0].attributes
-
-  // TODO: renderers for individual WR components
-  switch (wrType) {
-    case "21":
-      switch (wrSubtype) {
-        case "3":
-          var response = `WR ${wrType}.${wrSubtype}: At National Grid Reference ${wrAttributes.ngr} marked ‘${wrAttributes.point_ref}’ on the map. `
-          break
-        default:
-          var response = `Generic WR ${wrType} with data ${JSON.stringify(wrAttributes)}`
-      }
-      break
-    default:
-      var response = `Generic WR  component for WR ${wrType}.${wrSubtype} with data ${JSON.stringify(wrAttributes)}`
-  }
-
-  return response
-}
-
-module.exports = {
-  WR: wr
-}
-**/

3af897e7b505b5d50e44468365642dd1c28271bc
David Thomas <david.thomas@environment-agency.gov.uk>
2017-09-04 10:47:49 +0100

    ETL - Add licence type and organisation ID to command line

--- etl/export.js
+++ etl/export.js
@@ -9,20 +9,20 @@ function makeURIRequest (uri, data, cb) {
   }, cb)
 }
 
-function exportLicence(licence) {
+function exportLicence(licence, orgId, licenceTypeId) {
   var requestBody = {
       licence_ref: licence.id,
       licence_start_dt: "2017-01-01T00:00:00.000Z",
       licence_end_dt: "2018-01-01T00:00:00.000Z",
       licence_status_id: "1",
-      licence_type_id: "8",
-      licence_org_id: "1",
+      licence_type_id: licenceTypeId,
+      licence_org_id: orgId,
       attributes: {
         "licenceData": licence
     }
   }
 
-  makeURIRequest ('org/1/licencetype/8/licence', requestBody, function (error) {
+  makeURIRequest ('org/' + orgId + '/licencetype/' + licenceTypeId + '/licence', requestBody, function (error) {
     if (error) {
       console.log(error);
     }
--- etl/index.js
+++ etl/index.js
@@ -5,7 +5,6 @@ var anonymise = require('./anonymise')
 var normalise = require('./normalise')
 var exportLicence = require('./export')
 
-
 function loadWorkbook(workbookName) {
   return workbook = xlsx.readFile(workbookName)
 }
@@ -17,16 +16,27 @@ function loadData (workbook) {
 
 function writeWorkbook(workbook, data) {
   var firstSheetName = workbook.SheetNames[0]
-  workbook.Sheets[firstSheetName] = xlsx.utils.json_to_sheet(data);
+  workbook.Sheets[firstSheetName] = xlsx.utils.json_to_sheet(data)
   xlsx.writeFile(workbook, 'out.xlsx')
 }
 
-var workbook = loadWorkbook(process.argv[2])
+if (process.argv.length != 5) {
+  console.log("Correct format for running ETL tool is:")
+  console.log("")
+  console.log("    node index.js <input.xls> <Org ID> <Licence Type ID>")
+  process.exit(1)
+}
+
+var workbookName = process.argv[2]
+var orgId = process.argv[3]
+var licenceTypeId = process.argv[4]
+
+var workbook = loadWorkbook(workbookName)
 var data = loadData(workbook)
 data = anonymise.anonymiseData(data)
 //writeWorkbook(workbook, data)
 
 data = normalise.normalise(data)
 for(var i = 0; i < data.length; i++) {
-  exportLicence.exportLicence(data[i])
+  exportLicence.exportLicence(data[i], orgId, licenceTypeId)
 }

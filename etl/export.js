a4d546059f6f71254e9a14e74bc7e8eaf082c401
David Thomas <david.thomas@environment-agency.gov.uk>
2017-09-04 10:33:56 +0100

    ETL - Add whole licence as single attribute

--- etl/export.js
+++ etl/export.js
@@ -15,30 +15,14 @@ function exportLicence(licence) {
       licence_start_dt: "2017-01-01T00:00:00.000Z",
       licence_end_dt: "2018-01-01T00:00:00.000Z",
       licence_status_id: "1",
-      licence_type_id: "1",
+      licence_type_id: "8",
       licence_org_id: "1",
       attributes: {
-        "FlowConditions": [],
-        "SourceOfSupplyTest": [],
-        "Records": "Dummy",
-        "MeansOfMeasurement": licence.purposes[0].meansOfMeasurement,
-        "LicenceEffectiveTo": licence.effectiveTo,
-        "LicenceEffectiveFrom": licence.effectiveFrom,
-        "PeriodOfAbstraction": licence.purposes[0].periodOfAbstraction,
-        "SourceOfSupply": licence.sourceOfSupply,
-        "PointOfAbstraction": licence.purposes[0].points[0].name,
-        "LicenceHolder": licence.name,
-        "MaximumQuantities": licence.maxQuantity,
-        "LicenceHolderAddress": licence.address,
-        "LicenceHolderName": licence.name,
-        "LicenceHolderEmail": "dummy@email.com",
-        "LicenceHolderTelephone": "01234 567 890",
-        "MeansOfAbstraction": licence.purposes[0].points[0].meansOfAbstraction,
-        "PurposeOfAbstraction": licence.purposes[0].description
+        "licenceData": licence
     }
   }
 
-  makeURIRequest ('org/1/licencetype/1/licence', requestBody, function (error) {
+  makeURIRequest ('org/1/licencetype/8/licence', requestBody, function (error) {
     if (error) {
       console.log(error);
     }

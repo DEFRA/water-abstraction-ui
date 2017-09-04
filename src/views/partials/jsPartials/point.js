729e723ed4a06dc759dca78250b7a208cf7b3acf
Dave Cozens <davecozens.defra@gmail.com>
2017-09-04 11:06:49 +0100

    quick prototyping

--- src/views/partials/jsPartials/point.js
+++ src/views/partials/jsPartials/point.js
@@ -3,7 +3,12 @@ function getContent () {
 console.log('get content----')
   console.log(arguments[0])
 
-  response=JSON.stringify(arguments[0].hash.viewData);
+  response=`
+  ${arguments[0].hash.viewData.id}: ${arguments[0].hash.viewData.name} (${arguments[0].hash.viewData.ngr1})
+  ${arguments[0].hash.viewData.meansOfAbstraction}
+  `
+
+
   return response
 
 }

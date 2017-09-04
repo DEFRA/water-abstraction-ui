4eed171e491e2208ed93f38a5b1cb0388dd17946
Dave Cozens <davecozens.defra@gmail.com>
2017-09-04 11:58:32 +0100

    quick prototyping

--- src/views/partials/jsPartials/point.js
+++ src/views/partials/jsPartials/point.js
@@ -1,12 +1,11 @@
 function getContent () {
 
 
-  var response = `<div class="sourceofsupplyq">${arguments[0].hash.viewData.name}</div>`
+  var response = `<div class="sourceofsupplyq">${arguments[0].hash.viewData.name}<br>(NGR: ${arguments[0].hash.viewData.ngr1})</div>`
   response += `<div class="licenceAnswer">`
 
 
-  response+=`${arguments[0].hash.viewData.ngr1}<br>
-${arguments[0].hash.viewData.meansOfAbstraction}
+  response+=`${arguments[0].hash.viewData.meansOfAbstraction}
 </div><p></p>  `
 
 

049929c92d268378becc378f5daca7f6b3031479
Dave Cozens <davecozens.defra@gmail.com>
2017-11-23 10:57:36 +0000

    fetch nald data from s3

--- etl/fetchNaldData.js
+++ etl/fetchNaldData.js
@@ -10,11 +10,15 @@ var config = new AWS.Config({
   secretAccessKey: process.env.S3_SECRET
 });
 
-AWS.config.update({
-  httpOptions: {
-    agent: proxy('http://devpxlb01.aws-int.defra.cloud:3128') 
-  }
-});
+if(process.env.proxy){
+  AWS.config.update({
+    httpOptions: {
+      agent: proxy('http://devpxlb01.aws-int.defra.cloud:3128')
+    }
+  });  
+}
+
+
 
 console.log(config)
 var s3 = new AWS.S3(config);
@@ -36,6 +40,30 @@ s3.getObject(params, function(err, data) {
         {
             var buff = new Buffer(data.Body, "binary"); //i've tried everything.
             var fd = fs.openSync("nalds-data.xls", "w");
+            fs.writeSync(fd, buff, 0, buff.length,0);
+        }
+
+  }
+});
+
+var params = {
+  Bucket: process.env.S3_PATH,
+  Key: 'nald/'+process.env.environment+'-dbdump.sql'
+};
+
+s3.getObject(params, function(err, data) {
+  if (err) {
+    console.log(err)
+    //we don't actually care if there's an error as we always assume online if we don't know...
+    //console.log(`s3 file not found at ${process.env.environment}-status`)
+
+    return
+  } else {
+    //read file contents from s3, and write to local file
+    if(err == null)
+        {
+            var buff = new Buffer(data.Body, "binary"); //i've tried everything.
+            var fd = fs.openSync("dbdump.sql", "w");
             fs.writeSync(fd, buff, 0, buff.length,0);
         }
 

652c1c763baf9a5686e06010487b68ca27bcb329
James Carmichael <james@Jamess-MacBook-Pro.local>
2017-12-04 17:26:40 +0000

    Add cookie secret to environment vars

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ server.register([{
 ], (err) => {
 
   server.auth.strategy('standard', 'cookie', {
-    password: 'somecrazycookiesecretthatcantbeguesseswouldgohere', // cookie secret
+    password: process.env.cookie_secret, // cookie secret
     isSecure: process.env.NODE_ENV == 'production', // use secure cookie in production
     isSameSite: 'Lax',
     ttl: 24 * 60 * 60 * 1000, // Set session to 1 day,

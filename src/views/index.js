5ad48058644a830b9bdac81bbf479b3690b4fec0
James Carmichael <james@siteclick.co.uk>
2018-01-30 11:59:44 +0000

    Hide abstraction points on conditions view if >1

--- src/views/index.js
+++ src/views/index.js
@@ -12,6 +12,14 @@ handlebars.registerHelper('equal', require('handlebars-helper-equal'))
 
 
 
+handlebars.registerHelper('greaterThan', function(v1, v2, options) {
+  if(v1 > v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
+
 
 
 /**

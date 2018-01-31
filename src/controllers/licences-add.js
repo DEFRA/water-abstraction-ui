8cacbc021fae0bc9fbf3d0154b8fafb8cf837e7f
Dave Cozens <davecozens.defra@gmail.com>
2018-01-31 13:17:54 +0000

    Resolve WATER-706: Registration: empty checkboxes give "something went wrong" error

--- src/controllers/licences-add.js
+++ src/controllers/licences-add.js
@@ -191,9 +191,11 @@ async function postLicenceSelect(request, reply) {
 
     console.log(err);
     if(err.name === 'NoLicencesSelectedError') {
-      return reply.redirect('/select-licences?error=noLicenceSelected&token=' + token);
+      return reply.redirect('/select-licences?error=noLicenceSelected');
+    } else {
+      errorHandler(request, reply)(err);
     }
-    errorHandler(request, reply)(err);
+
   }
 
 }

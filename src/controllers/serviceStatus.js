46d342abb2fc723ff60bacc2abd857e14f02c12c
Dave Cozens <davecozens.defra@gmail.com>
2018-03-21 14:01:45 +0000

    Fix lint error

--- src/controllers/serviceStatus.js
+++ src/controllers/serviceStatus.js
@@ -3,10 +3,9 @@ const CRM = require('../lib/connectors/crm');
 const permits = require('../lib/connectors/permit');
 const water = require('../lib/connectors/water');
 
-
-async function serviceStatus(request, reply) {
+async function serviceStatus (request, reply) {
   var errors = 0;
-  var html = "<html><head><body><table><tr><th>Service</th><th>Info</th></tr>"
+  var html = '<html><head><body><table><tr><th>Service</th><th>Info</th></tr>';
   var data = {
     idm: {},
     crm: {},
@@ -17,93 +16,84 @@ async function serviceStatus(request, reply) {
   };
   try {
     const userData = await IDM.usersClient.findMany({}, {}, {
-      "perPage": 1
-    })
-    data.idm.users = userData.pagination.totalRows
-    html += `<tr><td>IDM Users</td><td>${data.idm.users}</td></tr>`
-
+      'perPage': 1
+    });
+    data.idm.users = userData.pagination.totalRows;
+    html += `<tr><td>IDM Users</td><td>${data.idm.users}</td></tr>`;
   } catch (e) {
-    html += `<tr><td>IDM Users</td><td>ERROR</td></tr>`
+    html += `<tr><td>IDM Users</td><td>ERROR</td></tr>`;
     errors++;
   }
   try {
     const documentData = await CRM.documents.findMany({}, {}, {
-      "perPage": 1
-    })
-    data.crm.documents = documentData.pagination.totalRows
-    html += `<tr><td>CRM Documents</td><td>${data.crm.documents}</td></tr>`
-
+      'perPage': 1
+    });
+    data.crm.documents = documentData.pagination.totalRows;
+    html += `<tr><td>CRM Documents</td><td>${data.crm.documents}</td></tr>`;
   } catch (e) {
-    html += `<tr><td>CRM Documents</td><td>ERROR</td></tr>`
+    html += `<tr><td>CRM Documents</td><td>ERROR</td></tr>`;
     errors++;
   }
 
   try {
     const verificationData = await CRM.verification.findMany({}, {}, {
-      "perPage": 1
-    })
-    data.crm.verifications = verificationData.pagination.totalRows
-    html += `<tr><td>CRM Verifications</td><td>${data.crm.verifications}</td></tr>`
-
+      'perPage': 1
+    });
+    data.crm.verifications = verificationData.pagination.totalRows;
+    html += `<tr><td>CRM Verifications</td><td>${data.crm.verifications}</td></tr>`;
   } catch (e) {
-    html += `<tr><td>CRM Verifications</td><td>ERROR</td></tr>`
+    html += `<tr><td>CRM Verifications</td><td>ERROR</td></tr>`;
     errors++;
   }
 
   try {
     const permitData = await permits.licences.findMany({}, {}, {
-      "perPage": 1
-    })
-    data.permitrepo.permits = permitData.pagination.totalRows
-    html += `<tr><td>Permit Repo Permits</td><td>${data.permitrepo.permits}</td></tr>`
-
+      'perPage': 1
+    });
+    data.permitrepo.permits = permitData.pagination.totalRows;
+    html += `<tr><td>Permit Repo Permits</td><td>${data.permitrepo.permits}</td></tr>`;
   } catch (e) {
-    html += `<tr><td>Permit Repo Permits</td><td>ERROR</td></tr>`
+    html += `<tr><td>Permit Repo Permits</td><td>ERROR</td></tr>`;
     errors++;
   }
 
-  data.waterservice.import = {}
+  data.waterservice.import = {};
 
   try {
     const importDataComplate = await water.pendingImport.findMany({
-      "status": 1
+      'status': 1
     }, {}, {
-      "perPage": 1
-    })
-    data.waterservice.import.complete = importDataComplate.pagination.totalRows
-    html += `<tr><td>Imported Permits</td><td>${data.waterservice.import.complete}</td></tr>`
-
+      'perPage': 1
+    });
+    data.waterservice.import.complete = importDataComplate.pagination.totalRows;
+    html += `<tr><td>Imported Permits</td><td>${data.waterservice.import.complete}</td></tr>`;
   } catch (e) {
-    html += `<tr><td>Imported Permits</td><td>ERROR</td></tr>`
+    html += `<tr><td>Imported Permits</td><td>ERROR</td></tr>`;
     errors++;
   }
   try {
     const importDataPending = await water.pendingImport.findMany({
-      "status": 0
+      'status': 0
     }, {}, {
-      "perPage": 1
-    })
-    data.waterservice.import.pending = importDataPending.pagination.totalRows
-    html += `<tr><td>Pending Permits</td><td>${data.waterservice.import.pending}</td></tr>`
-
-
+      'perPage': 1
+    });
+    data.waterservice.import.pending = importDataPending.pagination.totalRows;
+    html += `<tr><td>Pending Permits</td><td>${data.waterservice.import.pending}</td></tr>`;
   } catch (e) {
-    html += `<tr><td>Pending Permits</td><td>ERROR</td></tr>`
+    html += `<tr><td>Pending Permits</td><td>ERROR</td></tr>`;
     errors++;
   }
 
-  html += `<tr><td><b>Errors<b></td><td> ${errors} ERRORS REPORTED</td></tr>`
+  html += `<tr><td><b>Errors<b></td><td> ${errors} ERRORS REPORTED</td></tr>`;
 
-  html += "</body></html>"
+  html += '</body></html>';
 
-  if (request.query.format && request.query.format == 'json') {
-    return reply(data)
+  if (request.query.format && request.query.format === 'json') {
+    return reply(data);
   } else {
-    return reply(html)
+    return reply(html);
   }
-
 }
-
 
 module.exports = {
   serviceStatus

2f3198d97869e5457d55aea843752dbb46fd0c39
James Carmichael <james@siteclick.co.uk>
2018-03-23 17:28:16 +0000

    Amend licence verification process to only update documents at time code is entered

--- src/lib/connectors/crm/verification.js
+++ src/lib/connectors/crm/verification.js
@@ -6,13 +6,51 @@ const { APIClient } = require('hapi-pg-rest-api');
 const moment = require('moment');
 const rp = require('request-promise-native').defaults({
   proxy: null,
-  strictSSL: false,
+  strictSSL: false
 });
 
 // Create API client
-module.exports = new APIClient(rp, {
-  endpoint: `${ process.env.CRM_URI }/verification`,
+const client = new APIClient(rp, {
+  endpoint: `${process.env.CRM_URI}/verification`,
   headers: {
-    Authorization: process.env.JWT_TOKEN,
-  },
+    Authorization: process.env.JWT_TOKEN
+  }
 });
+
+/**
+ * Create a list of documents for a verification
+ * @param {String} verificationId - GUID for the verification ID
+ * @param {Array} documentIds - array of document Ids
+ * @return {Promise}
+ */
+client.addDocuments = (verificationId, documentIds) => {
+  const uri = `${process.env.CRM_URI}/verification/${verificationId}/documents`;
+  return rp({
+    uri,
+    method: 'POST',
+    headers: {
+      Authorization: process.env.JWT_TOKEN
+    },
+    json: true,
+    body: { document_id: documentIds }
+  });
+};
+
+/**
+ * Get a list of documents for a verification
+ * @param {String} verificationId - GUID for the verification ID
+ * @return {Promise}
+ */
+client.getDocuments = (verificationId, documentIds) => {
+  const uri = `${process.env.CRM_URI}/verification/${verificationId}/documents`;
+  return rp({
+    uri,
+    method: 'GET',
+    headers: {
+      Authorization: process.env.JWT_TOKEN
+    },
+    json: true
+  });
+};
+
+module.exports = client;

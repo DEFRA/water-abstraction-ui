60f2593f507cfa971bf8af6ea37f6a2249772d87
James Carmichael <james@siteclick.co.uk>
2018-05-02 13:59:03 +0100

    Fix expiry date issue and JS bundle file

--- gulpfile.js
+++ gulpfile.js
@@ -71,13 +71,13 @@ gulp.task('combine-minify-js', [], () => {
     './public/javascripts/vendor/polyfills/bind.js',
     './public/javascripts/govuk/shim-links-with-button-role.js',
     './public/javascripts/govuk/show-hide-content.js',
-    './public/javascripts/govuk/details.polyfill.js',
-    './public/javascripts/application.js'
+    './src/public/javascripts/govuk/details.polyfill.js',
+    './src/public/javascripts/application.js'
   ];
 
   return gulp.src(files)
     .pipe(concat('application.all.min.js'))
-    .pipe(uglify({ie8: true}))
+    .pipe(uglify({ ie8: true }))
     .pipe(gulp.dest('./public/javascripts'));
 });
 

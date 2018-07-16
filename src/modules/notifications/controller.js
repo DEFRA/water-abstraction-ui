1ccde0f3f102220397d5f7af3f4571c1485ca3e1
James Carmichael <james@siteclick.co.uk>
2018-07-16 10:53:51 +0100

    Allow user to save contact details as part of notification flow

--- src/modules/notifications/controller.js
+++ src/modules/notifications/controller.js
@@ -55,6 +55,12 @@ async function getStep (request, reply) {
   const taskData = new TaskData(task, state, context);
 
   if (start) {
+    // Does the user have contact details set?
+    if (!context.contactDetails.name) {
+      const url = encodeURIComponent(`/admin/notifications/${id}?start=1`);
+      return reply.redirect(`/admin/notifications/contact?redirect=${url}`);
+    }
+
     request.sessionStore.set('notificationsFlow', taskData.getData());
   }
 
--- src/modules/notifications/routes.js
+++ src/modules/notifications/routes.js
@@ -1,5 +1,7 @@
 const Joi = require('joi');
 const controller = require('./controller');
+const contactController = require('./contact-controller');
+const { VALID_EMAIL } = require('../../lib/validators');
 
 const getStep = {
   method: 'GET',
@@ -170,7 +172,105 @@ const routes = {
       }
     },
     handler: controller.postSend
+  },
+
+  getNameAndJob: {
+    method: 'GET',
+    path: '/admin/notifications/contact',
+    handler: contactController.getNameAndJob,
+    config: {
+      description: 'Display contact details form if not already set in notifications flow',
+      validate: {
+        query: {
+          redirect: Joi.string()
+        }
+      },
+      plugins: {
+        viewContext: {
+          pageTitle: 'Add your contact information',
+          activeNavLink: 'notifications'
+        }
+      }
+    }
+  },
+
+  postNameAndJob: {
+    method: 'POST',
+    path: '/admin/notifications/contact',
+    handler: contactController.postNameAndJob,
+    config: {
+      description: 'Post handler for name and job title',
+      validate: {
+        payload: {
+          'csrf_token': Joi.string().guid().required(),
+          'redirect': Joi.string().allow(''),
+          'contact-name': Joi.string().allow('').max(254),
+          'contact-job-title': Joi.string().allow('').max(254)
+        }
+      },
+      plugins: {
+        viewContext: {
+          pageTitle: 'Add your contact information',
+          activeNavLink: 'notifications'
+        },
+        formValidator: {
+          payload: {
+            'contact-name': Joi.string().required(),
+            'contact-job-title': Joi.string().required(),
+            'csrf_token': Joi.string().guid().required(),
+            'redirect': Joi.string().allow('')
+          }
+        }
+      }
+    }
+  },
+
+  getDetails: {
+    method: 'GET',
+    path: '/admin/notifications/contact-details',
+    handler: contactController.getDetails,
+    config: {
+      description: 'Next page of notification contact details - email, tel, address',
+      plugins: {
+        viewContext: {
+          pageTitle: 'Add your contact information',
+          activeNavLink: 'notifications'
+        }
+      }
+    }
+  },
+
+  postDetails: {
+    method: 'POST',
+    path: '/admin/notifications/contact-details',
+    handler: contactController.postDetails,
+    config: {
+      description: 'Post handler for email/tel/address',
+      validate: {
+        payload: {
+          'csrf_token': Joi.string().guid().required(),
+          'contact-tel': Joi.string().allow('').max(254),
+          'contact-email': Joi.string().allow('').max(254),
+          'contact-address': Joi.string().allow('').max(254)
+        }
+      },
+      plugins: {
+        viewContext: {
+          pageTitle: 'Add your contact information',
+          activeNavLink: 'notifications'
+        },
+        formValidator: {
+          payload: {
+            'csrf_token': Joi.string().guid().required(),
+            'contact-email': VALID_EMAIL,
+            'contact-tel': Joi.string().required(),
+            'contact-address': Joi.string().required()
+          }
+        }
+      }
+    }
   }
+
 };
 
 if (parseInt(process.env.test_mode) === 1) {

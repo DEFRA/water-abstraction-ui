44f508b8a8077efc774791e83875fcce39762d9f
James Carmichael <james@siteclick.co.uk>
2018-07-19 14:53:35 +0100

    Add unit test for points reducer

--- test/modules/abstraction-reform/lib/reducer.js
+++ test/modules/abstraction-reform/lib/reducer.js
@@ -2,7 +2,7 @@ const Lab = require('lab');
 const { expect } = require('code');
 
 const reducer = require('../../../../src/modules/abstraction-reform/lib/reducer');
-const { createEditLicence, createEditPurpose } = require('../../../../src/modules/abstraction-reform/lib/action-creators');
+const { createEditLicence, createEditPurpose, createEditPoint } = require('../../../../src/modules/abstraction-reform/lib/action-creators');
 const licence = require('../dummy-licence.json');
 
 const lab = exports.lab = Lab.script();
@@ -32,6 +32,17 @@ lab.experiment('Test abstraction reform reducer', () => {
 
     expect(nextState.licence.data.current_version.purposes[0].HOURLY_QTY).to.equal(852);
     expect(nextState.licence.data.current_version.purposes[0].NOTES).to.equal('Some new notes here');
+    expect(nextState.status).to.equal('In progress');
+  });
+
+  lab.test('Test editing point', async () => {
+    const action = createEditPoint({
+      LOCAL_NAME: 'Duck pond'
+    }, user, '1000000009');
+
+    const nextState = reducer({ licence }, action);
+
+    expect(nextState.licence.data.current_version.purposes[0].purposePoints[0].point_detail.LOCAL_NAME).to.equal('Duck pond');
     expect(nextState.status).to.equal('In progress');
   });
 });

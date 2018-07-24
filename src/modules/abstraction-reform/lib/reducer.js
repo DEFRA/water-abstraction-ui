0c1a46c1d6088dafb9edc4c90df3065d16304771
James Carmichael <james@siteclick.co.uk>
2018-07-24 14:01:46 +0100

    Refactor reducer

--- src/modules/abstraction-reform/lib/reducer.js
+++ src/modules/abstraction-reform/lib/reducer.js
@@ -3,8 +3,29 @@ const { EDIT_PURPOSE, EDIT_LICENCE, EDIT_POINT, EDIT_CONDITION, SET_STATUS } = r
 const { STATUS_IN_PROGRESS } = require('./statuses');
 const { findIndex } = require('lodash');
 
+/**
+ * Gets a base update query with the user who edited and timestamp
+ * included.
+ * @param {Object} action
+ * @return {Object} query for immutability helper
+ */
+const getBaseQuery = (action) => {
+  const { user, timestamp } = action.payload;
+  return {
+    status: {
+      $set: STATUS_IN_PROGRESS
+    },
+    lastEdit: {
+      $set: {
+        user,
+        timestamp
+      }
+    }
+  };
+};
+
 const editPurpose = (state, action) => {
-  const { purposeId, data, user, timestamp } = action.payload;
+  const { purposeId, data } = action.payload;
 
   const index = findIndex(state.licence.data.current_version.purposes, (row) => {
     return parseInt(row.ID) === parseInt(purposeId);
@@ -15,15 +36,7 @@ const editPurpose = (state, action) => {
   }
 
   const query = {
-    status: {
-      $set: STATUS_IN_PROGRESS
-    },
-    lastEdit: {
-      $set: {
-        user,
-        timestamp
-      }
-    },
+    ...getBaseQuery(action),
     licence: {
       data: {
         current_version: {
@@ -41,17 +54,9 @@ const editPurpose = (state, action) => {
 };
 
 const editLicence = (state, action) => {
-  const { data, user, timestamp } = action.payload;
+  const { data } = action.payload;
   let query = {
-    status: {
-      $set: STATUS_IN_PROGRESS
-    },
-    lastEdit: {
-      $set: {
-        user,
-        timestamp
-      }
-    },
+    ...getBaseQuery(action),
     licence: {
       $merge: data
     }
@@ -60,18 +65,10 @@ const editLicence = (state, action) => {
 };
 
 const editPoint = (state, action) => {
-  const { pointId, data, user, timestamp } = action.payload;
+  const { pointId, data } = action.payload;
 
   const query = {
-    status: {
-      $set: STATUS_IN_PROGRESS
-    },
-    lastEdit: {
-      $set: {
-        user,
-        timestamp
-      }
-    },
+    ...getBaseQuery(action),
     licence: {
       data: {
         current_version: {
@@ -102,18 +99,10 @@ const editPoint = (state, action) => {
 };
 
 const editCondition = (state, action) => {
-  const { conditionId, data, user, timestamp } = action.payload;
+  const { conditionId, data } = action.payload;
 
   const query = {
-    status: {
-      $set: STATUS_IN_PROGRESS
-    },
-    lastEdit: {
-      $set: {
-        user,
-        timestamp
-      }
-    },
+    ...getBaseQuery(action),
     licence: {
       data: {
         current_version: {

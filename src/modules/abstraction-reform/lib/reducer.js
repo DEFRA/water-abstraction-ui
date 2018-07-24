04e2e7d6c90b57862dc9f372ab8960f01f720552
James Carmichael <james@siteclick.co.uk>
2018-07-24 13:56:57 +0100

    Split reducer into separate functions

--- src/modules/abstraction-reform/lib/reducer.js
+++ src/modules/abstraction-reform/lib/reducer.js
@@ -1,174 +1,184 @@
 const update = require('immutability-helper');
 const { EDIT_PURPOSE, EDIT_LICENCE, EDIT_POINT, EDIT_CONDITION, SET_STATUS } = require('./action-types');
-const { STATUS_IN_PROGRESS, STATUS_IN_REVIEW } = require('./statuses');
+const { STATUS_IN_PROGRESS } = require('./statuses');
 const { findIndex } = require('lodash');
+
+const editPurpose = (state, action) => {
+  const { purposeId, data, user, timestamp } = action.payload;
+
+  const index = findIndex(state.licence.data.current_version.purposes, (row) => {
+    return parseInt(row.ID) === parseInt(purposeId);
+  });
+
+  if (index === -1) {
+    throw new Error(`Purpose ${purposeId} not found`);
+  }
+
+  const query = {
+    status: {
+      $set: STATUS_IN_PROGRESS
+    },
+    lastEdit: {
+      $set: {
+        user,
+        timestamp
+      }
+    },
+    licence: {
+      data: {
+        current_version: {
+          purposes: {
+            [index]: {
+              $merge: data
+            }
+          }
+        }
+      }
+    }
+  };
+
+  return update(state, query);
+};
+
+const editLicence = (state, action) => {
+  const { data, user, timestamp } = action.payload;
+  let query = {
+    status: {
+      $set: STATUS_IN_PROGRESS
+    },
+    lastEdit: {
+      $set: {
+        user,
+        timestamp
+      }
+    },
+    licence: {
+      $merge: data
+    }
+  };
+  return update(state, query);
+};
+
+const editPoint = (state, action) => {
+  const { pointId, data, user, timestamp } = action.payload;
+
+  const query = {
+    status: {
+      $set: STATUS_IN_PROGRESS
+    },
+    lastEdit: {
+      $set: {
+        user,
+        timestamp
+      }
+    },
+    licence: {
+      data: {
+        current_version: {
+          purposes: {
+          }
+        }
+      }
+    }
+  };
+
+  state.licence.data.current_version.purposes.forEach((purpose, i) => {
+    purpose.purposePoints.forEach((point, j) => {
+      if (parseInt(point.point_detail.ID) === parseInt(pointId)) {
+        query.licence.data.current_version.purposes[i] = {
+          purposePoints: {
+            [j]: {
+              point_detail: {
+                $merge: data
+              }
+            }
+          }
+        };
+      }
+    });
+  });
+
+  return update(state, query);
+};
+
+const editCondition = (state, action) => {
+  const { conditionId, data, user, timestamp } = action.payload;
+
+  const query = {
+    status: {
+      $set: STATUS_IN_PROGRESS
+    },
+    lastEdit: {
+      $set: {
+        user,
+        timestamp
+      }
+    },
+    licence: {
+      data: {
+        current_version: {
+          purposes: {
+          }
+        }
+      }
+    }
+  };
+
+  state.licence.data.current_version.purposes.forEach((purpose, i) => {
+    purpose.licenceConditions.forEach((condition, j) => {
+      if (parseInt(condition.ID) === parseInt(conditionId)) {
+        query.licence.data.current_version.purposes[i] = {
+          licenceConditions: {
+            [j]: {
+              $merge: data
+            }
+          }
+        };
+      }
+    });
+  });
+
+  return update(state, query);
+};
+
+const setState = (state, action) => {
+  const { status, notes, user, timestamp } = action.payload;
+
+  const query = {
+    status: {
+      $set: status
+    }
+  };
+
+  if (notes) {
+    query.notes = {
+      $push: [{
+        notes,
+        user,
+        timestamp
+      }]
+    };
+  }
+
+  return update(state, query);
+};
 
 const reducer = (state, action) => {
   switch (action.type) {
     case EDIT_PURPOSE:
-    {
-      const { purposeId, data, user, timestamp } = action.payload;
-
-      const index = findIndex(state.licence.data.current_version.purposes, (row) => {
-        return parseInt(row.ID) === parseInt(purposeId);
-      });
-
-      if (index === -1) {
-        throw new Error(`Purpose ${purposeId} not found`);
-      }
-
-      const query = {
-        status: {
-          $set: STATUS_IN_PROGRESS
-        },
-        lastEdit: {
-          $set: {
-            user,
-            timestamp
-          }
-        },
-        licence: {
-          data: {
-            current_version: {
-              purposes: {
-                [index]: {
-                  $merge: data
-                }
-              }
-            }
-          }
-        }
-      };
-
-      return update(state, query);
-    }
+      return editPurpose(state, action);
 
     case EDIT_LICENCE:
-    {
-      const { data, user, timestamp } = action.payload;
-      let query = {
-        status: {
-          $set: STATUS_IN_PROGRESS
-        },
-        lastEdit: {
-          $set: {
-            user,
-            timestamp
-          }
-        },
-        licence: {
-          $merge: data
-        }
-      };
-      return update(state, query);
-    }
+      return editLicence(state, action);
 
     case EDIT_POINT:
-    {
-      const { pointId, data, user, timestamp } = action.payload;
-
-      const query = {
-        status: {
-          $set: STATUS_IN_PROGRESS
-        },
-        lastEdit: {
-          $set: {
-            user,
-            timestamp
-          }
-        },
-        licence: {
-          data: {
-            current_version: {
-              purposes: {
-              }
-            }
-          }
-        }
-      };
-
-      state.licence.data.current_version.purposes.forEach((purpose, i) => {
-        purpose.purposePoints.forEach((point, j) => {
-          if (parseInt(point.point_detail.ID) === parseInt(pointId)) {
-            query.licence.data.current_version.purposes[i] = {
-              purposePoints: {
-                [j]: {
-                  point_detail: {
-                    $merge: data
-                  }
-                }
-              }
-            };
-          }
-        });
-      });
-
-      return update(state, query);
-    }
+      return editPoint(state, action);
 
     case EDIT_CONDITION:
-    {
-      const { conditionId, data, user, timestamp } = action.payload;
-
-      const query = {
-        status: {
-          $set: STATUS_IN_PROGRESS
-        },
-        lastEdit: {
-          $set: {
-            user,
-            timestamp
-          }
-        },
-        licence: {
-          data: {
-            current_version: {
-              purposes: {
-              }
-            }
-          }
-        }
-      };
-
-      state.licence.data.current_version.purposes.forEach((purpose, i) => {
-        purpose.licenceConditions.forEach((condition, j) => {
-          if (parseInt(condition.ID) === parseInt(conditionId)) {
-            query.licence.data.current_version.purposes[i] = {
-              licenceConditions: {
-                [j]: {
-                  $merge: data
-                }
-              }
-            };
-          }
-        });
-      });
-
-      return update(state, query);
-    }
+      return editCondition(state, action);
 
     case SET_STATUS:
-    {
-      const { status, notes, user, timestamp } = action.payload;
-
-      const query = {
-        status: {
-          $set: status
-        }
-      };
-
-      if (notes) {
-        query.notes = {
-          $push: [{
-            notes,
-            user,
-            timestamp
-          }]
-        };
-      }
-
-      return update(state, query);
-    }
+      return setState(state, action);
 
     default:
       return state;

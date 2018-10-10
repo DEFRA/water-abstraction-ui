36ef6ffe730b4000bb4c6b30328a71e94d5f540e
Mark Swaffer <mdswaffer@gmail.com>
2018-10-10 14:35:31 +0100

    Code climate correction

--- src/lib/forms/mapFields.js
+++ src/lib/forms/mapFields.js
@@ -3,19 +3,20 @@ const { cloneDeep, isObject } = require('lodash');
 /**
  * Applies the supplied function to every field
  * using a deep traversal
- * Mutates the supplied object
  */
 const mapFields = (form, fn) => {
   const f = cloneDeep(form);
-  if (isObject(f)) {
-    if ('fields' in f) {
-      f.fields = f.fields.map(fn);
-    }
-    for (let key in f) {
-      if (isObject(f[key])) {
-        f[key] = mapFields(f[key], fn);
-      }
-    }
+
+  if (!isObject(f)) {
+    return f;
+  }
+
+  if ('fields' in f) {
+    f.fields = f.fields.map(fn);
+  }
+
+  for (const key in f) {
+    f[key] = mapFields(f[key], fn);
   }
   return f;
 };

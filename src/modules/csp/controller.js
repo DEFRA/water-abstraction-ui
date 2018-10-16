57e5b5dd426131b30a53acdb7eb39297a3221c71
Mark Swaffer <mdswaffer@gmail.com>
2018-10-16 16:07:03 +0100

    Adds a daily limit to the number of logs

--- src/modules/csp/controller.js
+++ src/modules/csp/controller.js
@@ -1,8 +1,28 @@
+const moment = require('moment');
+let dateCounts = {};
+
 const postReport = async (request, h) => {
-  request.log(['warn', 'csp'], request.payload.toString('utf-8'));
-  return h.response('logged').code(201);
+  const date = moment().format('YYYY-MM-DD');
+
+  if (canLog(date, dateCounts) && request.payload.length < 3000) {
+    dateCounts = incrementDateCount(date, dateCounts);
+    request.log(['warn', 'csp'], request.payload.toString('utf-8'));
+  }
+  return dateCounts;
+};
+
+const canLog = (date, dateCounts) => {
+  const count = dateCounts[date] || 0;
+  return count < 100;
+};
+
+const incrementDateCount = (date, dateCounts) => {
+  dateCounts[date] = (dateCounts[date] || 0) + 1;
+  return dateCounts;
 };
 
 module.exports = {
+  canLog,
+  incrementDateCount,
   postReport
 };
---
+++ test/modules/csp/controller.js
@@ -0,0 +1,52 @@
+const { expect } = require('code');
+const { experiment, test } = exports.lab = require('lab').script();
+
+const { canLog, incrementDateCount } = require('../../../src/modules/csp/controller');
+
+experiment('canLog', () => {
+  test('returns true if there are less than 100 logs for the date', async () => {
+    const date = '2018-01-01';
+    const dateCounts = {
+      '2018-01-01': 99
+    };
+    expect(canLog(date, dateCounts)).to.be.true();
+  });
+
+  test('returns false if there are 100 logs for the date', async () => {
+    const date = '2018-01-01';
+    const dateCounts = {
+      '2018-01-01': 100
+    };
+    expect(canLog(date, dateCounts)).to.be.false();
+  });
+
+  test('returns false if there are 101 logs for the date', async () => {
+    const date = '2018-01-01';
+    const dateCounts = {
+      '2018-01-01': 101
+    };
+    expect(canLog(date, dateCounts)).to.be.false();
+  });
+});
+
+experiment('incrementDateCount', () => {
+  test('sets the count to one for a new date', async () => {
+    const date = '2018-01-01';
+    const dateCounts = {};
+    const updated = incrementDateCount(date, dateCounts);
+    expect(updated).to.equal({
+      [date]: 1
+    });
+  });
+
+  test('increments for an existing date', async () => {
+    const date = '2018-01-01';
+    const dateCounts = {
+      [date]: 81
+    };
+    const updated = incrementDateCount(date, dateCounts);
+    expect(updated).to.equal({
+      [date]: 82
+    });
+  });
+});

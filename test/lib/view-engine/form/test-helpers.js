8dd39549d58f4f086d6e6d8b7ff432add3d629e5
James Carmichael <james@siteclick.co.uk>
2018-12-07 11:35:43 +0000

    Unit tests for form library to GDS nunjucks mapper

---
+++ test/lib/view-engine/form/test-helpers.js
@@ -0,0 +1,60 @@
+const Joi = require('joi');
+const { formFactory, fields } = require('../../../../src/lib/forms');
+
+const textField = fields.text('text_field', {
+  label: 'A text field',
+  hint: 'A hint',
+  controlClass: 'css-class'
+}, 'value');
+
+const radioField = fields.radio('radio_field', {
+  label: 'A radio field',
+  hint: 'A hint',
+  controlClass: 'css-class',
+  choices: [{
+    value: 'option-a',
+    hint: 'A hint about option A',
+    label: 'A'
+  }]
+}, 'value');
+
+const checkboxField = fields.checkbox('checkbox_field', {
+  label: 'A checkbox field',
+  hint: 'A hint',
+  controlClass: 'css-class'
+}, 'value');
+
+const dateField = fields.checkbox('date_field', {
+  label: 'Date field',
+  hint: 'A hint'
+}, '2018-11-01');
+
+const dropdownField = fields.dropdown('dropdown_field', {
+  label: 'A dropdown field',
+  hint: 'A hint',
+  controlClass: 'css-class',
+  choices: [{
+    value: 'option-a',
+    label: 'A'
+  }]
+}, 'value');
+
+let f = formFactory('/action');
+f.fields.push(textField);
+f.fields.push(radioField);
+f.fields.push(checkboxField);
+
+const schema = {
+  text_field: Joi.string().required(),
+  radio_field: Joi.string().required(),
+  checkbox_field: Joi.string().required()
+};
+module.exports = {
+  textField,
+  radioField,
+  checkboxField,
+  form: f,
+  schema,
+  dateField,
+  dropdownField
+};

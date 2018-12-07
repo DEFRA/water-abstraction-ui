0ab0a772f2373d8ab9567df2a2f6e7914d34dc32
James Carmichael <james@siteclick.co.uk>
2018-12-07 09:13:30 +0000

    Add GOV.UK frontend support for form library

--- src/lib/forms/validationAdapters/joi.js
+++ src/lib/forms/validationAdapters/joi.js
@@ -16,7 +16,7 @@ const createSchemaFromForm = form => {
       s = Joi.boolean();
     }
     if (field.options.mapper === 'dateMapper') {
-      s = Joi.date().iso();
+      s = Joi.string().isoDate().options({ convert: false });
     }
     if (field.options.choices) {
       s = s.valid(field.options.choices.map(choice => choice.value));

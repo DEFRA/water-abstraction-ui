43f6bdf4df87e3238aee435b40d9b7cd5d7ded31
Mark Swaffer <mdswaffer@gmail.com>
2018-12-21 16:57:53 +0000

    Adds wr22 2.4 condition.

--- src/modules/abstraction-reform/lib/schema.js
+++ src/modules/abstraction-reform/lib/schema.js
@@ -8,7 +8,8 @@ module.exports = {
   wr22: [
     require('../schema/wr22/2.1.json'),
     require('../schema/wr22/2.2.json'),
-    require('../schema/wr22/2.3.json'),
+    // require('../schema/wr22/2.3.json'),
+    require('../schema/wr22/2.4.json'),
     require('../schema/wr22/2.7.json'),
     require('../schema/wr22/2.8.json')
   ]

2e00ccade64a3dcefacc75a1617675534da2b628
Mark Swaffer <mdswaffer@gmail.com>
2018-12-21 19:31:36 +0000

    Adds WR22 2.5 (not complete)
    Makes the NGR regex accept empty string.
    
    Begins the 2.5 condition.

--- src/modules/abstraction-reform/lib/schema.js
+++ src/modules/abstraction-reform/lib/schema.js
@@ -10,6 +10,7 @@ module.exports = {
     require('../schema/wr22/2.2.json'),
     // require('../schema/wr22/2.3.json'),
     require('../schema/wr22/2.4.json'),
+    require('../schema/wr22/2.5.json'),
     require('../schema/wr22/2.7.json'),
     require('../schema/wr22/2.8.json')
   ]

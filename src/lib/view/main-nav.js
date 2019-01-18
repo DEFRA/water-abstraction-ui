4e0ae4c41848a55a69fcde48f74f97f7f5bf2178
James Carmichael <james@siteclick.co.uk>
2019-01-18 14:26:56 +0000

    Change main nav link for AR to Digitise!

--- src/lib/view/main-nav.js
+++ src/lib/view/main-nav.js
@@ -8,7 +8,7 @@ const {
 // Internal links
 const internalLinks = {
   licences: createLink('View licences', '/admin/licences', 'view'),
-  ar: createLink('Abstraction reform', '/admin/abstraction-reform', 'ar'),
+  ar: createLink('Digitise!', '/admin/abstraction-reform', 'ar'),
   notifications: createLink('Reports and notifications', '/admin/notifications', 'notifications'),
   returns: createLink('Manage returns', '/admin/returns', 'returns')
 };

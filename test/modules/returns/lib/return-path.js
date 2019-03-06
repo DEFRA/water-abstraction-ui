d54016482c79ecd11d111bd9fe21f4d5cd714d66
Dana Gajewska <mdgajewska@outlook.com>
2019-03-06 14:32:38 +0000

    Allow internal returns user to edit returns with 'completed' status
        WATER-1954
    
        Change logic to allow internal returns users to edit returns which are not void, are after summer 2018 and are past the end date. This is to include returns with a 'completed' status. Fix test to match logic change

--- test/modules/returns/lib/return-path.js
+++ test/modules/returns/lib/return-path.js
@@ -129,7 +129,7 @@ experiment('returnPath -  internal users', () => {
     });
   });
 
-  test('An internal returns user can view a return if has completed status', async () => {
+  test('An internal returns user can edit a return if has completed status', async () => {
     const request = getInternalRequest(true);
     expect(getReturnPath({ ...ret, status: 'completed' }, request)).to.equal({
       path: internalEdit,

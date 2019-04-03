2fdbc32b304df69da3106db2ec8dded215935bf0
James Carmichael <james@siteclick.co.uk>
2019-04-03 17:09:16 +0100

    Fixes logic to display x10 help text on quantities form (#714)
    * Fixes logic to display x10 help text on quantities form
    
    * Add test for x10 help text
    
    * Fix issue with filtering form fields in return quantities unit tests
    
    * Unit test tweak

---
+++ test/modules/returns/forms/quantities.js
@@ -0,0 +1,48 @@
+const { expect } = require('code');
+const {
+  experiment,
+  test
+} = exports.lab = require('lab').script();
+const { filter } = require('lodash');
+const { quantitiesForm } = require('../../../../src/modules/returns/forms/quantities');
+
+const createRequest = () => {
+  return {
+    view: {
+      csrfToken: 'test-csrf-token'
+    },
+    query: {
+      returnId: 'test-return-id'
+    }
+  };
+};
+
+const createReturn = (type = 'measured') => {
+  return {
+    reading: {
+      units: 'l',
+      type
+    },
+    lines: [],
+    requiredLines: []
+  };
+};
+const isParagraph = (field) => {
+  return field.options.widget === 'paragraph';
+};
+
+experiment('quantitiesForm', () => {
+  const expectedText = 'Remember if you have a x10 meter you need to multiply your volumes.';
+
+  test('adds help text about x10 meters if measured volumes', async () => {
+    const form = quantitiesForm(createRequest(), createReturn());
+    const text = filter(form.fields, isParagraph).map(row => row.options.text);
+    expect(text).to.include(expectedText);
+  });
+
+  test('does not add help text about x10 meters if measured volumes', async () => {
+    const form = quantitiesForm(createRequest(), createReturn('estimated'));
+    const text = filter(form.fields, isParagraph).map(row => row.options.text);
+    expect(text).to.not.include(expectedText);
+  });
+});

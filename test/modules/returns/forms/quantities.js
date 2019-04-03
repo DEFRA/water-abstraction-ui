566a27b75f5f0c917047c66d22b05e89ed4f1be3
James Carmichael <james@siteclick.co.uk>
2019-04-03 15:39:32 +0100

    Add test for x10 help text

---
+++ test/modules/returns/forms/quantities.js
@@ -0,0 +1,42 @@
+const { expect } = require('code');
+const {
+  experiment,
+  test
+} = exports.lab = require('lab').script();
+const { filter } = require('lodash');
+const { quantitiesForm } = require('../../../../src/modules/returns/forms/quantities');
+
+const createRequest = () => {
+  return {
+    view: {
+      csrfToken: 'test-csrf-token'
+    },
+    query: {
+      returnId: 'test-return-id'
+    }
+  };
+};
+
+const createReturn = (type = 'measured') => {
+  return {
+    reading: {
+      units: 'l',
+      type
+    },
+    lines: [],
+    requiredLines: []
+  };
+};
+experiment('quantitiesForm', () => {
+  test('adds help text about x10 meters if measured volumes', async () => {
+    const form = quantitiesForm(createRequest(), createReturn());
+    const text = filter(form.fields).map(row => row.options.text);
+    expect(text).to.include('Remember if you have a x10 meter you need to multiply your volumes.');
+  });
+
+  test('does not add help text about x10 meters if measured volumes', async () => {
+    const form = quantitiesForm(createRequest(), createReturn('estimated'));
+    const text = filter(form.fields).map(row => row.options.text);
+    expect(text).to.not.include('Remember if you have a x10 meter you need to multiply your volumes.');
+  });
+});

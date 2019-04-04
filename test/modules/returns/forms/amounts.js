bda63205a93c0cbc71ab5b94e5d13f4370c46d2e
Dana Gajewska <mdgajewska@outlook.com>
2019-04-04 09:34:18 +0100

    Increase test coverage on amounts and meter-readings forms

---
+++ test/modules/returns/forms/amounts.js
@@ -0,0 +1,59 @@
+const { expect } = require('code');
+const { experiment, test } = exports.lab = require('lab').script();
+const { find } = require('lodash');
+const amountsForm = require('../../../../src/modules/returns/forms/amounts');
+
+const createRequest = (isInternal = true) => {
+  return {
+    view: {
+      csrfToken: 'test-csrf-token'
+    },
+    query: {
+      returnId: 'test-return-id'
+    },
+    auth: {
+      credentials: {
+        scope: isInternal ? 'internal' : 'external'
+      }
+    }
+  };
+};
+
+experiment('amountsForm', () => {
+  test('has a radio field for whether water has been abstracted field', async () => {
+    const request = createRequest();
+    const form = amountsForm(request);
+    const isNil = find(form.fields, { name: 'isNil' });
+    expect(isNil.options.widget).to.equal('radio');
+  });
+
+  test('internal label is shown for internal user', async () => {
+    const request = createRequest();
+    const form = amountsForm(request);
+    const label = form.fields[0];
+    expect(label.options.text).to.equal('Has water been abstracted in this return period?');
+  });
+
+  test('external label is shown for external user', async () => {
+    const request = createRequest(false);
+    const form = amountsForm(request);
+    const label = form.fields[0];
+    expect(label.options.text).to.equal('Have you abstracted water in this return period?');
+  });
+
+  test('has a continue button', async () => {
+    const request = createRequest();
+    const form = amountsForm(request);
+    const button = form.fields.find(f => {
+      return f.options.widget === 'button' && f.options.label === 'Continue';
+    });
+    expect(button).to.exist();
+  });
+
+  test('has a hidden csrf field', async () => {
+    const request = createRequest();
+    const form = amountsForm(request);
+    const csrf = form.fields.find(x => x.name === 'csrf_token');
+    expect(csrf.value).to.equal('test-csrf-token');
+  });
+});

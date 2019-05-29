1767ee2d787eaa8f5a031a88821539633bd07892
James Carmichael <james@siteclick.co.uk>
2019-05-29 14:07:00 +0100

    Create shared AuthConfig class with tests

--- src/external/lib/AuthConfig.js
+++ src/external/lib/AuthConfig.js
@@ -1,8 +1,7 @@
-const { get, set } = require('lodash');
-const uuid = require('uuid/v4');
-const { throwIfError } = require('@envage/hapi-pg-rest-api');
+const { get } = require('lodash');
 const loginHelpers = require('./login-helpers');
 const permissions = require('./permissions');
+const AuthConfigBase = require('../../shared/lib/AuthConfig');
 
 const getUniqueCompanyIds = (entityRoles = []) => {
   const allIds = entityRoles.map(er => er.company_entity_id);
@@ -18,49 +17,15 @@ const getCompanyPredicate = companyId => (role) => role.company_entity_id === co
 
 const mapRolesToScopes = roles => roles.map(role => role.role);
 
-class AuthConfig {
-  constructor (config, connectors) {
-    this.connectors = connectors;
-    this.config = config;
-  }
-
+class AuthConfig extends AuthConfigBase {
   ifAuthenticated (request, h) {
     return loginHelpers.preRedirectIfAuthenticated(request, h);
-  }
-
-  authenticate (email, password) {
-    return this.connectors.idm.users.authenticate(email, password, this.config.idm.application);
-  }
-
-  async signIn (request, user) {
-    const { user_id: userId } = user;
-
-    // Set user ID in auth cookie
-    request.cookieAuth.set({ userId });
-
-    // Set session
-    request.yar.set('csrfToken', uuid());
-    request.yar.set('userId', userId);
-    request.yar.set('ip', request.info.remoteAddress);
-
-    // Create entity
-    // This is currently required by both internal and external as the returns
-    // model requires an entity ID
-    if (!user.external_id) {
-      const entity = await this.connectors.crm.entities.getOrCreateIndividual(user.user_name);
-      await this.connectors.idm.users.updateExternalId(user, entity.entity_id);
-    }
   }
 
   async onSignIn (request, h, user) {
     const path = await loginHelpers.getLoginRedirectPath(request, user);
     return h.metaRedirect(path);
   };
-
-  signOut (request, h) {
-    request.cookieAuth.clear();
-    request.yar.reset();
-  }
 
   onSignOut (request, h) {
     const path = `/signed-out?u=${permissions.isInternal(request) ? 'i' : 'e'}`;
@@ -92,34 +57,6 @@ class AuthConfig {
       companyCount: companyIds.length,
       userScopes
     };
-  };
-
-  async validateFunc (request, data) {
-    const { userId } = data;
-
-    let credentials = {};
-    let valid = false;
-
-    const isValid = !!userId && (request.yar.get('userId') === userId);
-
-    if (isValid) {
-      const { error, data: user } = await this.connectors.idm.users.findOne(userId);
-      throwIfError(error);
-
-      // Get user data and augment request
-      const data = await this._mapUserRequestData(request, user);
-      set(request, 'defra', data);
-
-      if (user) {
-        valid = true;
-        credentials = {
-          userId,
-          scope: data.userScopes
-        };
-      }
-    }
-
-    return { valid, credentials };
   };
 }
 
--- src/internal/lib/AuthConfig.js
+++ src/internal/lib/AuthConfig.js
@@ -1,94 +1,17 @@
-const { get, set } = require('lodash');
-const uuid = require('uuid/v4');
-const { throwIfError } = require('@envage/hapi-pg-rest-api');
+const AuthConfigBase = require('../../shared/lib/AuthConfig');
 
-class AuthConfig {
-  constructor (config, connectors) {
-    this.connectors = connectors;
-    this.config = config;
-  }
-
+class AuthConfig extends AuthConfigBase {
   ifAuthenticated (request, h) {
     return h.metaRedirect('/licences');
-  }
-
-  authenticate (email, password) {
-    return this.connectors.idm.users.authenticate(email, password, this.config.idm.application);
-  }
-
-  async signIn (request, user) {
-    const { user_id: userId } = user;
-
-    // Set user ID in auth cookie
-    request.cookieAuth.set({ userId });
-
-    // Set session
-    request.yar.set('csrfToken', uuid());
-    request.yar.set('userId', userId);
-    request.yar.set('ip', request.info.remoteAddress);
-
-    // Create entity
-    // This is currently required by both internal and external as the returns
-    // model requires an entity ID
-    if (!user.external_id) {
-      const entity = await this.connectors.crm.entities.getOrCreateIndividual(user.user_name);
-      await this.connectors.idm.users.updateExternalId(user, entity.entity_id);
-    }
   }
 
   async onSignIn (request, h, user) {
     return h.metaRedirect('/licences');
   };
 
-  signOut (request, h) {
-    request.cookieAuth.clear();
-    request.yar.reset();
-  }
-
   onSignOut (request, h) {
     return h.metaRedirect(`/signed-out?u=i`);
   }
-
-  async _mapUserRequestData (request, user) {
-    const entityId = get(user, 'external_id');
-    const userScopes = get(user, 'role.scopes', []);
-
-    return {
-      userId: user.user_id,
-      userName: user.user_name,
-      user,
-      entityId,
-      userScopes
-    };
-  };
-
-  async validateFunc (request, data) {
-    const { userId } = data;
-
-    let credentials = {};
-    let valid = false;
-
-    const isValid = !!userId && (request.yar.get('userId') === userId);
-
-    if (isValid) {
-      const { error, data: user } = await this.connectors.idm.users.findOne(userId);
-      throwIfError(error);
-
-      // Get user data and augment request
-      const data = await this._mapUserRequestData(request, user);
-      set(request, 'defra', data);
-
-      if (user) {
-        valid = true;
-        credentials = {
-          userId,
-          scope: data.userScopes
-        };
-      }
-    }
-
-    return { valid, credentials };
-  };
 }
 
 module.exports = AuthConfig;

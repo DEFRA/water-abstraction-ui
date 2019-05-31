f1c973855398520b92c2514968f7a686ccdcb9c4
James Carmichael <james@siteclick.co.uk>
2019-05-31 15:32:53 +0100

    Fix code climate duplication error

--- src/shared/plugins/reset-password/map-joi-password-error.js
+++ src/shared/plugins/reset-password/map-joi-password-error.js
@@ -1,26 +1,4 @@
-
-/**
- * Formats a Joi error to a simple object which can easily be used in a
- * Handlebars template.
- * The field name and the validation that failed are converted into a string
- * which can then be tested in the view, e.g. {# if error.password_min }...
- *
- * The output is e.g.:  { password_min : true, confirmPassword_empty : true }
- * @param {Object} Joi error
- * @return {Object}
- */
-const formatViewError = (error) => {
-  if (!error) {
-    return null;
-  }
-  if (!error.isJoi) {
-    return error;
-  }
-  return error.details.reduce((memo, detail) => {
-    memo[detail.path.join('_') + '_' + detail.type.split('.')[1]] = true;
-    return memo;
-  }, {});
-};
+const formatViewError = require('../../lib/format-view-error');
 
 /**
  * Map new-style error object to existing handlebars template

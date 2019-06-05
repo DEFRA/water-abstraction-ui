b83b5692ceb8265680856edc7d0f064e40391461
James Carmichael <james@siteclick.co.uk>
2019-06-05 11:13:31 +0100

    Initial work on

--- src/external/lib/connectors/services/index.js
+++ src/external/lib/connectors/services/index.js
@@ -2,5 +2,6 @@ const config = require('../../../config');
 
 module.exports = {
   idm: require('./idm')(config),
-  crm: require('./crm')(config)
+  crm: require('./crm')(config),
+  water: require('./water')(config)
 };

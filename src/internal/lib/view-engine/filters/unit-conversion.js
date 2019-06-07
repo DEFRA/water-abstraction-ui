49845c706c2615c4767978ca2635f176af8c8c5d
Mark Swaffer <mdswaffer@gmail.com>
2019-06-07 11:57:48 +0100

    Moves the title-case filter to shared dir
    Moves the unit-conversion filter to shared dir
    Moves the unit filter to shared dir

--- src/internal/lib/view-engine/filters/unit-conversion.js
+++
@@ -1,5 +0,0 @@
-const { convertToCubicMetres } = require('../../../../shared/lib/unit-conversion');
-
-const unitConversion = (value, unit) => convertToCubicMetres(value, unit);
-
-exports.unitConversion = unitConversion;
---
+++ test/shared/view/nunjucks/filters/units.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { experiment, test } = exports.lab = require('lab').script();
+const { expect } = require('code');
+
+const {
+  units
+} = require('../../../../../src/shared/view/nunjucks/filters/units');
+
+experiment('units Nunjucks filter', () => {
+  test('converts m³ to Cubic metres', async () => {
+    expect(units('m³')).to.equal('Cubic metres');
+  });
+
+  test('converts gal to Gallons', async () => {
+    expect(units('gal')).to.equal('Gallons');
+  });
+
+  test('converts l (lowercase L) to Litres', async () => {
+    expect(units('l')).to.equal('Litres');
+  });
+  test('converts Ml to Megalitres', async () => {
+    expect(units('Ml')).to.equal('Megalitres');
+  });
+  test('returns undefined for unknown units', async () => {
+    expect(units('bucket')).to.equal(undefined);
+  });
+});

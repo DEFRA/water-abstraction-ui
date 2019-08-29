df1873ee83050f536f8cf7e371b82442aa9115e8
Mark Swaffer <mdswaffer@gmail.com>
2019-08-29 11:30:07 +0100

    Chore: Removes unused handlebars code
    Removes the unused returns helpers, then prunes any unused helpers from
    handlebars.

--- src/external/lib/view-engine/handlebars.js
+++ src/external/lib/view-engine/handlebars.js
@@ -2,48 +2,10 @@
 
 const handlebars = require('handlebars');
 const moment = require('moment');
-const momentTimezone = require('moment-timezone');
 const qs = require('querystring');
-const sentenceCase = require('sentence-case');
-const titleCase = require('title-case');
-const marked = require('marked');
 const { isString } = require('lodash');
 
-const timezone = 'Europe/London';
-const { pick, reduce } = require('lodash');
-const Joi = require('@hapi/joi');
-
-const commaNumber = require('comma-number');
-const { convertToCubicMetres } = require('../../../shared/lib/unit-conversion');
-const { maxPrecision } = require('../../../shared/lib/number-formatter');
 const { splitString } = require('../../../shared/lib/string-formatter');
-
-/**
- * Formats numbers with commas to separate thousands, eg. 1,000
- * @param {Number} value
- * @return {String} number formatted
- */
-handlebars.registerHelper('commaNumber', function (value) {
-  return commaNumber(value);
-});
-
-/**
- * Converts gallons to cubic metres
- * @param {Number} value
- * @return {String} number formatted
- */
-handlebars.registerHelper('gallonsToCubicMetres', function (value) {
-  return value * 0.00454609;
-});
-
-/**
- * Converts cubic metres to gallons
- * @param {Number} value
- * @return {String} number formatted
- */
-handlebars.registerHelper('cubicMetresToGallons', function (value) {
-  return value / 0.00454609;
-});
 
 /**
  * Creates a pagination anchor tag for the pagination helper
@@ -107,20 +69,6 @@ handlebars.registerHelper('pagination', function (pagination = {}, options) {
   return html;
 });
 
-handlebars.registerHelper('markdown', function (param = '') {
-  // Replace ^ with > because notify represents a blockquote using the carat.
-  const updated = param.replace(/\^/g, '>');
-  return marked(updated);
-});
-
-handlebars.registerHelper('sentenceCase', function (value) {
-  return sentenceCase(value);
-});
-
-handlebars.registerHelper('titleCase', function (value) {
-  return titleCase(value);
-});
-
 handlebars.registerHelper('for', function (from, to, incr, block) {
   var accum = '';
   for (var i = from; i < to; i += incr) { accum += block.fn(i); }
@@ -129,18 +77,8 @@ handlebars.registerHelper('for', function (from, to, incr, block) {
 
 handlebars.registerHelper('equal', require('handlebars-helper-equal'));
 
-handlebars.registerHelper('ifNot', function (param, options) {
-  if (!param) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
-});
-
 handlebars.registerHelper('notEqual', function (v1, v2, options) {
-  if (v1 !== v2) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
+  return (v1 === v2) ? options.inverse(this) : options.fn(this);
 });
 
 handlebars.registerHelper('notNull', function (param, options) {
@@ -169,105 +107,12 @@ handlebars.registerHelper('widgetErrors', function (fieldName, errors = [], opti
   return str;
 });
 
-/**
- * 1 megalitre = 1,000,000 litres = 1,000 cubic metres
- * @param {float} value - the value in cubic metres per second
- * @param {String} unit - can be cm|litre|megalitre
- * @param {String} period - can be second|day
- */
-handlebars.registerHelper('flowConverter', function (value, unit = 'litre', period = 'second', options) {
-  let val = value;
-
-  // Validate
-  const { error } = Joi.validate({ unit, period }, {
-    unit: Joi.string().allow('cm', 'litre', 'megalitre'),
-    period: Joi.string().allow('second', 'day')
-  });
-
-  if (error) {
-    throw error;
-  }
-
-  if (unit === 'litre') {
-    val = val * 1000;
-  }
-  if (unit === 'megalitre') {
-    val = val / 1000;
-  }
-  if (period === 'day') {
-    val = val * 86400;
-  }
-
-  return parseFloat(val).toFixed(1);
-});
-
-/*
-handlebars.registerHelper('toFixed', function (value, dp) {
-  return parseFloat(value).toFixed(dp);
-});
-
-handlebars.registerHelper('toMega')
-*/
-
-/**
- * Gets gauging station value with units
- * - For flows, converts m3/s to m3/day
- * - For levels, adds mASD to datum and returns in m
- */
-handlebars.registerHelper('gsValue', function (measure, convertTo, options) {
-  const { unitName, latestReading: { value } } = measure;
-
-  // Flows - convert to m3/day
-  if (unitName === 'm3/s') {
-    if (convertTo === 'm3/day') {
-      return `${(value * 86400).toFixed(1)}m³/day`;
-    }
-
-    return `${(value).toFixed(1)}m³/s`;
-
-    // return `${(value * 86400).toFixed(1)}m³/day`;
-  }
-
-  // Levels in mASD - convert to level in m
-  if (unitName === 'mASD') {
-    return `${(value).toFixed(2)}m`;
-  }
-
-  // Unknown unit - return as is
-  return `${value}${unitName}`;
-});
-
-/**
- * Tests whether given condition code and subcode is a hands-off flow (HOF)
- * @param {String} code - condition code
- * @param {String} subCode - condition subcode
- */
-handlebars.registerHelper('isHof', function (code, subCode, options) {
-  if (code === 'CES' && (subCode === 'FLOW' || subCode === 'LEV')) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
-});
-
 handlebars.registerHelper('greaterThan', function (v1, v2, options) {
-  if (v1 > v2) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
+  return (v1 > v2) ? options.fn(this) : options.inverse(this);
 });
 
 handlebars.registerHelper('lessThan', function (v1, v2, options) {
-  if (v1 < v2) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
-});
-
-handlebars.registerHelper('or', function (v1, v2, options) {
-  if (v1 || v2) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
+  return (v1 < v2) ? options.fn(this) : options.inverse(this);
 });
 
 /**
@@ -321,33 +166,6 @@ handlebars.registerHelper('concat', function () {
   return arg.join('');
 });
 
-handlebars.registerHelper('join', function (values, separator = ',') {
-  return values.join(separator);
-});
-
-handlebars.registerHelper('encode', function (value) {
-  return encodeURIComponent(value.hash.value);
-});
-
-handlebars.registerHelper('toLowerCase', function (str) {
-  return str.toLowerCase();
-});
-
-handlebars.registerHelper('stringify', function (variable) {
-  var arg = JSON.stringify(variable);
-  return arg;
-});
-
-handlebars.registerHelper('parse', function (variable) {
-  try {
-    var arg = JSON.parse(variable);
-  } catch (e) {
-    return variable;
-  }
-
-  return arg;
-});
-
 const getDatePartFromString = (date, part) => {
   const indexes = { year: 0, month: 1, day: 2 };
   return splitString(date, indexes[part], '-');
@@ -377,40 +195,13 @@ const getDatePart = (date, part) => {
     throw new Error(err);
   }
 
-  if (!date) {
-    return;
+  if (date) {
+    const func = isString(date) ? getDatePartFromString : getDatePartFromDate;
+    return func(date, part);
   }
-
-  const func = isString(date) ? getDatePartFromString : getDatePartFromDate;
-  return func(date, part);
 };
 
 handlebars.registerHelper('getDatePart', getDatePart);
-
-handlebars.registerHelper('formatISODate', function (dateInput, options) {
-  const date = momentTimezone(dateInput);
-  const { format = 'D MMMM YYYY' } = options.hash;
-  return date.isValid() ? date.tz(timezone).format(format) : dateInput;
-});
-
-handlebars.registerHelper('formatISOTime', function (dateInput) {
-  const date = momentTimezone(dateInput);
-  return date.isValid() ? date.tz(timezone).format('h:mma') : dateInput;
-});
-
-handlebars.registerHelper('isFirstDayOfMonth', function (date, options) {
-  if (moment(date).startOf('month').isSame(date, 'day')) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
-});
-
-handlebars.registerHelper('isLastDayOfMonth', function (date, options) {
-  if (moment(date).endOf('month').isSame(date, 'day')) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
-});
 
 handlebars.registerHelper('formatDate', function (dateInput) {
   var date = moment(dateInput, 'DD/MM/YYYY');
@@ -427,7 +218,6 @@ const changeDateFormatting = (dateInput, inputFormat, outputFormat = 'D MMMM YYY
 };
 
 handlebars.registerHelper('formatSortableDate', dateInput => changeDateFormatting(dateInput, 'YYYYMMDD'));
-handlebars.registerHelper('formatTS', dateInput => changeDateFormatting(dateInput, 'YYYY-MM-DD'));
 
 handlebars.registerHelper('formatToDate', function (dateInput, defaultValue) {
   if (dateInput === null) {
@@ -453,18 +243,6 @@ handlebars.registerHelper('formatPeriod', function (inputStart = '', inputEnd = 
     periodEnd = moment(tmpInputEnd, 'DD/MM/YYYY');
   }
   return 'From ' + periodStart.format('D MMMM') + ' until ' + periodEnd.format('D MMMM');
-});
-
-handlebars.registerHelper('formatNotifyAddress', function (address) {
-  const addressParts = pick(address, [
-    'address_line_1', 'address_line_2', 'address_line_3',
-    'address_line_4', 'address_line_5', 'address_line_6',
-    'postcode'
-  ]);
-
-  return reduce(addressParts, (acc, part) => {
-    return part ? `${acc}${part}<br />` : acc;
-  }, '');
 });
 
 /**
@@ -512,68 +290,7 @@ handlebars.registerHelper('ngrPoint', function (points) {
   return response;
 });
 
-handlebars.registerHelper('precision', function (value, dp) {
-  return Number(value).toFixed(dp);
-});
-
-handlebars.registerHelper('yesIfTruthy', value => value ? 'Yes' : '');
-
-handlebars.registerHelper('naldRegion', function (code) {
-  const codes = {
-    1: 'Anglian',
-    2: 'Midlands',
-    3: 'North east',
-    4: 'North west',
-    5: 'South west',
-    6: 'Southern',
-    7: 'Thames'
-  };
-
-  return codes[code];
-});
-
-/**
- * Accepts an object containing period start/end month/day
- * and returns a string in the format 20 January to 30 December
- * @param {Object} metadata
- * @return {String} formatted date
- */
-handlebars.registerHelper('returnPeriod', function (metadata) {
-  const { periodEndDay,
-    periodEndMonth,
-    periodStartDay,
-    periodStartMonth } = metadata;
-
-  const start = moment().month(periodStartMonth - 1).date(periodStartDay);
-  const end = moment().month(periodEndMonth - 1).date(periodEndDay);
-
-  return start.format('D MMMM') + ' to ' + end.format('D MMMM');
-});
-
-/**
- * Converts a value in cubic metres to the specified user unit
- * @param {Object} metadata
- * @return {String} formatted date
- */
-handlebars.registerHelper('convertToCubicMetres', (value, options) => {
-  const { unit } = options.hash;
-  return convertToCubicMetres(value, unit);
-});
-
-/**
- * Formats a number to 3 DP and comma separates 000's
- */
-handlebars.registerHelper('formatQuantity', (value) => {
-  return commaNumber(maxPrecision(value, 3));
-});
-
-/**
- * Splits a string to array, and gets the numbered segment
- */
-handlebars.registerHelper('splitString', (value, options) => {
-  const { index = 0, separator = ',' } = options.hash;
-  return splitString(value, index, separator);
-});
+handlebars.registerHelper('precision', (value, dp) => Number(value).toFixed(dp));
 
 /**
  * Splits a string to array, and gets the numbered segment
@@ -585,25 +302,5 @@ handlebars.registerHelper('includes', (arr = [], options) => {
   }
   return options.inverse(this);
 });
-
-/**
- * Each iterates in reverse order
- * @see {@link https://github.com/diy/handlebars-helpers/blob/master/lib/each-reverse.js}
- */
-function eachReverse (context) {
-  var options = arguments[arguments.length - 1];
-  var ret = '';
-
-  if (context && context.length > 0) {
-    for (var i = context.length - 1; i >= 0; i--) {
-      ret += options.fn(context[i]);
-    }
-  } else {
-    ret = options.inverse(this);
-  }
-
-  return ret;
-};
-handlebars.registerHelper('eachReverse', eachReverse);
 
 module.exports = handlebars;

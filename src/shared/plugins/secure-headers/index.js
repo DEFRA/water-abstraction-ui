0e46791259b0942868f3ad03b57d657f50ee058c
James Carmichael <james@siteclick.co.uk>
2019-10-18 12:51:33 +0100

    FIx code climate complexity issue

--- src/shared/plugins/secure-headers/index.js
+++ src/shared/plugins/secure-headers/index.js
@@ -17,32 +17,34 @@ const getFeaturePolicy = () => {
 const isStaticAsset = request =>
   ['.js', '.css', '.ico', '.png', '.woff', '.woff2'].includes(path.extname(request.path));
 
+const postHandler = (request, reply) => {
+  if ('headers' in request.response) {
+    request.response.headers['X-Frame-Options'] = 'DENY';
+    request.response.headers['X-Content-Type-Options'] = 'nosniff';
+    request.response.headers['X-XSS-Protection'] = '1';
+    request.response.headers['Strict-Transport-Security'] = 'max-age=86400; includeSubDomains';
+    request.response.headers['Referrer-Policy'] = 'strict-origin-when-cross-origin';
+    request.response.headers['Feature-Policy'] = getFeaturePolicy();
+
+    // Please note the CSP headers are handled and configured using the
+    // Blankie plugin which is added in the index.js entry fie.
+    if (isStaticAsset(request)) {
+      request.response.headers['cache-control'] = 'public, max-age=3600';
+      request.response.headers['Expires'] = 3600;
+    } else {
+      request.response.headers['cache-control'] = 'no-cache, no-store, must-revalidate';
+      request.response.headers['Expires'] = 0;
+      request.response.headers['Pragma'] = 'no-cache';
+    }
+  }
+  return reply.continue;
+};
+
 const secureHeadersPlugin = {
   register: (server, options) => {
     server.ext({
       type: 'onPostHandler',
-      method (request, reply) {
-        if ('headers' in request.response) {
-          request.response.headers['X-Frame-Options'] = 'DENY';
-          request.response.headers['X-Content-Type-Options'] = 'nosniff';
-          request.response.headers['X-XSS-Protection'] = '1';
-          request.response.headers['Strict-Transport-Security'] = 'max-age=86400; includeSubDomains';
-          request.response.headers['Referrer-Policy'] = 'strict-origin-when-cross-origin';
-          request.response.headers['Feature-Policy'] = getFeaturePolicy();
-
-          // Please note the CSP headers are handled and configured using the
-          // Blankie plugin which is added in the index.js entry fie.
-          if (isStaticAsset(request)) {
-            request.response.headers['cache-control'] = 'public, max-age=3600';
-            request.response.headers['Expires'] = 3600;
-          } else {
-            request.response.headers['cache-control'] = 'no-cache, no-store, must-revalidate';
-            request.response.headers['Expires'] = 0;
-            request.response.headers['Pragma'] = 'no-cache';
-          }
-        }
-        return reply.continue;
-      }
+      method: postHandler
     });
   },
 

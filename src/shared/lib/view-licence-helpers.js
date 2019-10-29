90224fa358d58c7076ccd5604a1694634960b5d0
stephan-hough <stephan.hough@rpa.gov.uk>
2019-10-29 10:12:00 +0000

    Feat/WATER-2330 -- bug fix

--- src/shared/lib/view-licence-helpers.js
+++ src/shared/lib/view-licence-helpers.js
@@ -14,8 +14,7 @@ const getCommonViewContext = request => {
 };
 
 const getCommonBackLink = request => {
-  const { documentId } = request.params;
-  const { licenceNumber } = request.licence.summary;
+  const { documentId } = request.params;  
   return {
     back: `/licences/${documentId}`,
     backText: `Back`

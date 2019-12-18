59e0c267b3f810af08098860cf0b5a05749c9e79
Dana Gajewska <danusia.x@outlook.com>
2019-12-18 09:11:58 +0000

    Add tests for notifications controller

--- test/internal/modules/notifications/controller.js
+++ test/internal/modules/notifications/controller.js
@@ -6,6 +6,9 @@ const { experiment, test, beforeEach, afterEach } = exports.lab = require('@hapi
 const sinon = require('sinon');
 const sandbox = sinon.createSandbox();
 
+const {
+  getStep
+} = require('internal/modules/notifications/controller');
 const services = require('internal/lib/connectors/services');
 
 if (process.env.TEST_MODE) {
@@ -62,3 +65,105 @@ if (process.env.TEST_MODE) {
     });
   });
 }
+
+const getRequest = options => {
+  return {
+    auth: {
+      credentials: {
+        scope: ['hof_notifications']
+      }
+    },
+    defra: {
+      userId: 'test-user'
+    },
+    params: {
+      id: 3
+    },
+    query: {
+      step: 1,
+      start: options.start
+    },
+    yar: {
+      notificationsFlow: options.state || {},
+      set: sandbox.stub(),
+      get: sandbox.stub()
+    }
+  };
+};
+
+const userData = {
+  userId: 'test-user'
+};
+
+const h = {
+  view: sandbox.stub(),
+  redirect: sandbox.stub()
+};
+
+const widget = {
+  lookup: {
+    filter: 'filter'
+  }
+};
+
+const configData = {
+  task_config_id: 'config-id',
+  config: {
+    title: 'page-title',
+    steps: [{
+      widgets: [widget]
+    }, {
+      widgets: [widget]
+    }]
+  },
+  subtype: 'hof-stop'
+};
+experiment('modules/notifications/controller', async () => {
+  beforeEach(() => {
+    sandbox.stub(services.water.taskConfigs, 'findOne').resolves({ data: configData });
+    sandbox.stub(services.idm.users, 'findOne').resolves({ data: { user_data: userData } });
+    sandbox.stub(services.water.lookups, 'findMany').resolves({ data: 'widget-data' });
+  });
+  afterEach(async () => {
+    await sandbox.restore();
+  });
+  experiment('.getStep', async () => {
+    test('returns the expected template and view context when start is undefined', async () => {
+      const request = getRequest({});
+      await getStep(request, h);
+      const [template, view] = h.view.lastCall.args;
+
+      expect(template).to.equal('nunjucks/notifications/step');
+      expect(view).to.be.an.object();
+      expect(view.step).to.equal(configData.config.steps[0]);
+      expect(view.formAction).to.equal(`/notifications/${configData.task_config_id}?step=${request.query.step}`);
+      expect(view.pageTitle).to.equal(configData.config.title);
+    });
+  });
+  experiment('.getStartFlow', async () => {
+    test('redirects to expected url if contact details not set', async () => {
+      const request = getRequest({ start: 1 });
+      await getStep(request, h);
+      const [url] = h.redirect.lastCall.args;
+      const encodedUri = encodeURIComponent(`/notifications/${request.params.id}?start=${request.query.start}`);
+
+      expect(url).to.equal(`/notifications/contact?redirect=${encodedUri}`);
+    });
+    test('returns the expected template and view context when contact details are set', async () => {
+      const userData = {
+        contactDetails: { name: 'Timothy' }
+      };
+      services.idm.users.findOne.resolves({ data: { user_data: userData } });
+      const request = getRequest({ start: 1 });
+      await getStep(request, h);
+
+      const [template, view] = h.view.lastCall.args;
+
+      expect(template).to.equal('nunjucks/notifications/step');
+      expect(view).to.be.an.object();
+      expect(view.step).to.equal(configData.config.steps[1]);
+      expect(view.formAction).to.equal(`/notifications/${configData.task_config_id}?step=0`);
+      expect(view.pageTitle).to.equal(configData.config.title);
+    });
+  });
+});

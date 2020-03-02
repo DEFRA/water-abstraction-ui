6702e09c8266d4c0abc4b94fc8d653ec9822123b
James Carmichael <james@siteclick.co.uk>
2020-03-02 09:26:40 +0000

    Feat/water 2512 invoice view (#1183)
    * Link up batch invoice detail page to API
    
    * Move getDocumentIdMap to CRM documents service, add tests
    
    * Adding tests for mappers
    
    * Additional tests
    
    * Improve layout of table on batch invoice page

--- src/shared/view/nunjucks/filters/abstraction-period.js
+++ src/shared/view/nunjucks/filters/abstraction-period.js
@@ -1,20 +1,32 @@
 const moment = require('moment');
+const { isObject, isString } = require('lodash');
+
+const formatDate = (day, month) => {
+  const m = moment(`${day}/${month}`, 'D/M');
+  if (!m.isValid()) {
+    throw new Error(`Invalid abstraction period ${day}/${month} in abstractionPeriod filter`);
+  }
+  return m.format('D MMMM');
+};
 
 /**
  * Formats an abstraction period in the form day/month, e.g. 1/4
  * to a readable form, e.g. 1 April
- * @param  {String} str - day and month string
+ * @param  {String|Object} val - day and month string
  * @return {String}     - readable day and month string
  */
-const abstractionPeriod = str => {
-  if (str === null) {
-    return null;
+const abstractionPeriod = val => {
+  if (isObject(val)) {
+    const dates = [
+      formatDate(val.startDay, val.startMonth),
+      formatDate(val.endDay, val.endMonth)
+    ];
+    return dates.join(' to ');
   }
-  const m = moment(str, 'D/M');
-  if (!m.isValid()) {
-    throw new Error(`Invalid abstraction period ${str} in abstractionPeriod filter`);
+  if (isString(val)) {
+    const [day, month] = val.split('/');
+    return formatDate(day, month);
   }
-  return m.format('D MMMM');
 };
 
 exports.abstractionPeriod = abstractionPeriod;

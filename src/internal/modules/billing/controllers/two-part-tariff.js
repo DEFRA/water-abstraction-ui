d0f504802c47a10e8537b2bed2b62ec85f0d8d81
stephan.hough <stephan.hough@rpa.gov.uk>
2020-03-26 14:38:10 +0000

    feat/water-2597 # View 2PT Licences Ready for Billing -- new route, controller and tests

--- src/internal/modules/billing/controllers/two-part-tariff.js
+++ src/internal/modules/billing/controllers/two-part-tariff.js
@@ -9,6 +9,24 @@ const messages = {
   60: 'Over abstraction'
 };
 
+const getTotals = licences => {
+  const initVals = {
+    ready: 0,
+    errors: 0,
+    total: 0
+  };
+  const totalErrors = licences.reduce((acc, row) => ({
+    errors: acc.errors + (row.twoPartTariffStatuses.length > 0 ? 1 : 0)
+  }), initVals);
+
+  const totals = {
+    ...totalErrors,
+    ready: licences.length - totalErrors.errors,
+    total: licences.length
+  };
+  return totals;
+};
+
 const getErrorString = errorCodes => errorCodes.reduce((acc, code) => {
   return acc ? 'Multiple errors' : messages[code];
 }, null);
@@ -16,25 +34,33 @@ const getErrorString = errorCodes => errorCodes.reduce((acc, code) => {
 const getTwoPartTariffReview = async (request, h) => {
   const { batch } = request.pre;
   const licencesData = await services.water.billingBatches.getBatchLicences(batch.id);
-
   // gets 2pt matching error messages and define error types
   const licences = licencesData.map(licence => ({
     ...licence,
     twoPartTariffStatuses: getErrorString(licence.twoPartTariffStatuses)
   }));
 
-  // calculate total errors
-  const totals = licences.reduce((acc, row) => ({
-    errors: acc.errors + (row.twoPartTariffStatuses ? 1 : 0)
-  }), { errors: 0 });
-
   return h.view('nunjucks/billing/two-part-tariff-review', {
     ...request.view,
     batch,
     licences,
-    totals,
+    totals: getTotals(licencesData),
+    back: `/billing/batch/list`
+  });
+};
+
+const getTwoPartTariffReady = async (request, h) => {
+  const { batch } = request.pre;
+  const licences = await services.water.billingBatches.getBatchLicences(batch.id);
+
+  return h.view('nunjucks/billing/two-part-tariff-ready', {
+    ...request.view,
+    batch,
+    licences,
+    totals: getTotals(licences),
     back: `/billing/batch/list`
   });
 };
 
 module.exports.getTwoPartTariffReview = getTwoPartTariffReview;
+module.exports.getTwoPartTariffReady = getTwoPartTariffReady;
--- test/internal/modules/billing/routes/two-part-tariff.js
+++ test/internal/modules/billing/routes/two-part-tariff.js
@@ -5,10 +5,16 @@ const { experiment, test } = exports.lab = require('@hapi/lab').script();
 const { scope } = require('internal/lib/constants');
 const routes = require('internal/modules/billing/routes/two-part-tariff');
 
-experiment('internal/modules/billing/routes', () => {
+experiment('internal/modules/billing/routes/two-part-tarriff', () => {
   experiment('.getBillingTwoPartTariffReview', () => {
     test('limits scope to users with billing role', async () => {
       expect(routes.getBillingTwoPartTariffReview.config.auth.scope)
+        .to.only.include([scope.billing]);
+    });
+  });
+  experiment('.getBillingTwoPartTariffReady', () => {
+    test('limits scope to users with billing role', async () => {
+      expect(routes.getBillingTwoPartTariffReady.config.auth.scope)
         .to.only.include([scope.billing]);
     });
   });

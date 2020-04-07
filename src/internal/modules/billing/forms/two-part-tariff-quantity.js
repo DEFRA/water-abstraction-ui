ce3569b488a79c07e6e5912e4b171b6123c3eba2
James Carmichael <james@siteclick.co.uk>
2020-04-07 18:02:55 +0100

    Fix codeclimate issue due to long function. Add form error summary on TPT quantity form page. Fix issue with form error summary anchor links.

--- src/internal/modules/billing/forms/two-part-tariff-quantity.js
+++ src/internal/modules/billing/forms/two-part-tariff-quantity.js
@@ -3,6 +3,53 @@
 const { formFactory, fields } = require('shared/lib/forms/');
 const Joi = require('@hapi/joi');
 
+/**
+ * Gets an array of form choices for the billable quantity
+ * @param {Number} authorisedAnnualQuantity
+ * @return {Array}
+ */
+const getRadioChoices = authorisedAnnualQuantity => [{
+  label: `Authorised (${authorisedAnnualQuantity}ML)`,
+  value: 'authorised'
+}, {
+  label: 'Custom (ML)',
+  value: 'custom',
+  fields: [
+    fields.text('customQuantity', {
+      label: 'Billable quantity',
+      type: 'number',
+      controlClass: 'govuk-!-width-one-third',
+      errors: {
+        'number.base': {
+          message: 'Enter the billable quantity'
+        },
+        'number.min': {
+          message: 'The quantity must be zero or higher'
+        },
+        'number.max': {
+          message: 'The quantity must be the same as or less than the authorised amount'
+        }
+      }
+    })
+  ]
+}];
+
+/**
+ * Gets a form field object for the billable quantity radio button
+ * @param {Number} authorisedAnnualQuantity
+ * @return {Object} form radio object
+ */
+const getQuantityRadio = authorisedAnnualQuantity => fields.radio('quantity', {
+  label: 'Set the billable quantity for this bill run',
+  subHeading: true,
+  errors: {
+    'any.required': {
+      message: 'Select the billable quantity'
+    }
+  },
+  choices: getRadioChoices(authorisedAnnualQuantity)
+});
+
 const twoPartTariffQuantityForm = (request, transaction) => {
   const { csrfToken } = request.view;
 
@@ -13,40 +60,7 @@ const twoPartTariffQuantityForm = (request, transaction) => {
 
   const f = formFactory(action, 'POST');
 
-  f.fields.push(fields.radio('quantity', {
-    label: 'Set the billable quantity for this bill run',
-    subHeading: true,
-    errors: {
-      'any.required': {
-        message: 'Select the billable quantity'
-      }
-    },
-    choices: [{
-      label: `Authorised (${authorisedAnnualQuantity}ML)`,
-      value: 'authorised'
-    }, {
-      label: 'Custom (ML)',
-      value: 'custom',
-      fields: [
-        fields.text('customQuantity', {
-          label: 'Billable quantity',
-          type: 'number',
-          controlClass: 'govuk-!-width-one-third',
-          errors: {
-            'number.base': {
-              message: 'Enter the billable quantity'
-            },
-            'number.min': {
-              message: 'The quantity must be zero or higher'
-            },
-            'number.max': {
-              message: 'The quantity must be the same as or less than the authorised amount'
-            }
-          }
-        })
-      ]
-    }]
-  }));
+  f.fields.push(getQuantityRadio(authorisedAnnualQuantity));
   f.fields.push(fields.hidden('csrf_token', {}, csrfToken));
   f.fields.push(fields.button(null, { label: 'Continue' }));
   return f;

8667b0a54642392fa17de421dca037e6dd777110
James Carmichael <james@siteclick.co.uk>
2020-06-03 17:15:09 +0100

    Add pages to set charge information reason and start date

---
+++ src/internal/modules/charge-information/plugins/charge-information.js
@@ -0,0 +1,34 @@
+const generateChargeVersion = licenceId => ({
+  licenceId,
+  changeReason: null,
+  startDate: null,
+  chargeElements: [],
+  invoiceAccount: null
+});
+
+/**
+ * Creates a cache policy for draft charge information and
+ * specifies default shape
+ * @param {Object} server - hapi server
+ */
+const createDraftChargeInformationPolicy = server => server.cache({
+  expiresIn: 1000 * 60 * 60 * 24 * 365, // 1 year
+  segment: 'draftChargeInformation',
+  generateFunc: generateChargeVersion,
+  generateTimeout: 2000
+});
+
+const chargeInformationPlugin = {
+  register: (server, options) => {
+    const policy = createDraftChargeInformationPolicy(server);
+    server.method('getDraftChargeInformation', licenceId => policy.get(licenceId));
+    server.method('setDraftChargeInformation', (licenceId, data) => policy.set(licenceId, data));
+  },
+
+  pkg: {
+    name: 'chargeInformationPlugin',
+    version: '1.0.0'
+  }
+};
+
+module.exports = chargeInformationPlugin;
---
+++ src/shared/lib/session-forms.js
@@ -0,0 +1,51 @@
+const { get } = require('lodash');
+const uuid = require('uuid/v4');
+const queryString = require('querystring');
+
+/**
+ * Gets a form from the session, or uses the default one if none found
+ * @param {Object} request - hapi request
+ * @param {Object} defaultForm - the default form object to use
+ * @return {Object} form
+ */
+const getSessionForm = (request, defaultForm) => {
+  const sessionForm = request.yar.get(get(request, 'query.form'));
+  if (sessionForm) {
+    request.yar.clear(get(request, 'query.form'));
+    return sessionForm;
+  }
+  return defaultForm;
+};
+
+/**
+ * Sets the form object in the session
+ * @param {Object} request - hapi request
+ * @param {Object} form - the form object to set in the session
+ * @return {String} session key guid
+ */
+const setSessionForm = (request, form) => {
+  const key = uuid();
+  request.yar.set(key, form);
+  return key;
+};
+
+const postRedirectGet = function (form) {
+  const key = setSessionForm(this.request, form);
+  const path = `${form.action}?${queryString.stringify({ form: key })}`;
+  return this.redirect(path);
+};
+
+const postRedirectGetPlugin = {
+  register: (server) => {
+    server.decorate('toolkit', 'postRedirectGet', postRedirectGet);
+  },
+
+  pkg: {
+    name: 'postRedirectGetPlugin',
+    version: '1.0.0'
+  }
+};
+
+exports.get = getSessionForm;
+exports.set = setSessionForm;
+exports.plugin = postRedirectGetPlugin;

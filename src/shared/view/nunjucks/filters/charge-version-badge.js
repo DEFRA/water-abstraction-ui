751bc3c8c2540c7e6e9679b4b040d4ddb6c89a9e
Tim <timtamimi@ip-192-168-68-103.eu-west-2.compute.internal>
2020-08-28 09:25:58 +0100

    Update charge information badges to match the text described in the acceptance criteria

--- src/shared/view/nunjucks/filters/charge-version-badge.js
+++ src/shared/view/nunjucks/filters/charge-version-badge.js
@@ -3,7 +3,7 @@
 const titleCase = require('title-case');
 
 const styles = {
-  current: 'completed',
+  current: 'active',
   draft: 'void',
   approved: 'success',
   replaced: 'inactive',
@@ -12,15 +12,27 @@ const styles = {
   review: 'warning'
 };
 
+//Map the backend statuses to the desired front-end labels from acceptance criteria.
+//Statuses are: 'Draft', 'Review', 'Approved', 'Replaced' - We will also keep invalid to accomodate errors and edge cases. TT 2020-08-28
+const displayedTextTransformer = {
+  current: 'approved',
+  draft: 'draft',
+  approved: 'approved',
+  replaced: 'replaced',
+  superseded: 'replaced',
+  invalid: 'invalid',
+  review: 'review'
+}
+
 /**
  * Gets badge object to render for charge version status
  */
 const chargeVersionBadge = chargeVersion => {
   const { status } = chargeVersion;
-
+  let displayedLabel = displayedTextTransformer[status]
   return {
-    text: titleCase(status),
-    status: styles[status]
+    text: titleCase(displayedLabel),
+    status: styles[displayedLabel]
   };
 };
 

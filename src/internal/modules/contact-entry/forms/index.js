95c5eecd75680d39b3bc1bdd17c32e0b962f99bf
Tim <timtamimi@Tims-MacBook-Pro.local>
2020-09-15 14:33:10 +0100

    Remove unnecessary form

--- src/internal/modules/contact-entry/forms/index.js
+++ src/internal/modules/contact-entry/forms/index.js
@@ -1,6 +1,5 @@
 exports.selectContact = require('./select-contact');
 exports.selectAddress = require('./select-address');
-exports.FAORequired = require('./select-fao-required');
 exports.selectAccountType = require('./select-account-type');
 exports.companySearch = require('./company-search');
 exports.personName = require('./person-name');

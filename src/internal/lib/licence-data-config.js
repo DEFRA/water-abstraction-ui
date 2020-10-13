a1fac11713c17e1038de421edbf286dbc95f7c11
Dana Gajewska <danusia.x@outlook.com>
2020-10-13 08:34:23 +0100

    Adds charge version workflows to charge information tab.
    WATER-2845
    
    Uncomments links to view charge information

--- src/internal/lib/licence-data-config.js
+++ src/internal/lib/licence-data-config.js
@@ -1,8 +1,17 @@
 const services = require('./connectors/services');
 
-exports.getLicenceData = (method, documentId) => {
+exports.getLicenceData = async (method, documentId) => {
   if (method === 'getChargeVersionsByDocumentId') {
-    return services.water.chargeVersions[method](documentId);
+    // temporary work around until the licence page is updated to use the licenceId
+    const { data: licence } = await services.water.licences.getByDocumentId(documentId);
+    const chargeVersionWorkflows = await services.water.chargeVersionWorkflows.getChargeVersionWorkflowsForLicence(licence.id);
+    const { error, data: chargeVersions } = await services.water.chargeVersions[method](documentId);
+    return {
+      error,
+      data: [
+        ...chargeVersionWorkflows.map(workflow => ({ ...workflow, status: 'review' })),
+        ...chargeVersions
+      ] };
   }
   return services.water.licences[method](documentId, { includeExpired: true });
 };

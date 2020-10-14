cda2bebcbe136eb8b6419624bf1a029799bc0986
Tim <timtamimi@Tims-MacBook-Pro.local>
2020-10-14 09:33:26 +0100

    Further initial work on the UI

--- src/internal/modules/charge-information/controllers/charge-information-workflow.js
+++ src/internal/modules/charge-information/controllers/charge-information-workflow.js
@@ -1,7 +1,17 @@
 'use-strict';
 
 const getChargeInformationWorkflow = async (request, h) => {
-  return h.view('nunjucks/charge-information/workflow', request.view);
+  const view = {
+    back: '/manage',
+    ...request.view,
+    pageTitle: 'Charge information workflow',
+    licencesCounts: {
+      toSetUp: 1,
+      review: 2,
+      changeRequest: 3
+    }
+  };
+  return h.view('nunjucks/charge-information/workflow', view);
 };
 
 exports.getChargeInformationWorkflow = getChargeInformationWorkflow;

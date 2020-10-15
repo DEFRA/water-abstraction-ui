c69e75095d2924fa57e211bba1d23c02092d5b4a
Dana Gajewska <danusia.x@outlook.com>
2020-10-15 15:15:54 +0100

    Adds option to includeExpired licences when getting licence by document id

--- src/internal/lib/licence-data-config.js
+++ src/internal/lib/licence-data-config.js
@@ -3,13 +3,13 @@ const services = require('./connectors/services');
 exports.getLicenceData = async (method, documentId) => {
   if (method === 'getChargeVersionsByDocumentId') {
     // temporary work around until the licence page is updated to use the licenceId
-    const { data: licence } = await services.water.licences.getByDocumentId(documentId);
+    const { data: licence } = await services.water.licences.getByDocumentId(documentId, { includeExpired: true });
     const chargeVersionWorkflows = await services.water.chargeVersionWorkflows.getChargeVersionWorkflowsForLicence(licence.id);
     const { error, data: chargeVersions } = await services.water.chargeVersions[method](documentId);
     return {
       error,
       data: [
-        ...chargeVersionWorkflows.map(workflow => ({ ...workflow, status: 'review' })),
+        ...chargeVersionWorkflows,
         ...chargeVersions
       ] };
   }

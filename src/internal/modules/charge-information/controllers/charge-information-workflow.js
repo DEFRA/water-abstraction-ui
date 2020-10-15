54b1a3ae4ed0d7ef1639108e81b495c2a5e29ffb
Tim <timtamimi@ip-192-168-68-106.eu-west-2.compute.internal>
2020-10-15 12:17:57 +0100

    Continued UI work

--- src/internal/modules/charge-information/controllers/charge-information-workflow.js
+++ src/internal/modules/charge-information/controllers/charge-information-workflow.js
@@ -1,9 +1,8 @@
 'use-strict';
 
 const getChargeInformationWorkflow = async (request, h) => {
-  const licencesAwaitingReview = request.pre.inProgress.filter(x => x.status === 'review');
-  const licencesWithChangesRequested = request.pre.inProgress.filter(x => x.status === 'draft');
-
+  const licencesAwaitingReview = request.pre.inProgress.filter(x => x.status === 'draft');
+  const licencesWithChangesRequested = request.pre.inProgress.filter(x => x.status === 'changes_requested');
   const view = {
     back: '/manage',
     ...request.view,

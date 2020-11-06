1815295576579a755dc797ca03b4d591d68e9d4c
James Carmichael <james@siteclick.co.uk>
2020-11-06 17:26:28 +0000

    Ensures most recent completed return cycle is selected in create bill run flow

---
+++ src/internal/modules/billing/lib/batch-financial-year.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const helpers = require('@envage/water-abstraction-helpers');
+const { TWO_PART_TARIFF } = require('./bill-run-types');
+const moment = require('moment');
+
+/**
+ *
+ * @param {String} type - batch type
+ * @param {Boolean} isSummer - true for TPT summer bill run only
+ * @param {String} [refDate] - reference date for testing
+ * @return {Number} financial year ending
+ */
+const getBatchFinancialYearEnding = (type, isSummer, refDate) => {
+  if (type !== TWO_PART_TARIFF) {
+    return helpers.charging.getFinancialYear(refDate);
+  }
+  // Find most recent cycle with matching season where due date has passed
+  const cycles = helpers.returns.date.createReturnCycles().reverse();
+  const cycle = cycles.find(cycle => {
+    const isDueDatePast = moment(cycle.dueDate).isBefore(moment(refDate), 'day');
+    const isSeasonMatch = cycle.isSummer === isSummer;
+    return isDueDatePast && isSeasonMatch;
+  });
+
+  // Get financial year of cycle end date
+  return helpers.charging.getFinancialYear(cycle.endDate);
+};
+
+exports.getBatchFinancialYearEnding = getBatchFinancialYearEnding;

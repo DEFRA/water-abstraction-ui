d777f94606f41fc978488eff871ec192f8e39776
Dana Gajewska <danusia.x@outlook.com>
2020-11-19 12:14:08 +0000

    Update: Make date signed optional in add agreement flow
    WATER-2883
    
    * Updates date signed form
    * Updates reducer to store date signed correctly when being overwritten
    * Removes redundant file with agreement descriptions

--- src/internal/modules/agreements/forms/date-signed.js
+++ src/internal/modules/agreements/forms/date-signed.js
@@ -7,6 +7,18 @@ const { formFactory, fields } = require('shared/lib/forms/');
 const { getCommonErrors, getDateValidator } = require('./lib/date-picker');
 const { getFormAction } = require('./lib/routing');
 
+const getDateErrors = (isDateSignedKnown, endDate) => {
+  const errors = {
+    ...getCommonErrors(endDate)
+  };
+  if (isDateSignedKnown) {
+    errors['any.required'] = {
+      message: 'Enter the date the agreement was signed',
+      summary: 'Enter the date the agreement was signed'
+    };
+  }
+  return errors;
+};
 /**
  * Gets form to select agreement signed date
  */
@@ -15,21 +27,34 @@ const dateSignedForm = request => {
 
   const f = formFactory(getFormAction(request), 'POST');
 
-  const dateSigned = get(request, 'pre.flowState.dateSigned');
+  const { isDateSignedKnown, dateSigned } = get(request, 'pre.flowState');
 
   const { endDate } = request.pre.licence;
 
-  f.fields.push(fields.date('dateSigned', {
-    label: 'Enter date agreement was signed',
+  f.fields.push(fields.radio('isDateSignedKnown', {
+    label: 'Do you know the date the agreement was signed?',
     heading: true,
     size: 'l',
     errors: {
       'any.required': {
-        message: 'Enter the date the agreement was signed'
-      },
-      ...getCommonErrors(endDate)
-    }
-  }, dateSigned));
+        message: 'Select yes if you know the date the agreement was signed'
+      }
+    },
+    choices: [{
+      value: true,
+      label: 'Yes',
+      fields: [
+        fields.date('dateSigned', {
+          type: 'date',
+          mapper: 'dateMapper',
+          controlClass: 'form-control form-control--small',
+          errors: getDateErrors(isDateSignedKnown, endDate)
+        }, dateSigned)]
+    }, {
+      value: false,
+      label: 'No'
+    }]
+  }, isDateSignedKnown));
 
   f.fields.push(fields.hidden('csrf_token', {}, csrfToken));
   f.fields.push(fields.button(null, { label: 'Continue' }));
@@ -41,7 +66,11 @@ const dateSignedSchema = (request, refDate) => {
   const { licence } = request.pre;
   return {
     csrf_token: Joi.string().uuid().required(),
-    dateSigned: getDateValidator(licence)
+    isDateSignedKnown: Joi.boolean().required(),
+    dateSigned: Joi.when('isDateSignedKnown', {
+      is: true,
+      then: getDateValidator(licence)
+    })
   };
 };
 
--- src/internal/modules/agreements/lib/descriptions.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  S127: 'Two-part tariff (S127)',
-  S130S: 'Canal and Rivers Trust, supported source (S130S)',
-  S130U: 'Canal and Rivers Trust, unsupported source (S130S)'
-};

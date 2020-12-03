e31a3b46aab9e3e1a2e062becf615c311b91ab2f
Tim <tim@tamimi.dev>
2020-12-03 08:54:45 +0000

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -51,7 +51,7 @@ exports.config = {
     // grid with only 5 firefox instances available you can make sure that not more than
     // 5 instances get started at a time.
     maxInstances: 5,
-    chromeOptions: {
+    'goog:chromeOptions': {
       args: ['--headless', '--disable-gpu']
     },
     browserName: 'chrome',

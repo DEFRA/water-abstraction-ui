6a3b28ee098fc5b0f0eba6519ed94f9be8dcd3d7
James Carmichael <james@siteclick.co.uk>
2020-12-09 17:29:10 +0000

    Additional tests

--- test/internal/modules/address-entry/controller.js
+++ test/internal/modules/address-entry/controller.js
@@ -19,6 +19,8 @@ const ADDRESS_ID = uuid();
 const UPRN = 1234;
 const CSRF_TOKEN = uuid();
 const REDIRECT_PATH = '/redirect/path';
+const COMPANY_NAME = 'TEST CO LTD';
+const BACK_PATH = '/back';
 
 const ADDRESS = {
   addressLine1: '',
@@ -46,13 +48,23 @@ experiment('src/internal/modules/address-entry/pre-handlers .searchForAddressesB
       pre: {
         sessionData: {
           caption: 'Licence 01/234',
-          back: '/back/path',
+          back: BACK_PATH,
           redirectPath: REDIRECT_PATH
         },
         addressSearchResults: [{
           id: ADDRESS_ID,
           uprn: UPRN
-        }]
+        }],
+        addresses: [{
+          address: {
+            id: ADDRESS_ID,
+            ...ADDRESS
+          }
+        }],
+        company: {
+          name: COMPANY_NAME,
+          address: ADDRESS
+        }
       },
       payload: {},
       query: {},
@@ -225,6 +237,178 @@ experiment('src/internal/modules/address-entry/pre-handlers .searchForAddressesB
 
       test('redirects to the redirect path', async () => {
         expect(h.redirect.calledWith(REDIRECT_PATH)).to.be.true();
+      });
+    });
+  });
+
+  experiment('.getSelectCompanyAddress', () => {
+    experiment('when there are no addresses for the company', async () => {
+      beforeEach(async () => {
+        request.pre.addresses = [];
+        await controller.getSelectCompanyAddress(request, h);
+      });
+
+      test('redirects to postcode page', async () => {
+        expect(h.redirect.calledWith(
+          `/address-entry/${KEY}/postcode`
+        )).to.be.true();
+      });
+    });
+
+    experiment('when there are >0 addresses for the company', async () => {
+      beforeEach(async () => {
+        await controller.getSelectCompanyAddress(request, h);
+      });
+
+      test('the correct template is used', async () => {
+        const [template] = h.view.lastCall.args;
+        expect(template).to.equal('nunjucks/form');
+      });
+
+      test('the correct data is output to the view', async () => {
+        const [, { pageTitle, caption, back, form }] = h.view.lastCall.args;
+        expect(pageTitle).to.equal(`Select an existing address for ${COMPANY_NAME}`);
+        expect(caption).to.equal(request.pre.sessionData.caption);
+        expect(back).to.equal(request.pre.sessionData.back);
+        expect(form).to.be.an.object();
+      });
+    });
+  });
+
+  experiment('.postSelectCompanyAddress', () => {
+    beforeEach(async () => {
+      request.method = 'post';
+    });
+
+    experiment('when the form is invalid', async () => {
+      beforeEach(async () => {
+        await controller.postSelectCompanyAddress(request, h);
+      });
+
+      test('the user is redirected to the get page with errors', async () => {
+        expect(h.postRedirectGet.called).to.be.true();
+      });
+    });
+
+    experiment('when the selected address is an existing address', async () => {
+      beforeEach(async () => {
+        request.payload = {
+          selectedAddress: ADDRESS_ID,
+          csrf_token: CSRF_TOKEN
+        };
+        await controller.postSelectCompanyAddress(request, h);
+      });
+
+      test('the address is stored in the session with empty strings converted to null', async () => {
+        expect(session.merge.calledWith(
+          request, KEY, {
+            data: {
+              id: ADDRESS_ID,
+              ...ADDRESS
+            }
+          }
+        )).to.be.true();
+      });
+
+      test('redirects to the redirect path', async () => {
+        expect(h.redirect.calledWith(REDIRECT_PATH)).to.be.true();
+      });
+    });
+
+    experiment('when the option to enter a new address is selected', async () => {
+      beforeEach(async () => {
+        request.payload = {
+          selectedAddress: 'new_address',
+          csrf_token: CSRF_TOKEN
+        };
+        await controller.postSelectCompanyAddress(request, h);
+      });
+
+      test('the address is not stored', async () => {
+        expect(session.merge.called).to.be.false();
+      });
+
+      test('redirects to the postcode page', async () => {
+        expect(h.redirect.calledWith(
+          `/address-entry/${KEY}/postcode`
+        )).to.be.true();
+      });
+    });
+  });
+
+  experiment('.getUseRegisteredAddress', () => {
+    beforeEach(async () => {
+      await controller.getUseRegisteredAddress(request, h);
+    });
+
+    test('the correct template is used', async () => {
+      const [template] = h.view.lastCall.args;
+      expect(template).to.equal('nunjucks/address-entry/use-registered-address');
+    });
+
+    test('the correct data is output to the view', async () => {
+      const [, { pageTitle, caption, back, form }] = h.view.lastCall.args;
+      expect(pageTitle).to.equal(`Registered office address`);
+      expect(caption).to.equal(request.pre.sessionData.caption);
+      expect(back).to.equal(request.pre.sessionData.back);
+      expect(form).to.be.an.object();
+    });
+  });
+
+  experiment('.postUseRegisteredAddress', () => {
+    beforeEach(async () => {
+      request.method = 'post';
+    });
+
+    experiment('when the form is invalid', async () => {
+      beforeEach(async () => {
+        await controller.postUseRegisteredAddress(request, h);
+      });
+
+      test('the user is redirected to the get page with errors', async () => {
+        expect(h.postRedirectGet.called).to.be.true();
+      });
+    });
+
+    experiment('when the user selects "yes"', async () => {
+      beforeEach(async () => {
+        request.payload = {
+          useRegisteredAddress: 'true',
+          csrf_token: CSRF_TOKEN
+        };
+        await controller.postUseRegisteredAddress(request, h);
+      });
+
+      test('the address is stored in the session with empty strings converted to null', async () => {
+        expect(session.merge.calledWith(
+          request, KEY, {
+            data: ADDRESS
+          }
+        )).to.be.true();
+      });
+
+      test('redirects to the redirect path', async () => {
+        expect(h.redirect.calledWith(REDIRECT_PATH)).to.be.true();
+      });
+    });
+
+    experiment('when the user selects "no"', async () => {
+      beforeEach(async () => {
+        request.payload = {
+          useRegisteredAddress: 'false',
+          csrf_token: CSRF_TOKEN
+        };
+        await controller.postUseRegisteredAddress(request, h);
+      });
+
+      test('the address is not stored in the session', async () => {
+        expect(session.merge.called).to.be.false();
+      });
+
+      test('redirects to the postcode page', async () => {
+        expect(h.redirect.calledWith(
+          `/address-entry/${KEY}/postcode`
+        )).to.be.true();
       });
     });
   });

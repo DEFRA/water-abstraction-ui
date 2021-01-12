d1156f4a5c2b0dbe1b32c1230f97318366f5aadc
James Carmichael <james@siteclick.co.uk>
2021-01-12 15:15:10 +0000

    Additional tests

--- test/internal/modules/billing-accounts/controllers/select-billing-account.js
+++ test/internal/modules/billing-accounts/controllers/select-billing-account.js
@@ -9,6 +9,7 @@ const {
 } = exports.lab = require('@hapi/lab').script();
 const sandbox = require('sinon').createSandbox();
 const uuid = require('uuid/v4');
+const { get } = require('lodash');
 
 const controller = require('internal/modules/billing-accounts/controllers/select-billing-account');
 const session = require('internal/modules/billing-accounts/lib/session');
@@ -20,12 +21,16 @@ const CSRF_TOKEN = uuid();
 const REDIRECT_PATH = '/redirect/path';
 const BACK_PATH = '/back';
 const CAPTION = 'Licence 01/234';
+const ACCOUNT_ENTRY_PATH = '/account-entry-redirect-path';
+const ADDRESS_ENTRY_PATH = '/address-entry-redirect-path';
+const COMPANY_ID = uuid();
 
 const data = {
   billingAccounts: [{
     id: uuid(),
     accountNumber: 'A1234B',
     company: {
+      id: COMPANY_ID,
       name: 'Test Co Ltd'
     },
     invoiceAccountAddresses: [{
@@ -46,7 +51,14 @@ const data = {
     }]
   }],
   account: {
+    id: COMPANY_ID,
     name: 'Test Co Ltd'
+  },
+  sessionData: {
+    caption: CAPTION,
+    back: BACK_PATH,
+    redirectPath: REDIRECT_PATH,
+    data: {}
   }
 };
 
@@ -67,14 +79,13 @@ const createRequest = (overrides = {}) => ({
     clear: sandbox.stub()
   },
   pre: {
-    sessionData: {
-      caption: CAPTION,
-      back: BACK_PATH,
-      redirectPath: REDIRECT_PATH
-    },
+    sessionData: data.sessionData,
     billingAccounts: overrides.billingAccounts || data.billingAccounts,
     account: data.account
-  }
+  },
+  accountEntryRedirect: sandbox.stub().returns(ACCOUNT_ENTRY_PATH),
+  addressLookupRedirect: sandbox.stub().returns(ADDRESS_ENTRY_PATH)
+
 });
 
 const createPostRequest = (overrides = {}) => createRequest({
@@ -95,6 +106,7 @@ experiment('internal/modules/billing-accounts/controllers/select-billing-account
     sandbox.stub(session, 'merge').returns({
       redirectPath: REDIRECT_PATH
     });
+    sandbox.stub(session, 'get').returns(data.sessionData);
     sandbox.stub(session, 'setProperty');
   });
 
@@ -236,6 +248,163 @@ experiment('internal/modules/billing-accounts/controllers/select-billing-account
         expect(h.redirect.calledWith(
           REDIRECT_PATH
         )).to.be.true();
+      });
+    });
+  });
+
+  experiment('.getSelectAccount', () => {
+    beforeEach(async () => {
+      request = createRequest();
+      await controller.getSelectAccount(request, h);
+    });
+
+    test('the page uses the correct template', async () => {
+      const [template] = h.view.lastCall.args;
+      expect(template).to.equal('nunjucks/form');
+    });
+
+    test('the page has the correct title', async () => {
+      const [, { pageTitle }] = h.view.lastCall.args;
+      expect(pageTitle).to.equal('Who should the bills go to?');
+    });
+
+    test('the page has the correct caption', async () => {
+      const [, { caption }] = h.view.lastCall.args;
+      expect(caption).to.equal(CAPTION);
+    });
+
+    test('a form object is output to the view', async () => {
+      const [, { form }] = h.view.lastCall.args;
+      expect(form).to.be.an.object();
+    });
+
+    test('the form has a CSRF token field', async () => {
+      const [, { form }] = h.view.lastCall.args;
+      const field = formTest.findField(form, 'csrf_token');
+      expect(field.value).to.equal(CSRF_TOKEN);
+    });
+
+    test('the form has radio options for licence holder or agent account', async () => {
+      const [, { form }] = h.view.lastCall.args;
+      const field = formTest.findField(form, 'account');
+
+      expect(field.options.widget).to.equal('radio');
+      expect(field.options.choices.length).to.equal(2);
+
+      expect(field.options.choices[0].label).to.equal('Test Co Ltd');
+      expect(field.options.choices[0].value).to.equal(constants.BILLING_ACCOUNT_HOLDER);
+
+      expect(field.options.choices[1].label).to.equal('Another billing contact');
+      expect(field.options.choices[1].value).to.equal(constants.OTHER_ACCOUNT);
+    });
+
+    test('the "other billing" account option has a sub-field for the account name', async () => {
+      const [, { form }] = h.view.lastCall.args;
+      const field = formTest.findField(form, 'account');
+      const subField = get(field, 'options.choices[1].fields[0]');
+      expect(subField.name).to.equal('accountSearch');
+    });
+
+    test('the form has a continue button', async () => {
+      const [, { form }] = h.view.lastCall.args;
+      const button = formTest.findButton(form);
+      expect(button.options.label).to.equal('Continue');
+    });
+  });
+
+  experiment('.postSelectAccount', () => {
+    experiment('when the form has validation errors', async () => {
+      beforeEach(async () => {
+        request = createPostRequest({
+          payload: {
+            csrf_token: CSRF_TOKEN
+          }
+        });
+        await controller.postSelectAccount(request, h);
+      });
+
+      test('the user is redirected to the form with errors displayed', async () => {
+        expect(h.postRedirectGet.called).to.be.true();
+      });
+    });
+
+    experiment('when the billing account holder is selected', () => {
+      beforeEach(async () => {
+        request = createPostRequest({
+          payload: {
+            csrf_token: CSRF_TOKEN,
+            account: constants.BILLING_ACCOUNT_HOLDER
+          }
+        });
+        session.get.returns({
+          ...data.sessionData,
+          data: {
+            company: {
+              id: COMPANY_ID
+            }
+          }
+        });
+        await controller.postSelectAccount(request, h);
+      });
+
+      test('the agent company is set to null in the session', async () => {
+        expect(session.setProperty.calledWith(
+          request, KEY, 'data.agentCompany', null
+        )).to.be.true();
+      });
+
+      test('the address plugin method is called with the correct arguments', async () => {
+        const [options] = request.addressLookupRedirect.lastCall.args;
+        expect(options).to.equal({
+          caption: CAPTION,
+          key: KEY,
+          back: `/billing-account-entry/${KEY}/select-account`,
+          redirectPath: `/billing-account-entry/${KEY}/address-entry`,
+          companyId: COMPANY_ID,
+          companyNumber: undefined
+        });
+      });
+
+      test('the user is redirected to the address plugin flow', async () => {
+        expect(h.redirect.calledWith(ADDRESS_ENTRY_PATH)).to.be.true();
+      });
+    });
+
+    experiment('when an agent account is selected', () => {
+      const SEARCH_QUERY = 'Test query';
+
+      beforeEach(async () => {
+        request = createPostRequest({
+          payload: {
+            csrf_token: CSRF_TOKEN,
+            account: constants.OTHER_ACCOUNT,
+            accountSearch: SEARCH_QUERY
+          }
+        });
+        session.get.returns({
+          ...data.sessionData,
+          data: {
+            company: {
+              id: COMPANY_ID
+            }
+          }
+        });
+        await controller.postSelectAccount(request, h);
+      });
+
+      test('a account entry plugin method is called with the correct arguments', async () => {
+        const [options] = request.accountEntryRedirect.lastCall.args;
+        expect(options).to.equal({
+          back: `/billing-account-entry/${KEY}/select-account`,
+          redirectPath: `/billing-account-entry/${KEY}/account-entry`,
+          caption: CAPTION,
+          searchQuery: SEARCH_QUERY,
+          key: KEY
+        });
+      });
+
+      test('the user is redirected to the account entry flow', async () => {
+        expect(h.redirect.calledWith(ACCOUNT_ENTRY_PATH)).to.be.true();
       });
     });
   });

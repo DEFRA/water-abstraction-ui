1e563a8c1040981e28bf4cb4c182edf5d172512f
Tim <tim@tamimi.dev>
2021-02-05 15:35:19 +0000

    Update notify-callback.spec.js

--- test/regression/external/notify-callback.spec.js
+++ test/regression/external/notify-callback.spec.js
@@ -4,19 +4,21 @@ const config = require('./config');
 
 /* eslint-disable no-undef */
 describe('notify callback attempt', function () {
-  let scheduledNotification, scheduledNotificationAfterCallback, scheduledNotificationId;
+  let scheduledNotification, scheduledNotificationAfterCallback, scheduledNotificationNotifyId;
   before(async () => {
+    await sleepFor(1000);
     scheduledNotification = await getLastNotifications(config.baseUrl, config.userEmails.notifyCallbackTestEmail);
-    scheduledNotificationId = JSON.parse(scheduledNotification).data[0].id;
+    console.log(scheduledNotification);
+    scheduledNotificationNotifyId = JSON.parse(scheduledNotification).data[0].notify_id;
   });
 
   it('checks if the notification was found', () => {
-    expect(scheduledNotificationId.length).toBe(36);
+    expect(scheduledNotificationNotifyId.length).toBe(36);
   });
 
   it('calls the notify callback endpoint', async () => {
-    await simulateNotifyCallback(scheduledNotificationId);
-    await sleepFor(2000);
+    await simulateNotifyCallback(scheduledNotificationNotifyId);
+    await sleepFor(1000);
     scheduledNotificationAfterCallback = await getLastNotifications(config.baseUrl, config.userEmails.notifyCallbackTestEmail);
     expect(JSON.parse(scheduledNotificationAfterCallback).data[0].notify_status).toBe('delivered');
   });

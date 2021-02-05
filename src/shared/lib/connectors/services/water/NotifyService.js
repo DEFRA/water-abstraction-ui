fe83ab9862c9a26f8252f9f7bddda32d5af7a970
Tim <tim@tamimi.dev>
2021-02-05 15:04:03 +0000

    Remove unnecessary semicolon

--- src/shared/lib/connectors/services/water/NotifyService.js
+++ src/shared/lib/connectors/services/water/NotifyService.js
@@ -4,7 +4,7 @@ class NotifyService extends ServiceClient {
   postNotifyCallback (payload) {
     const url = this.joinUrl('notify', 'callback');
     return this.serviceRequest.post(url, { body: payload });
-  };
+  }
 }
 
 module.exports = NotifyService;

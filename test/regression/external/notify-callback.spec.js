19c915e0c3d1205f799249f58e4dedbdcec3c321
Tim <tim@tamimi.dev>
2021-02-16 21:38:29 +0000

    WIP

--- test/regression/external/notify-callback.spec.js
+++ test/regression/external/notify-callback.spec.js
@@ -1,11 +1,13 @@
 const { getLastNotifications, simulateNotifyCallback } = require('../shared/helpers/notifications');
 const { sleepFor } = require('../shared/helpers/utils');
 const config = require('./config');
+const { setUp } = require('../shared/helpers/setup');
 
 /* eslint-disable no-undef */
 describe('notify callback attempt', function () {
   let scheduledNotification, scheduledNotificationAfterCallback, scheduledNotificationNotifyId;
   before(async () => {
+    await setUp('notify-mock-notification');
     await sleepFor(1000);
     scheduledNotification = await getLastNotifications(config.baseUrl, config.userEmails.notifyCallbackTestEmail);
     scheduledNotificationNotifyId = scheduledNotification.notify_id;

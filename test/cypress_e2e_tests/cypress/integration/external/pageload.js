582861db371ff7a36f1a1a1673ee05f98777b039
Srujan Chada <srujan.chada@gmail.com>
2021-04-06 11:27:06 +0100

    Cypress
    Cypress features and folder structure with initial tests

--- test/cypress_e2e_tests/cypress/integration/external/pageload.js
+++ test/cypress_e2e_tests/cypress/integration/external/pageload.js
@@ -1,12 +1,9 @@
-/// <reference types="cypress" />
+describe('Page loading', () => {
+  it('the login page loads', () => {
+    //  cy.visit ('http://localhost:8008/signin')
+    cy.visit('');
 
-describe('Page loading',() => {
-it('the login page loads', () => { 
-//cy.visit ('http://localhost:8008/signin')
-cy.visit('')
-
-//finding the site title
-cy.contains('Manage your water abstraction or impoundment licence').should('have.attr','href','/')
-})
-
-})
+    //  finding the site title
+    cy.contains('Manage your water abstraction or impoundment licence').should('have.attr', 'href', '/');
+  });
+});
--- test/cypress_e2e_tests/cypress/integration/external/searchforlicense.js
+++ test/cypress_e2e_tests/cypress/integration/external/searchforlicense.js
@@ -1,26 +1,22 @@
-describe('User Login',() => {
-    it('Try to login', () => { 
-//cy.visit to visit the URL 
-cy.visit('')
+describe('User Login', () => {
+  it('Try to login', () => {
+    // cy.visit to visit the URL
+    cy.visit('');
 
-//Enter the user name and Password 
-cy.get('#email').type(Cypress.env('Uname'))
-cy.get('#password').type(Cypress.env('Pwd'))
+    // Enter the user name and Password
+    cy.get('#email').type(Cypress.env('Uname'));
+    cy.get('#password').type(Cypress.env('Pwd'));
 
-//Click Sign in Button 
-cy.get ('.govuk-button.govuk-button--start').click()  
+    // Click Sign in Button
+    cy.get('.govuk-button.govuk-button--start').click();
 
-//assert once the user is signed in
-cy.contains ('Licences, users and returns')
+    // assert once the user is signed in
+    cy.contains('Licences, users and returns');
 
-//search for a license 
-cy.get ('#query').type('Anglian').should('be.visible')
+    // search for a license
+    cy.get('#query').type('Anglian').should('be.visible');
 
-cy.get ('.search__button').click()
-cy.contains ('Licences')
-
- 
-
-})
-
-})
+    cy.get('.search__button').click();
+    cy.contains('Licences');
+  });
+});

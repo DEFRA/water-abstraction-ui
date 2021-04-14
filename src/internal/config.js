7be2e6bd321dbe3074a862cb2078532f2d782c49
James Carmichael <james@siteclick.co.uk>
2021-04-14 12:22:09 +0100

    Update cookie content

--- src/internal/config.js
+++ src/internal/config.js
@@ -53,7 +53,7 @@ module.exports = {
     password: process.env.COOKIE_SECRET,
     isSecure: !isLocal,
     isSameSite: 'Lax',
-    ttl: 24 * 60 * 60 * 1000, // Set session to 1 day,
+    ttl: 2 * 60 * 60 * 1000, // Set session to 2 hours,
     redirectTo: '/signin',
     isHttpOnly: true,
     keepAlive: true // ttl restarts after each request

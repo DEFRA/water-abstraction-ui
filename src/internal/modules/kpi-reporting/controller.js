16dfb7eab2af9d3202fbde5dd98a9987ca61eb00
James Carmichael <james@siteclick.co.uk>
2021-04-19 18:28:15 +0100

    Fix tests, content tweak

--- src/internal/modules/kpi-reporting/controller.js
+++ src/internal/modules/kpi-reporting/controller.js
@@ -1,14 +1,20 @@
 'use-strict';
 
+const { sortBy } = require('lodash');
 const services = require('internal/lib/connectors/services');
 const returnCycleMapper = require('./mappers/return-cycle');
 
 const getKPIDashboard = async (request, h) => {
   const { data } = await services.water.kpiReporting.getKpiData();
+
+  const returnCycles = sortBy(data.returnCycles, cycle => cycle.startDate)
+    .reverse()
+    .map(returnCycleMapper.mapReturnCycle);
+
   const viewContext = {
     ...request.view,
     ...data,
-    returnCycles: data.returnsCycles.map(returnCycleMapper.mapReturnCycle).reverse()
+    returnCycles
   };
   return h.view('nunjucks/kpi-reporting/dashboard', viewContext);
 };

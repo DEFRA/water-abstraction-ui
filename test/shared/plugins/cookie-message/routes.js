c5d23382d09a065ae50a3bd3704a564e56162bce
James Carmichael <james@siteclick.co.uk>
2021-04-29 12:14:50 +0100

    Refactor tests

--- test/shared/plugins/cookie-message/routes.js
+++ test/shared/plugins/cookie-message/routes.js
@@ -9,18 +9,7 @@ const { createServer, createRouteWithNoOpHandler } = require('../../../lib/serve
 experiment('internal/shared/plugins/cookie-message/routes', () => {
   let request, server;
 
-  experiment(`get /cookies`, () => {
-    beforeEach(async () => {
-      request = {
-        method: 'get',
-        url: 'http://localhost/cookies'
-      };
-
-      server = await createServer(
-        createRouteWithNoOpHandler(routes.getCookies)
-      );
-    });
-
+  const testPaths = () => {
     test('200 response for a valid redirect path', async () => {
       request.url += '?redirectPath=/login';
       const res = await server.inject(request);
@@ -50,6 +39,21 @@ experiment('internal/shared/plugins/cookie-message/routes', () => {
       const res = await server.inject(request);
       expect(res.statusCode).to.equal(400);
     });
+  };
+
+  experiment(`get /cookies`, () => {
+    beforeEach(async () => {
+      request = {
+        method: 'get',
+        url: 'http://localhost/cookies'
+      };
+
+      server = await createServer(
+        createRouteWithNoOpHandler(routes.getCookies)
+      );
+    });
+
+    testPaths();
   });
 
   experiment(`post /cookies`, () => {
@@ -64,35 +68,7 @@ experiment('internal/shared/plugins/cookie-message/routes', () => {
       );
     });
 
-    test('200 response for a valid redirect path', async () => {
-      request.url += '?redirectPath=/login';
-      const res = await server.inject(request);
-      expect(res.statusCode).to.equal(200);
-    });
-
-    test('400 response for a path not beginning with /', async () => {
-      request.url += '?redirectPath=login';
-      const res = await server.inject(request);
-      expect(res.statusCode).to.equal(400);
-    });
-
-    test('200 response for a valid redirect path with query params', async () => {
-      request.url += '?redirectPath=/login%3Dpage=2';
-      const res = await server.inject(request);
-      expect(res.statusCode).to.equal(200);
-    });
-
-    test('400 response for a non-relative path', async () => {
-      request.url += '?redirectPath=https://example.com';
-      const res = await server.inject(request);
-      expect(res.statusCode).to.equal(400);
-    });
-
-    test('400 response for a path with no protocol', async () => {
-      request.url += '?redirectPath=//example.com';
-      const res = await server.inject(request);
-      expect(res.statusCode).to.equal(400);
-    });
+    testPaths();
   });
 
   experiment(`get /set-cookie-preferences`, () => {
@@ -107,34 +83,6 @@ experiment('internal/shared/plugins/cookie-message/routes', () => {
       );
     });
 
-    test('200 response for a valid redirect path', async () => {
-      request.url += '?redirectPath=/login';
-      const res = await server.inject(request);
-      expect(res.statusCode).to.equal(200);
-    });
-
-    test('400 response for a path not beginning with /', async () => {
-      request.url += '?redirectPath=login';
-      const res = await server.inject(request);
-      expect(res.statusCode).to.equal(400);
-    });
-
-    test('200 response for a valid redirect path with query params', async () => {
-      request.url += '?redirectPath=/login%3Dpage=2';
-      const res = await server.inject(request);
-      expect(res.statusCode).to.equal(200);
-    });
-
-    test('400 response for a non-relative path', async () => {
-      request.url += '?redirectPath=https://example.com';
-      const res = await server.inject(request);
-      expect(res.statusCode).to.equal(400);
-    });
-
-    test('400 response for a path with no protocol', async () => {
-      request.url += '?redirectPath=//example.com';
-      const res = await server.inject(request);
-      expect(res.statusCode).to.equal(400);
-    });
+    testPaths();
   });
 });

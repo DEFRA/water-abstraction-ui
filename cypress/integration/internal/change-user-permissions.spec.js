e8cc4c2d1a130b119b74b392276b50d057c6f313
Srujan Chada <srujan.chada@gmail.com>
2021-04-30 13:51:47 +0100

    user permissions test

--- cypress/integration/internal/change-user-permissions.spec.js
+++ cypress/integration/internal/change-user-permissions.spec.js
@@ -19,27 +19,23 @@ describe('change internal user permissions as B&D user', () => {
       cy.get('.govuk-button.govuk-button--start').click();
       cy.get('#query').type(EMAIL_ADDRESS);
       cy.get('.search__button').click();
-      // cy.get('h2.govuk-heading-m').should('have.text', 'Users');
       cy.get('.govuk-grid-column-full > .govuk-heading-m').should('have.text', 'Users');
       cy.get('.govuk-list > li').should('contain.text', EMAIL_ADDRESS);
       cy.get('.govuk-list .govuk-link').click();
-    });
-  });
 
-  it('navigates to the user page', () => {
-    cy.url().should('include', '/status');
-    cy.contains('Internal').should('be.visible');
-    cy.get('.govuk-heading-l').eq(1).should('have.text', 'Set permissions');
-    // cy.get('.govuk-grid-column-two-thirds > .govuk-heading-l').should('have.text', EMAIL_ADDRESS);
-    describe('changes the user permissions and navigates to the success page', () => {
-      cy.get('#permission-5').check();
-      cy.get('form > .govuk-button').click();
-      cy.url().should('include', '/update-permissions/success');
-      // cy.getPageTitle().should('have.text', 'Account permissions are updated');
-    });
-    describe('user page shows updated permissions', () => {
-      // cy.getBackLink().click();
-      cy.get('.govuk-radios__input[checked=""] + label').should('have.text', 'National Permitting Service');
+      describe('navigates to the user page', () => {
+        cy.url().should('include', '/status');
+        cy.contains('Internal').should('be.visible');
+        cy.get('.govuk-heading-l').eq(0).should('contain', EMAIL_ADDRESS);
+        cy.get('.govuk-heading-l').eq(1).should('have.text', 'Set permissions');
+      });
+      describe('changes the user permissions and navigates to the success page', () => {
+        cy.get('#permission-4').check();
+        cy.get('form > .govuk-button').click();
+        cy.url().should('include', '/update-permissions/success');
+        cy.get('.govuk-heading-l').should('contain.text', 'Account permissions are updated');
+        cy.get('p.govuk-body').eq(1).should('contain', 'National Permitting Service permissions');
+      });
     });
   });
 });

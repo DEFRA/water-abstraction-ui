26e06cb1d440b849890c0b17b311152fd3b26f30
Srujan Chada <srujan.chada@gmail.com>
2021-05-05 10:52:51 +0100

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,7 +2,6 @@ const { get } = require('lodash');
 const querystring = require('querystring');
 const axios = require('axios');
 
-
 Cypress.Commands.add('getPasswordResetUrl', async (baseUrl, email) => {
   const url = `${baseUrl}notifications/last?${querystring.encode({ email })}`;
   const req = await axios.get(url);
@@ -16,5 +15,3 @@ Cypress.Commands.add('getUserRegistrationUrl', async (baseUrl, email) => {
   const personalisation = get(req, `data.data[0].personalisation['link']`, null);
   return personalisation.replace((/^https?:\/\/[^/]+\//g).exec(personalisation), baseUrl);
 });
-
-

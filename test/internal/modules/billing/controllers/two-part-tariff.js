a3450a0caea51c44e11bbb34cdf315142c63b508
James Carmichael <james@siteclick.co.uk>
2021-05-08 10:08:45 +0100

    Include charge period in TPT review screens

--- test/internal/modules/billing/controllers/two-part-tariff.js
+++ test/internal/modules/billing/controllers/two-part-tariff.js
@@ -581,11 +581,6 @@ experiment('internal/modules/billing/controller/two-part-tariff', () => {
         expect(returnsLink).to.equal('/licences/test-document_id/returns');
       });
 
-      test('sets the return cycle in the view', async () => {
-        const [, { returnCycle }] = h.view.lastCall.args;
-        expect(returnCycle).to.equal('Winter and all year 2022');
-      });
-
       experiment('view.aggregateConditions', () => {
         let aggregateConditions;
         beforeEach(async () => {
@@ -837,11 +832,6 @@ experiment('internal/modules/billing/controller/two-part-tariff', () => {
     test('outputs the quantity to the view', async () => {
       const [, { quantity }] = h.view.lastCall.args;
       expect(quantity).to.equal(request.query.quantity);
-    });
-
-    test('sets the return cycle in the view', async () => {
-      const [, { returnCycle }] = h.view.lastCall.args;
-      expect(returnCycle).to.equal('Winter and all year 2022');
     });
 
     test('outputs the licence to the view', async () => {

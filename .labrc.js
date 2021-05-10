be76cf81f7202bd6725eb8e10fe5a854485181de
Srujan Chada <srujan.chada@gmail.com>
2021-05-10 17:59:02 +0100

    Update .labrc.js

--- .labrc.js
+++ .labrc.js
@@ -6,7 +6,7 @@ require('app-module-path').addPath(require('path').join(__dirname, 'src/'));
 // This is overridden if arguments are passed to lab via the command line.
 module.exports = {
   // This version global seems to be introduced by sinon.
-  globals: 'version,fetch,Response,Headers,Request',
+  globals: 'version,fetch,Response,Headers,Request,global-symbol-property',
   paths: ['test/internal', 'test/external', 'test/shared'],
   'coverage-exclude': [
     'data',

36ca09c6c20f9ee94174619b2a909d7d8d0b3124
Srujan Chada <srujan.chada@gmail.com>
2021-05-11 15:10:12 +0100

    Create supplementary.bills.run.spec.js

---
+++ cypress/integration/internal/supplementary.bills.run.spec.js
@@ -0,0 +1,34 @@
+const { setUp, tearDown } = require('../../support/setup');
+const LICENCE_NUMBER = 'AT/CURR/DAILY/01';
+
+describe('supplementary bill run', () => {
+  before(() => {
+    tearDown();
+    setUp('billing-data');
+  });
+
+  after(() => {
+    tearDown();
+  });
+
+  it('user logs in', () => {
+    // cy.visit to visit the URL
+    cy.visit(Cypress.env('ADMIN_URI'));
+    // Enter the user name and Password
+    cy.fixture('users.json').then(users => {
+      cy.get('input#email').type(users.billingAndData);
+      cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
+      cy.get('.govuk-button.govuk-button--start').click();
+      // assert once the user is signed in
+      cy.contains('Licences, users and returns');
+      
+// user clicks on manage link to set up the supplementary bill run
+        describe('user clicks on Manage link', () => {
+            cy.get('#navbar-notifications').click();
+            cy.get('.govuk-link').eq(12).contains('Create a bill run').click();
+          });
+        
+        });
+    });
+});
+

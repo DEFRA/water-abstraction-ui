77cb64acea4d24f24cb6f347dec74e103abdd798
Srujan Chada <srujan.chada@gmail.com>
2021-05-17 19:20:03 +0100

    Update annual-bills-run.spec.js

--- cypress/integration/internal/annual-bills-run.spec.js
+++ cypress/integration/internal/annual-bills-run.spec.js
@@ -1,16 +1,16 @@
 const { setUp, tearDown } = require('../../support/setup');
 
 describe('annual bill run', () => {
-  before(() => {
+  beforeEach(() => {
     tearDown();
     setUp('annual-billing');
   });
 
-  after(() => {
+  afterEach(() => {
     tearDown();
   });
 
-  it('user logs in', () => {
+  it('user logs in and generates annual bill', () => {
     // cy.visit to visit the URL
     cy.visit(Cypress.env('ADMIN_URI'));
     // Enter the user name and Password
@@ -26,7 +26,7 @@ describe('annual bill run', () => {
       });
 
       describe('user enters the create a new bill flow', () => {
-        cy.get('.govuk-link').eq(12).contains('Create a bill run').click();
+        cy.get('.govuk-link').contains('Create a bill run').click();
       });
 
       describe('user selects annual billing type', () => {
@@ -44,8 +44,8 @@ describe('annual bill run', () => {
         cy.get('.pagination__link').click();
         cy.get('.pagination__link').click();
         cy.get('#main-content > a.govuk-button').click();
-      }); 
-      
+      });
+
       describe('user generates the annual bill ', () => {
         cy.get('#selectedBillingType').click();
         cy.get('form > .govuk-button').click();
@@ -53,47 +53,41 @@ describe('annual bill run', () => {
         cy.get('button.govuk-button').click();
         cy.get('.govuk-heading-xl', { timeout: 20000 }).contains('Annual bill run');
         cy.url().should('contain', '/summary');
+        cy.get('#tab_other-abstractors').click();
         cy.get('div.govuk-grid-column-two-thirds').eq(3).children(0).contains('Confirm bill run').click();
         cy.get('.govuk-heading-l').should('contain', 'You are about to send this bill run');
         cy.get('form > .govuk-button').click();
         cy.get('.govuk-panel__title', { timeout: 20000 }).contains('Bill run sent');
         cy.get('.govuk-grid-column-two-thirds > :nth-child(4) > a').click();
         cy.get('.govuk-heading-xl').contains('Annual bill run');
-      }); 
+      });
+    });
+  });
 
-      describe('user verifies the annual bill ', () => {
-        cy.get('#navbar-notificationsType').click();
-        //cy.get('form > .govuk-button').click();
-        //cy.get('.govuk-radios__item').last().children().first().click();
-        //cy.get('button.govuk-button').click();
-        //cy.get('.govuk-heading-xl', { timeout: 20000 }).contains('Annual bill run');
-        //cy.url().should('contain', '/summary');
-        //cy.get('div.govuk-grid-column-two-thirds').eq(3).children(0).contains('Confirm bill run').click();
-  
-      }); 
-
-
-      /*describe('user verifys the generated bill', () => {
-        cy.get('.govuk-grid-column-two-thirds > :nth-child(4) > a').click();
-        cy.get('.govuk-heading-xl').contatins('Annual bill run');
-        //cy.get('.govuk-body > .govuk-tag').contains('SENT');
-      });*/
-
-      /* describe('user confirms the bill', () => {
-        cy.get('div.govuk-grid-column-two-thirds').eq(3).children(0).contains('Confirm bill run').click();
-      }); */
-
-      /* describe('send the bill run', () => {
-        cy.get('.govuk-heading-l').contains('You are about to send this bill run');
-        cy.get('button.govuk-button').contains('Send bill run').click();
-      }); */
-
-      /* describe('verify the bill run is sent successfully', () => {
-        cy.get('.govuk-heading-l', { timeout: 40000 }).contains('supplementary bill run');
-        cy.get('.govuk-panel__title', { timeout: 40000 }).contains('Bill run sent');
-        cy.url().should('contain', '/confirm');
-        cy.get('div.govuk-grid-column-two-thirds').eq(1).contains('Download the bill run');
-      }); */
+  it('user verifies the data in other abstractors', () => {
+    cy.visit(Cypress.env('ADMIN_URI'));
+    // Enter the user name and Password
+    cy.fixture('users.json').then(users => {
+      cy.get('input#email').type(users.billingAndData);
+      cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
+      cy.get('.govuk-button.govuk-button--start').click();
+      cy.get('#navbar-notifications').click();
+      cy.get('.govuk-link').contains('Create a bill run').click();
+      cy.get('#selectedBillingType').click();
+      cy.get('button.govuk-button').click();
+      cy.get('.govuk-radios__item').last().children().first().click();
+      cy.get('button.govuk-button').click();
+      cy.get('.govuk-heading-xl', { timeout: 20000 }).contains('Annual bill run');
+      cy.get('#tab_other-abstractors').click();
+      // verify the data
+      cy.get('.govuk-table__row').eq(1).should('contain', 'A99999999A');
+      cy.get('.govuk-table__row').eq(1).should('contain', 'Big Farm Co Ltd');
+      // cy.get('.govuk-link').contains('view').click();
+      cy.get('td.govuk-table__cell').last().children(0).click({ force: true });
+      describe('user verifys the generated bill', () => {
+        cy.get('.govuk-caption-l').should('contain', 'Billing account A99999999A');
+        cy.get('.govuk-grid-column-full > :nth-child(4) > a').click();
+      });
     });
   });
 });

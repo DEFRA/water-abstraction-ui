87164e205f85c2e0894f91c4c57702748dbca361
Axel Sunnehall <axels@dashup.co>
2021-05-26 15:44:15 +0100

    increase testcoverage for nunjucks filter html-decode

--- test/internal/lib/view-engine/filters/html-decode.js
+++ test/internal/lib/view-engine/filters/html-decode.js
@@ -4,7 +4,6 @@ const { experiment, test } = exports.lab = require('@hapi/lab').script();
 const { htmlDecode } = require('shared/view/nunjucks/filters/html-decode');
 
 experiment('htmlDecode', () => {
-
   test('returns text with html character entities transformed', async () => {
     const str = 'text with special characters &#39; &amp; &num;';
     expect(htmlDecode(str)).to.equal('text with special characters \' & #');
@@ -14,5 +13,4 @@ experiment('htmlDecode', () => {
     const str = '1st line of text without special characters';
     expect(htmlDecode(str)).to.equal('1st line of text without special characters');
   });
-
 });

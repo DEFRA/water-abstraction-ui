093384c21680e675ce3a55ff5562c8646ff3766f
Tim <tim@tamimi.dev>
2021-05-26 17:11:07 +0100

    Adds search functionality for billing accounts

---
+++ src/internal/modules/internal-search/lib/redirect-to-billing-account.js
@@ -0,0 +1,22 @@
+const { get } = require('lodash');
+const Boom = require('@hapi/boom');
+
+/**
+ * Redirects the user to the billing account
+ * @param  {Object} billingAccount
+ * @param  {Object} view  - view data
+ * @param  {Object} h     - HAPI response toolkit
+ * @return {Object}       - HAPI HTTP redirect response
+ */
+const redirectToBillingAccount = (billingAccount, view, h) => {
+  const billingAccountId = get(view, 'billingAccount.invoiceAccountId');
+  if (billingAccountId) {
+    return h.redirect(`/billing-accounts/${billingAccountId}`);
+  } else {
+    throw Boom.notFound(`Billing account not found`);
+  }
+};
+
+module.exports = {
+  redirectToBillingAccount
+};

918932edffcd49837c08ac228490cb3967cf3c96
James Carmichael <james@siteclick.co.uk>
2021-06-04 09:22:45 +0100

    Fix caption

--- src/internal/modules/billing/controllers/bill-run.js
+++ src/internal/modules/billing/controllers/bill-run.js
@@ -45,6 +45,10 @@ const getBillingBatchSummary = async (request, h) => {
   });
 };
 
+const getCaption = invoice => invoice.invoiceNumber
+  ? `Bill ${invoice.invoiceNumber}`
+  : `Billing account ${invoice.invoiceAccount.accountNumber}`;
+
 const getBillingBatchInvoice = async (request, h) => {
   const { batchId, invoiceId } = request.params;
 
@@ -63,7 +67,7 @@ const getBillingBatchInvoice = async (request, h) => {
     batchType: mappers.mapBatchType(batch.type),
     invoiceLicences: invoice.invoiceLicences.map(mappers.mapInvoiceLicence),
     isCredit: get(invoice, 'totals.netTotal', 0) < 0,
-    caption: `Billing account ${invoice.invoiceAccount.accountNumber}`,
+    caption: getCaption(invoice),
     errors: mappers.mapInvoiceLevelErrors(invoice),
     isCreditDebitBlockVisible: mappers.isCreditDebitBlockVisible(batch),
     links: {

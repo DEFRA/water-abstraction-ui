4f14b21d104bf5f882666386f59e6b7400e7d576
Axel Sunnehall <axels@dashup.co>
2021-06-15 11:54:13 +0100

    invalid GUID should display error page

--- src/internal/modules/gauging-stations/controller.js
+++ src/internal/modules/gauging-stations/controller.js
@@ -1,13 +1,6 @@
 'use strict';
 
-const { pick } = require('lodash');
-
 const mappers = require('./lib/mappers');
-const { scope } = require('../../lib/constants');
-const { hasScope } = require('../../lib/permissions');
-const { featureToggles } = require('../../config');
-const moment = require('moment');
-
 
 /**
  * Main Gauging station page
@@ -18,23 +11,25 @@ const moment = require('moment');
 
 const getLicencesForGaugingStation = async (request, h) => {
   const { gaugingStationId } = request.params;
-
   let { data } = request.pre.gaugingStations;
   let newData = mappers.mapStationsLicences(data);
-
   /* Format data for stations */
   if (newData.stations) {
     newData = mappers.mapStations(newData);
   }
-
   /* Format data for tags */
   if (newData.stations) {
     newData = mappers.mapTags(newData);
   }
-
-  console.log('************************************');
-  console.log(JSON.stringify(newData));
-
+  if (newData.stations.length === 0) {
+    const view = {
+      ...request.view,
+      pageTitle: `We cannot find information for monitoring station with id ${gaugingStationId}`
+    };
+    return h
+      .view('nunjucks/errors/404', view)
+      .code(404);
+  }
   return h.view('nunjucks/gauging-stations/licences', {
     ...request.view,
     tableCaption: 'All licences for gaugingstation',

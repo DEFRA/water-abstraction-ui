65ab1b1abfa9c6faf7006b07887b8cd1b74740bc
Axel Sunnehall <axels@dashup.co>
2021-06-21 13:47:45 +0100

    refactor and fix code smell

--- src/internal/modules/gauging-stations/lib/mappers.js
+++ src/internal/modules/gauging-stations/lib/mappers.js
@@ -3,17 +3,17 @@
 const moment = require('moment');
 const formatDate = date => moment(date).format('D MMMM YYYY');
 
-const defaultToNA = (item) => {
-  return (!item) ? 'n/a' : item;
-};
-const defaultToZero = (item) => {
-  return (!item) ? '0' : item;
-};
-const defaultToMld = (item) => {
-  return (!item) ? 'Ml/d' : item;
-};
+const defaultToNA = item =>
+  !item ? 'n/a' : item;
+
+const defaultToZero = item =>
+  !item ? '0' : item;
+
+const defaultToMld = item =>
+  !item ? 'Ml/d' : item;
+
 /* Draft: Independent objects currently joined in frontend */
-const mapStations = (newData) => {
+const mapStations = newData => {
   for (const rkey in newData.stations) {
     newData.stations[rkey].wiskiId = defaultToNA(newData.stations[rkey].wiskiId);
     newData.stations[rkey].easting = defaultToNA(newData.stations[rkey].easting);
@@ -22,16 +22,18 @@ const mapStations = (newData) => {
     newData.stations[rkey].licences = [];
     newData.licences = [];
     if (newData.stations.hasOwnProperty(rkey)) {
-      const licenceObj = {};
-      licenceObj.number = newData.stations[rkey].licenceRef;
-      licenceObj.communications = [];
-      const comObj = {};
-      comObj.type = defaultToNA(newData.stations[rkey].comstatus);
-      if (!newData.stations[rkey].dateStatusUpdated) {
-        comObj.sent = 'n/a';
-      } else {
+      const licenceObj = {
+        number: newData.stations[rkey].licenceRef,
+        communications: []
+      };
+      const comObj = {
+        type: defaultToNA(newData.stations[rkey].comstatus),
+        sent: 'n/a'
+      };
+      if (newData.stations[rkey].dateStatusUpdated) {
         comObj.sent = formatDate(newData.stations[rkey].dateStatusUpdated);
       }
+
       licenceObj.communications.push(comObj);
       newData.licences.push(licenceObj);
       newData.stations[rkey].licences.push(licenceObj);
@@ -40,24 +42,29 @@ const mapStations = (newData) => {
   return newData;
 };
 
-const mapTags = (newData) => {
+const mapTags = newData => {
   for (const rkey in newData.stations) {
     if (newData.stations.hasOwnProperty(rkey)) {
-      const tagObj = {};
-      tagObj.licenceNumber = newData.stations[rkey].licenceRef;
-      tagObj.tagValues = [];
-      const tagValueObj = {};
-      tagValueObj.licenceNumber = newData.stations[rkey].licenceRef;
-      const abstractionPeriod = { periodStart: 'n/a', periodEnd: 'n/a' };
+      const tagObj = {
+        licenceNumber: newData.stations[rkey].licenceRef,
+        tagValues: []
+      };
+      const abstractionPeriod = {
+        periodStart: 'n/a',
+        periodEnd: 'n/a'
+      };
       if (newData.stations[rkey].abstractionPeriodStartDay && newData.stations[rkey].abstractionPeriodStartMonth) {
-        abstractionPeriod.periodStart = newData.stations[rkey].abstractionPeriodStartDay + '/' + newData.stations[rkey].abstractionPeriodStartMonth;
+        abstractionPeriod.periodStart = `${newData.stations[rkey].abstractionPeriodStartDay}/${newData.stations[rkey].abstractionPeriodStartMonth}`;
       }
       if (newData.stations[rkey].abstractionPeriodEndDay && newData.stations[rkey].abstractionPeriodEndMonth) {
-        abstractionPeriod.periodEnd = newData.stations[rkey].abstractionPeriodEndDay + '/' + newData.stations[rkey].abstractionPeriodEndMonth;
+        abstractionPeriod.periodEnd = `${newData.stations[rkey].abstractionPeriodEndDay}/${newData.stations[rkey].abstractionPeriodEndMonth}`;
       }
-      tagValueObj.abstractionPeriod = abstractionPeriod;
-      tagValueObj.conditionType = defaultToNA(newData.stations[rkey].restrictionType);
-      tagValueObj.thresholdValue = defaultToZero(newData.stations[rkey].thresholdValue);
+      const tagValueObj = {
+        licenceNumber: newData.stations[rkey].licenceRef,
+        abstractionPeriod: abstractionPeriod,
+        conditionType: defaultToNA(newData.stations[rkey].restrictionType),
+        thresholdValue: defaultToZero(newData.stations[rkey].thresholdValue)
+      };
       tagValueObj.thresholdUnits = defaultToMld(newData.stations[rkey].thresholdUnit);
       tagObj.tagValues.push(tagValueObj);
       newData.stations[0].tags.push(tagObj);
@@ -66,13 +73,13 @@ const mapTags = (newData) => {
   return newData;
 };
 
-const mapStationsLicences = (data) => {
-  if (!data) {
-    return null;
-  }
-  const newData = {};
-  newData.stations = [];
-  newData.stationID = 0;
+const mapStationsLicences = data => {
+  if (!data) { return null; }
+
+  const newData = {
+    stations: [],
+    stationID: 0
+  };
   const newStations = JSON.parse(JSON.stringify(data));
   newData.stations = newStations;
   return newData;

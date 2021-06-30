c4625cac9ad8bd8dc8cb6c1721095a627846fdea
James Carmichael <james@siteclick.co.uk>
2021-06-30 15:48:43 +0100

    Create dynamic JWT token

---
+++ src/shared/lib/dynamic-jwt-token.js
@@ -0,0 +1,22 @@
+'use strict';
+
+require('dotenv').config();
+
+const jwtEncode = require('jsonwebtoken');
+const { throttle } = require('lodash');
+
+const expiresIn = 1800;
+
+const getJWTToken = () => jwtEncode.sign(
+  getPayload(),
+  process.env.JWT_SECRET,
+  { expiresIn }
+);
+
+const getDynamicJWTToken = throttle(getJWTToken, (expiresIn - 10) * 1000);
+
+const getPayload = () => ({ id: getRandomInt() });
+
+const getRandomInt = () => parseInt(Math.random() * Number.MAX_SAFE_INTEGER);
+
+exports.getDynamicJWTToken = getDynamicJWTToken;

c695144e563ecd3f2f71dfac4e25f84a251e7150
Srujan Chada <srujan.chada@gmail.com>
2021-07-07 13:12:54 +0100

    Update searchForLicence.spec.js

--- cypress/integration/internal/searchForLicence.spec.js
+++ cypress/integration/internal/searchForLicence.spec.js
@@ -13,23 +13,41 @@ describe('search for licences as internal user', () => {
   it('user logs in and searches for a License', () => {
     // cy.visit to visit the URL
     cy.visit(Cypress.env('ADMIN_URI'));
-
-    // Enter the user name and Password
     cy.fixture('users.json').then(users => {
       cy.get('input#email').type(users.psc);
     });
     cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
-
-    // Click Sign in Button
     cy.get('.govuk-button.govuk-button--start').click();
 
     // assert once the user is signed in
     cy.contains('Licences, users and returns');
 
-    // search for a license
+    // search for a license by using Name
     cy.get('#query').type('Anglian').should('be.visible');
     cy.get('.search__button').click();
     cy.contains('Licences').should('be.visible');
+    cy.get('.govuk-table__body').contains('03/28/69/0034').should('be.visible');
+    cy.get('.govuk-table__body').contains('03/28/69/0034').click();
+    cy.get('.govuk-summary-list__value').contains('Anglian Water Services Ltd').should('be.visible');
+    cy.get('.govuk-back-link').click();
+
+    // search for a license by using Alias Name
+    cy.get('#query').clear();
+    cy.get('#query').type('crumpet').should('be.visible');
+    cy.get('.search__button').click();
+    cy.contains('Licences').should('be.visible');
+    cy.get('.govuk-table__row').contains('AT/CURR/WEEKLY/01').should('be.visible');
+    cy.get('.govuk-table__row').contains('AT/CURR/WEEKLY/01').click();
+    cy.get('.govuk-back-link').click();
+
+    // search for a license by using Licence Number
+    cy.get('#query').clear();
+    cy.get('#query').type('AT/CURR/WEEKLY/01').should('be.visible');
+    cy.get('.search__button').click();
+    cy.contains('Licences').should('be.visible');
+    cy.get('.govuk-table__row').contains('AT/CURR/WEEKLY/01').should('be.visible');
+    cy.get('.govuk-table__row').contains('AT/CURR/WEEKLY/01').click();
+    cy.get('.govuk-back-link').click();
 
     //  Click Sign out Button
     cy.get('#signout').click();

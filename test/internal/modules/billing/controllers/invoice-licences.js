a411d179ff3cec5c5c5dd8dce070ed8cff99e314
Tim <tim@tamimi.dev>
2021-09-01 13:00:05 +0100

    Added more test cases

--- test/internal/modules/billing/controllers/invoice-licences.js
+++ test/internal/modules/billing/controllers/invoice-licences.js
@@ -32,7 +32,9 @@ experiment('internal/modules/billing/controllers/invoice-licences', () => {
     invoiceLicences: [
       {
         id: invoiceLicenceId,
-        transactions: [],
+        transactions: [{
+          value: '123'
+        }],
         licence: {
           licenceNumber: '12/34/56'
         },
@@ -103,6 +105,38 @@ experiment('internal/modules/billing/controllers/invoice-licences', () => {
       test('sets the invoiceLicence in the view', async () => {
         const [, { invoiceLicence }] = h.view.lastCall.args;
         expect(invoiceLicence).to.equal(request.pre.invoiceLicence);
+      });
+
+      experiment('correct calculation of total invoice licence transaction value', () => {
+        test('calculates the total invoice licence transaction value (1 of 4) - single value', async () => {
+          const [, { invoiceLicenceTotal }] = h.view.lastCall.args;
+          expect(invoiceLicenceTotal).to.equal(123);
+        });
+
+        test('calculates the total invoice licence transaction value (2 of 4) - adding values', async () => {
+          request.pre.invoiceLicence.transactions.push({
+            value: '200'
+          });
+          await controller.getDeleteInvoiceLicence(request, h);
+          const [, { invoiceLicenceTotal }] = h.view.lastCall.args;
+          expect(invoiceLicenceTotal).to.equal(323);
+        });
+
+        test('calculates the total invoice licence transaction value (3 of 4) - ignoring NaN', async () => {
+          request.pre.invoiceLicence.transactions.push({
+            value: 'crumpets'
+          });
+          await controller.getDeleteInvoiceLicence(request, h);
+          const [, { invoiceLicenceTotal }] = h.view.lastCall.args;
+          expect(invoiceLicenceTotal).to.equal(323);
+        });
+
+        test('calculates the total invoice licence transaction value (4 of 4) - defaults to zero', async () => {
+          request.pre.invoiceLicence.transactions = [ { value: 'cupcakes' } ];
+          await controller.getDeleteInvoiceLicence(request, h);
+          const [, { invoiceLicenceTotal }] = h.view.lastCall.args;
+          expect(invoiceLicenceTotal).to.equal(0);
+        });
       });
 
       test('sets the confirm form object in the view', async () => {

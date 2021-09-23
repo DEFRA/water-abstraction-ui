d5089ce3095c9881251527b9debb0384c740d7e8
Axel Sunnehall <axels@dashup.co>
2021-09-23 21:43:04 +0100

    ui function: fetching jwt tokens from cognito

--- src/shared/plugins/auth/controller.js
+++ src/shared/plugins/auth/controller.js
@@ -9,6 +9,83 @@ const { handleRequest, setValues } = require('shared/lib/forms');
 
 const isAuthenticated = request => !!get(request, 'auth.credentials.userId');
 
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+
+/* idToken, acccessToken lookup */
+const validateCognitoUser = async (request, h) => {
+  const { email, password } = request.payload;
+  // ENVIRONMENT VARIABLES REQUIRED IN ENV FILE:
+  // JWT_COGNITO_HOST=
+  // JWT_COGNITO_USERPOOLID=
+  // JWT_COGNITO_CLIENTID=
+  // JWT_COGNITO_REGION=*
+  let poolData = {
+    UserPoolId: process.env.JWT_COGNITO_USERPOOLID,
+    ClientId: process.env.JWT_COGNITO_CLIENTID
+  };
+
+  let userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+  let userData = {
+    Username: email,
+    Pool: userPool
+  };
+  let cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  let alreadyLoggedIn = false;
+  if (cognitoUser != null) {
+    await cognitoUser.getSession(function (err, result) {
+      if (err) {
+        alreadyLoggedIn = false;
+      }
+      if (result) {
+        alreadyLoggedIn = true;
+      }
+    });
+  }
+  // Optional Security feature:
+  // If suspected activity, invalidate issued token
+  // cognitoUser.globalSignOut(callback);
+
+  let authenticationData = {
+    Username: email,
+    Password: password
+  };
+  let authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
+
+  cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+  return new Promise((resolve, reject) => {
+    if (alreadyLoggedIn) {
+      let obj = { result: 'already logged in' };
+      return resolve(obj);
+    }
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: function (session) {
+        const tokens = {
+          accessToken: session.getAccessToken().getJwtToken(),
+          idToken: session.getIdToken().getJwtToken(),
+          refreshToken: session.getRefreshToken().getToken()
+        };
+        cognitoUser['tokens'] = tokens; // Save tokens for later use
+        resolve(cognitoUser);
+      },
+      onFailure: function (err) {
+        return reject(err);
+      },
+      newPasswordRequired: function (userAttributes, requiredAttributes) {
+        /* User signed up by admin and require password update */
+
+        // remove fields not to update
+        delete userAttributes.email_verified;
+        delete userAttributes.phone_number_verified;
+
+        // update new Password challenge, password can be same as original
+        cognitoUser.completeNewPasswordChallenge(password, userAttributes, this);
+
+        let obj = { result: 'updated password challenge', status: 'done' };
+        return resolve(obj);
+      }
+    });
+  });
+};
 /**
  * View signin page
  * @param {Object} request - the HAPI HTTP request
@@ -72,6 +149,17 @@ const postSignin = async (request, h) => {
 
   const { email, password } = request.payload;
 
+  // Request JWT token before IM login
+  /*
+  const res = await validateCognitoUser(request, h);
+  if (res) {
+    // let accessToken = res.getAccessToken().getJwtToken();
+    console.log(JSON.stringify(res));
+  } else {
+    console.log('Failed to login');
+  }
+  */
+  
   const user = await h.realm.pluginOptions.authenticate(email, password);
 
   // Forced reset

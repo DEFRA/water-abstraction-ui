f7fea8d9df8655bf3578f94082f173def1a23f26
Tim <tim@tamimi.dev>
2021-12-01 11:43:42 +0000

    localhost -> 127.0.0.1

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -5,8 +5,8 @@ module.exports = (on, config) => {
   config.env.JWT_TOKEN = process.env.JWT_TOKEN;
   config.env.NOTIFY_CALLBACK_TOKEN = process.env.NOTIFY_CALLBACK_TOKEN;
   config.env.WATER_URI = process.env.WATER_URI;
-  config.env.ADMIN_URI = 'http://localhost:8008/';
-  config.env.USER_URI = 'http://localhost:8000/';
+  config.env.ADMIN_URI = 'http://127.0.0.1:8008/';
+  config.env.USER_URI = 'http://127.0.0.1:8000/';
   config.env.DEFAULT_PASSWORD = 'P@55word';
   return config;
 };

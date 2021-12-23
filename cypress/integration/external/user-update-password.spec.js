af9907a544f50bad0de18b83e7c162224f245a9b
Axel Sunnehall <axels@dashup.co>
2021-12-23 14:05:38 +0000

    negative test scenarios

--- cypress/integration/external/user-update-password.spec.js
+++ cypress/integration/external/user-update-password.spec.js
@@ -32,19 +32,99 @@ describe('User update password', () => {
     // Click account settings
     cy.get('a[href*="/account"]').click();
 
-    // Click update password link in header 
+    // Click update password link in header
     cy.get('a[href*="/account/update-password"]').click();
 
     // Enter account password and press continue
     cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
     cy.get('button').contains('Continue').should('have.class', 'govuk-button').click();
 
-    // Enter and Confirm new password 
+    // Enter and Confirm new password
     cy.get('form').within(($form) => {
       cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
       cy.get('#confirm-password').type(Cypress.env('DEFAULT_PASSWORD'));
       cy.root().submit()
     })
+  });
+
+  it('Password not matching displays error message', () => {
+    //  cy.visit to visit the URL
+    cy.visit(Cypress.env('USER_URI'));
+
+    // tap the sign in button on the welcome page
+    cy.get('a[href*="/signin"]').click();
+
+    //  Enter the user name and Password
+    cy.fixture('users.json').then(users => {
+      cy.get('input#email').type(users.external);
+    });
+    cy.get('input#password').type(Cypress.env('DEFAULT_PASSWORD'));
+
+    //  Click Sign in Button
+    cy.get('.govuk-button.govuk-button--start').click();
+
+    //  assert once the user is signed in
+    cy.contains('Add licences or give access');
+
+    // Click account settings
+    cy.get('a[href*="/account"]').click();
+
+    // Click update password link in header
+    cy.get('a[href*="/account/update-password"]').click();
+
+    // Enter account password and press continue
+    cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
+    cy.get('button').contains('Continue').should('have.class', 'govuk-button').click();
+
+    // Enter and Confirm new password
+    cy.get('form').within(($form) => {
+      cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
+      cy.get('#confirm-password').type('notthepassword');
+      cy.root().submit()
+    });
+
+    cy.get('#error-summary-title').contains('There is a problem').should('be.visible');
+    cy.get('p.govuk-hint').contains('Your new password must have at least:').should('be.visible');
+  });
+
+  it('Password is not secure displays error message', () => {
+    //  cy.visit to visit the URL
+    cy.visit(Cypress.env('USER_URI'));
+
+    // tap the sign in button on the welcome page
+    cy.get('a[href*="/signin"]').click();
+
+    //  Enter the user name and Password
+    cy.fixture('users.json').then(users => {
+      cy.get('input#email').type(users.external);
+    });
+    cy.get('input#password').type(Cypress.env('DEFAULT_PASSWORD'));
+
+    //  Click Sign in Button
+    cy.get('.govuk-button.govuk-button--start').click();
+
+    //  assert once the user is signed in
+    cy.contains('Add licences or give access');
+
+    // Click account settings
+    cy.get('a[href*="/account"]').click();
+
+    // Click update password link in header
+    cy.get('a[href*="/account/update-password"]').click();
+
+    // Enter account password and press continue
+    cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
+    cy.get('button').contains('Continue').should('have.class', 'govuk-button').click();
+
+    // Enter and Confirm new password
+    cy.get('form').within(($form) => {
+      cy.get('#password').type('passwordisnotok');
+      cy.get('#confirm-password').type('passwordisnotok');
+      cy.root().submit()
+    });
+
+    cy.get('#error-summary-title').contains('There is a problem').should('be.visible');
+    cy.get('.govuk-error-summary__body').contains('Your password must contain').should('be.visible');
   });
 
 });
--- cypress/integration/internal/user-update-password.spec.js
+++ cypress/integration/internal/user-update-password.spec.js
@@ -21,19 +21,75 @@ describe('Internal user update password', () => {
     // Click account settings
     cy.get('a[href*="/account"]').click();
 
-    // Click update password link in header 
+    // Click update password link in header
     cy.get('a[href*="/account/update-password"]').click();
 
     // Enter account password and press continue
     cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
     cy.get('button').contains('Continue').should('have.class', 'govuk-button').click();
 
-    // Enter and Confirm new password 
+    // Enter and Confirm new password
     cy.get('form').within(($form) => {
       cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
       cy.get('#confirm-password').type(Cypress.env('DEFAULT_PASSWORD'));
       cy.root().submit()
     })
+  });
+
+  it('password is not matching displays error message', () => {
+    cy.visit(Cypress.env('ADMIN_URI'));
+    cy.fixture('users.json').then(users => {
+      cy.get('input#email').type(users.psc);
+    });
+    cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
+    cy.get('.govuk-button.govuk-button--start').click();
+
+    // Click account settings
+    cy.get('a[href*="/account"]').click();
+
+    // Click update password link in header
+    cy.get('a[href*="/account/update-password"]').click();
+
+    // Enter account password and press continue
+    cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
+    cy.get('button').contains('Continue').should('have.class', 'govuk-button').click();
+
+    // Enter and Confirm new password
+    cy.get('form').within(($form) => {
+      cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
+      cy.get('#confirm-password').type('nonmatchingpassword1234*');
+      cy.root().submit()
+    });
+    cy.get('#error-summary-title').contains('There is a problem').should('be.visible');
+    cy.get('p.govuk-hint').contains('Your new password must have at least:').should('be.visible');
+  });
+
+  it('password is not secure displays error message', () => {
+    cy.visit(Cypress.env('ADMIN_URI'));
+    cy.fixture('users.json').then(users => {
+      cy.get('input#email').type(users.psc);
+    });
+    cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
+    cy.get('.govuk-button.govuk-button--start').click();
+
+    // Click account settings
+    cy.get('a[href*="/account"]').click();
+
+    // Click update password link in header
+    cy.get('a[href*="/account/update-password"]').click();
+
+    // Enter account password and press continue
+    cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
+    cy.get('button').contains('Continue').should('have.class', 'govuk-button').click();
+
+    // Enter and Confirm new password
+    cy.get('form').within(($form) => {
+      cy.get('#password').type(Cypress.env('DEFAULT_PASSWORD'));
+      cy.get('#confirm-password').type('passwo');
+      cy.root().submit()
+    });
+    cy.get('#error-summary-title').contains('There is a problem').should('be.visible');
+    cy.get('.govuk-error-summary__body').contains('Re-enter your new password').should('be.visible');
   });
 
 });

747236012f2904c4d82c98e2798ec78f4e7c2bfd
Ben Surgison <33627753+defra-contractor@users.noreply.github.com>
2022-03-01 09:59:43 +0000

    WATER-3415: Addition of Notes to Charge Information review page (#2048)

---
+++ src/internal/modules/notes/forms/note.js
@@ -0,0 +1,42 @@
+const Joi = require('joi');
+const { formFactory, fields } = require('shared/lib/forms');
+const session = require('../lib/session');
+
+const MAX_NOTES_CHARACTERS = 500;
+
+/**
+ * Creates a form object for internal users to create and update notes.
+ * @return {Object}       form object
+ */
+const form = request => {
+  const { csrfToken } = request.view;
+  const { noteId } = request.params;
+  const { note, hint } = session.get(request, noteId) || {};
+  const f = formFactory(`/note/${noteId}`);
+
+  f.fields.push(fields.text('note', {
+    hint,
+    multiline: true,
+    maxlength: MAX_NOTES_CHARACTERS,
+    errors: {
+      'string.empty': {
+        message: 'Enter details.'
+      },
+      'string.max': {
+        message: `Enter no more than ${MAX_NOTES_CHARACTERS} characters.`
+      }
+    }
+  }, note));
+  f.fields.push(fields.hidden('csrf_token', {}, csrfToken));
+  f.fields.push(fields.button(null, { label: 'Continue' }));
+
+  return f;
+};
+
+const schema = () => Joi.object().keys({
+  csrf_token: Joi.string().guid().required(),
+  note: Joi.string().max(MAX_NOTES_CHARACTERS)
+});
+
+exports.schema = schema;
+exports.form = form;

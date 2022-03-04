7b2c2d12fc3449feb8807445a501275e5f366368
Tim <tim@tamimi.dev>
2022-03-04 16:14:07 +0000

    Tidy up and add unit test

--- src/shared/view/nunjucks/filters/year.js
+++ src/shared/view/nunjucks/filters/year.js
@@ -4,17 +4,16 @@ const { isNil } = require('lodash');
 const moment = require('moment');
 
 /**
- * Format a timestamp
+ * Format a timestamp to YYYY
  * @param  {String|Number} value        - Timestamp / date (parsed by moment)
- * @param  {String} [format='DDD MMM YYYY'] - Format (see moment docs)
  * @return {String}              formatted date
  */
-const year = (value, format = 'YYYY') => {
+const year = (value) => {
   if (isNil(value)) {
     return undefined;
   }
   const m = moment(value);
-  return m.isValid() ? m.format(format) : undefined;
+  return m.isValid() ? m.format('YYYY') : undefined;
 };
 
 exports.year = year;

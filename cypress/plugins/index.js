43d71933a187962d27b654f16283079a5a39b779
Jonathan Goulding <jonathangoulding21@gmail.com>
2022-03-14 08:44:06 +0000

    feat: run cyopress test in container

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -6,7 +6,7 @@ module.exports = (on, config) => {
   config.env.NOTIFY_CALLBACK_TOKEN = process.env.NOTIFY_CALLBACK_TOKEN;
   config.env.WATER_URI = process.env.WATER_URI  || 'http://127.0.0.1:8001/water/1.0';
   config.env.ADMIN_URI = process.env.ADMIN_URI  || 'http://localhost:8008/';
-  config.env.USER_URI = process.env.USER_URI    || 'http://localhost:8000/';
+  config.env.USER_URI = process.env.USER_URI  || 'http://localhost:8000/';
   config.env.DEFAULT_PASSWORD = 'P@55word';
   return config;
 };
---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || exit
+
+export $(grep -v '^#' ../water-abstraction-orchestration/secrets/.env | xargs)
+DISPLAY=$IP:0
+
+export WATER_URI="http://host.docker.internal:8001/water/1.0"
+export ADMIN_URI='http://host.docker.internal:8008/'
+export USER_URI='http://host.docker.internal:8000/'
+export BASE_URL='http://host.docker.internal:8008'
+#docker rm cypress:1
+DOCKER_BUILDKIT=0 docker build -f cypress.Dockerfile -t cypress:1 .
+
+docker run \
+  --env DISPLAY \
+  -w /e2e \
+  --env JWT_TOKEN=$JWT_TOKEN \
+  --env NOTIFY_CALLBACK_TOKEN=$NOTIFY_CALLBACK_TOKEN \
+  --env WATER_URI=$WATER_URI \
+  --env ADMIN_URI=$ADMIN_URI \
+  --env USER_URI=$USER_URI \
+  cypress:1 \
+    "node_modules/.bin/cypress run --spec \"cypress/integration/internal/remove-individual-licence-from-bill.spec.js\" --config baseUrl=${BASE_URL}"
+

222678a90700fbe25d488ffd8963aa546782b130
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2022-03-29 16:07:38 +0100

    Feat/pipeline regression tests (#2060)
    * feat: env vars for cypres
    
    * ci: dockerise application
    
    * fix: debug failing test issue
    
    * fix: debug failing test issue
    
    * fix: run test script

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 module.exports = (on, config) => {
   config.env.JWT_TOKEN = process.env.JWT_TOKEN;
   config.env.NOTIFY_CALLBACK_TOKEN = process.env.NOTIFY_CALLBACK_TOKEN;
-  config.env.WATER_URI = process.env.WATER_URI;
-  config.env.ADMIN_URI = 'http://localhost:8008/';
-  config.env.USER_URI = 'http://localhost:8000/';
+  config.env.WATER_URI = process.env.WATER_URI  || 'http://127.0.0.1:8001/water/1.0';
+  config.env.ADMIN_URI = process.env.ADMIN_URI  || 'http://localhost:8008/';
+  config.env.USER_URI = process.env.USER_URI  || 'http://localhost:8000/';
   config.env.DEFAULT_PASSWORD = 'P@55word';
   return config;
 };

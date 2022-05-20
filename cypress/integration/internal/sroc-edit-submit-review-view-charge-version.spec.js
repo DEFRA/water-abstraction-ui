e87554cfbacd59286fd978baf100923dfd038a47
Shabbir Ahmad <shabbirshirazi@users.noreply.github.com>
2022-05-20 08:56:45 +0100

    Update sroc-edit-submit-review-view-charge-version.spec.js
    Edit journey update

--- cypress/integration/internal/sroc-edit-submit-review-view-charge-version.spec.js
+++ cypress/integration/internal/sroc-edit-submit-review-view-charge-version.spec.js
@@ -93,117 +93,106 @@ describe('Create SRoC Charge version workflow journey', () => {
 
     describe('Change charge version start date',()=>{
         cy.get('a[href*="charge-information/start-date"]').click()
+        cy.get('.govuk-heading-l').contains('Set charge start date');
+        cy.get('#startDate-4').click();
+        cy.get('#customDate-day').clear().type('01');
+        cy.get('#customDate-month').clear().type('06');
+        cy.get('#customDate-year').clear().type('2022');
+        cy.get('form > .govuk-button').contains('Continue').click();
     });
 
-    // describe('Add a new charge element', () => {
-    //   cy.get('[value="addElement"]').click();
-    //   cy.get('.govuk-heading-l').contains('Select a purpose use');
-    //   cy.get('[type="radio"]#purpose').click();
-    //   cy.get('button.govuk-button').click();
-    //   cy.get('.govuk-heading-l').contains('Add element description');
-    //   cy.get('[type="text"]#description').type('test element description');
-    //   cy.get('button.govuk-button').click();
-    //   cy.get('.govuk-heading-l').contains('Set abstraction period');
-    //   cy.get('[type="text"]#startDate-day').type('01');
-    //   cy.get('[type="text"]#startDate-month').type('04');
-    //   cy.get('[type="text"]#endDate-day').type('30');
-    //   cy.get('[type="text"]#endDate-month').type('09');
-    //   cy.get('button.govuk-button').click();
-    //   cy.get('.govuk-heading-l').contains('Add annual quantities');
-    //   cy.get('[type="text"]#authorisedAnnualQuantity').type('10');
-    //   cy.get('button.govuk-button').click();
-    //   cy.get('.govuk-heading-l').contains('Set time limit?');
-    //   cy.get('[type="radio"]#timeLimitedPeriod-2').click();
-    //   cy.get('button.govuk-button').click();
-    //   cy.get('.govuk-heading-l').contains('Select loss category');
-    //   cy.get('[type="radio"]#loss-2').click();
-    //   cy.get('button.govuk-button').click();
-    // });
+    describe('Select an existing address for Big Farm Co Ltd', () => {
+        cy.get('[type="radio"]#billingAccountId').click();
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
+  
+      describe('Use abstraction data to set up the element?', () => {
+        cy.get('.govuk-heading-l').contains('Use abstraction data to set up the element?');
+        cy.get('[type="radio"]#useAbstractionData').click();
+        cy.get('button.govuk-button').click();
+      });
+  
+      describe('Check charge information', () => {
+        cy.get('#main-content')
+          .children()
+          .should('contain', 'Check charge information');
+      });
 
-    // describe('Check charge information and add Charge Category', () => {
-    //   cy.get('#main-content')
-    //     .children()
-    //     .should('contain', 'Check charge information');
-    // });
+    describe('user Assign charge reference', () => {
+        cy.get('[value="addChargeCategory"]').click();
+        cy.get('button.govuk-button').click();
 
-    // describe('user Assign charge reference', () => {
-    //   cy.get('[value="addChargeCategory"]').click();
-    //   cy.get('.govuk-heading-l').contains('Select the elements this charge reference is for');
-    //   cy.get('[type="checkbox"]').check();
-    //   cy.get('button.govuk-button').click();
+      describe('Enter a description for the charge reference', () => {
+        cy.get('.govuk-heading-l').contains('Enter a description for the charge reference');
+        cy.get('#description').clear().type('Automation-Test');
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
 
-    //   describe('Enter a description for the charge reference', () => {
-    //     cy.get('.govuk-heading-l').contains('Enter a description for the charge reference');
-    //     cy.get('#description').clear().type('Automation-Test');
-    //     cy.get('form > .govuk-button').contains('Continue').click();
-    //   });
+      describe('Select the source', () => {
+        cy.get('.govuk-heading-l').contains('Select the source');
+        cy.get('[type="radio"]#source').click();
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
 
-    //   describe('Select the source', () => {
-    //     cy.get('.govuk-heading-l').contains('Select the source');
-    //     cy.get('[type="radio"]#source').click();
-    //     cy.get('form > .govuk-button').contains('Continue').click();
-    //   });
+      describe('Select the loss category', () => {
+        cy.get('.govuk-heading-l').contains('Select the loss category');
+        cy.get('[type="radio"]#loss').click();
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
 
-    //   describe('Select the loss category', () => {
-    //     cy.get('.govuk-heading-l').contains('Select the loss category');
-    //     cy.get('[type="radio"]#loss').click();
-    //     cy.get('form > .govuk-button').contains('Continue').click();
-    //   });
+      describe('Enter the total quantity to use for this charge reference', () => {
+        cy.get('.govuk-heading-l').contains('Enter the total quantity to use for this charge reference');
+        cy.get('#volume').clear().type('150');
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
 
-    //   describe('Enter the total quantity to use for this charge reference', () => {
-    //     cy.get('.govuk-heading-l').contains('Enter the total quantity to use for this charge reference');
-    //     cy.get('#volume').clear().type('150');
-    //     cy.get('form > .govuk-button').contains('Continue').click();
-    //   });
+      describe('Select the water availability', () => {
+        cy.get('.govuk-heading-l').contains('Select the water availability');
+        cy.get('[type="radio"]#isRestrictedSource').click();
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
 
-    //   describe('Select the water availability', () => {
-    //     cy.get('.govuk-heading-l').contains('Select the water availability');
-    //     cy.get('[type="radio"]#isRestrictedSource').click();
-    //     cy.get('form > .govuk-button').contains('Continue').click();
-    //   });
+      describe('Select the water modelling charge', () => {
+        cy.get('.govuk-heading-l').contains('Select the water modelling charge');
+        cy.get('[type="radio"]#waterModel-2').click();
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
 
-    //   describe('Select the water modelling charge', () => {
-    //     cy.get('.govuk-heading-l').contains('Select the water modelling charge');
-    //     cy.get('[type="radio"]#waterModel-2').click();
-    //     cy.get('form > .govuk-button').contains('Continue').click();
-    //   });
+      describe('Do additional charges apply?', () => {
+        cy.get('.govuk-heading-l').contains('Do additional charges apply?');
+        cy.get('[type="radio"]#isAdditionalCharges').click();
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
 
-    //   describe('Do additional charges apply?', () => {
-    //     cy.get('.govuk-heading-l').contains('Do additional charges apply?');
-    //     cy.get('[type="radio"]#isAdditionalCharges').click();
-    //     cy.get('form > .govuk-button').contains('Continue').click();
-    //   });
+      describe('Is abstraction from a supported source?', () => {
+        cy.get('.govuk-heading-l').contains('Is abstraction from a supported source?');
+        cy.get('[type="radio"]#isSupportedSource').click();
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
 
-    //   describe('Is abstraction from a supported source?', () => {
-    //     cy.get('.govuk-heading-l').contains('Is abstraction from a supported source?');
-    //     cy.get('[type="radio"]#isSupportedSource').click();
-    //     cy.get('form > .govuk-button').contains('Continue').click();
-    //   });
+      describe('Select the name of the supported source', () => {
+        cy.get('.govuk-heading-l').contains('Select the name of the supported source');
+        cy.get('[type="radio"]#supportedSourceId-12').click();
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
 
-    //   describe('Select the name of the supported source', () => {
-    //     cy.get('.govuk-heading-l').contains('Select the name of the supported source');
-    //     cy.get('[type="radio"]#supportedSourceId-12').click();
-    //     cy.get('form > .govuk-button').contains('Continue').click();
-    //   });
+      describe('Is abstraction for the supply of public water?', () => {
+        cy.get('.govuk-heading-l').contains('Is abstraction for the supply of public water?');
+        cy.get('[type="radio"]#isSupplyPublicWater').click();
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
 
-    //   describe('Is abstraction for the supply of public water?', () => {
-    //     cy.get('.govuk-heading-l').contains('Is abstraction for the supply of public water?');
-    //     cy.get('[type="radio"]#isSupplyPublicWater').click();
-    //     cy.get('form > .govuk-button').contains('Continue').click();
-    //   });
+      describe('Do adjustments apply?', () => {
+        cy.get('.govuk-heading-l').contains('Do adjustments apply?');
+        cy.get('[type="radio"]#isAdjustments-2').click();
+        cy.get('form > .govuk-button').contains('Continue').click();
+      });
+    });
 
-    //   describe('Do adjustments apply?', () => {
-    //     cy.get('.govuk-heading-l').contains('Do adjustments apply?');
-    //     cy.get('[type="radio"]#isAdjustments-2').click();
-    //     cy.get('form > .govuk-button').contains('Continue').click();
-    //   });
-    // });
+    describe('Charge category and the element are visible on the charge summary screen', () => {
+      cy.get('.govuk-caption-l').eq(1).invoke('text').should('contains', 'Charge reference');
+      cy.get('.govuk-summary-list__value').should('contain', 'Potable Water Supply - Direct');
+    });
 
-    // describe('Both charge elements are visible on the charge summary screen', () => {
-    //   cy.get('.govuk-caption-m').eq(0).invoke('text').should('eq', 'Charge element 1');
-    //   cy.get('.govuk-caption-m').eq(1).invoke('text').should('eq', 'Charge element 2');
-    //   cy.get('.govuk-summary-list__value').should('contain', 'Potable Water Supply - Direct');
-    //   cy.get('.govuk-summary-list__value').should('contain', 'test element description');
-    // });
   });
 });

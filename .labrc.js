2a4225f8a03d3325bc1247a031d17324eaf8cc28
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-07-10 23:53:21 +0100

    Get tests passing 2

--- .labrc.js
+++ .labrc.js
@@ -1,4 +1,8 @@
 'use strict'
+
+require('dotenv').config();
+require('app-module-path').addPath(require('path').join(__dirname, 'src/'));
+
 // default settings for lab test runs.
 //
 // This is overridden if arguments are passed to lab via the command line.

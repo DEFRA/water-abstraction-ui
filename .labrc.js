c6e30735873eee6332cd58f1326a5835576a2e8c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-07-10 23:46:23 +0100

    Implement changes

--- .labrc.js
+++ .labrc.js
@@ -1,15 +1,14 @@
-require('dotenv').config();
-require('app-module-path').addPath(require('path').join(__dirname, 'src/'));
-
+'use strict'
 // default settings for lab test runs.
 //
 // This is overridden if arguments are passed to lab via the command line.
 module.exports = {
-  paths: ['test/internal', 'test/external', 'test/shared'],
-  'coverage-exclude': [
-    'data',
-    'govuk_modules',
-    'node_modules',
-    'public'
-  ]
+  verbose: true,
+  coverage: true,
+  // Means when we use *.only() in our tests we just get the output for what we've flagged rather than all output but
+  // greyed out to show it was skipped
+  'silent-skips': true,
+  // lcov reporter required for SonarCloud
+  reporter: ['console', 'html', 'lcov'],
+  output: ['stdout', 'coverage/coverage.html', 'coverage/lcov.info']
 };

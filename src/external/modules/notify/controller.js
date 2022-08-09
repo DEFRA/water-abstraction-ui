8b5c39a609c54e638787852f3ee666fdc0fe64f2
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2022-08-09 11:57:01 +0100

    Changing logging level from `info` to `warn`

--- src/external/modules/notify/controller.js
+++ src/external/modules/notify/controller.js
@@ -3,14 +3,14 @@ const { logger } = require('../../logger')
 const callback = async (request, h) => {
   const receivedToken = request.headers.authorization ? request.headers.authorization.replace('Bearer ', '') : null
   if (receivedToken !== process.env.NOTIFY_CALLBACK_TOKEN) {
-    logger.info('A Notify callback request was declined due to token mismatch.')
+    logger.warn('A Notify callback request was declined due to token mismatch.')
     return h.response('Unauthorized').code(403)
   }
 
   try {
     await services.water.notify.postNotifyCallback(request.payload)
   } catch (error) {
-    logger.info(`A Notify callback request was declined due to HTTP error code ${error.statusCode}`)
+    logger.warn(`A Notify callback request was declined due to HTTP error code ${error.statusCode}`)
     return h.response(null).code(error.statusCode)
   }
 

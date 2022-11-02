8276746d1e487c20e4a4efe0fa5a321fea12b7e3
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-11-02 23:07:37 +0000

    Add routes tests
    I know! These are really not doing anything. But with a lack of 'proper' controller tests the routes doesn't get the coverage needed. Plus we're following the pattern that exists, for an endpoint that we've added to support us making this project redundant.
    
    So, watcha gonna do!?

--- test/internal/modules/system-proxy/routes.test.js
+++ test/internal/modules/system-proxy/routes.test.js
@@ -8,18 +8,46 @@ const { experiment, test, beforeEach } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Thing under test
-const routes = require('../../../../src/internal/modules/acceptance-tests-proxy/routes')
+const routes = require('../../../../src/internal/modules/system-proxy/routes')
 
-experiment('acceptance-tests-proxy routes', () => {
+experiment('system-proxy routes', () => {
   let route
 
-  experiment('/acceptance-tests', () => {
+  experiment('/system/{tail*}', () => {
     beforeEach(() => {
       route = routes[0]
     })
 
-    test('handles POST requests', () => {
-      expect(route.method).to.equal('POST')
+    test('handles GET requests', () => {
+      expect(route.method).to.equal('GET')
+    })
+
+    test('does not require authentication', () => {
+      expect(route.config.auth).to.equal(false)
+    })
+  })
+
+  experiment('/assets/all.js', () => {
+    beforeEach(() => {
+      route = routes[0]
+    })
+
+    test('handles GET requests', () => {
+      expect(route.method).to.equal('GET')
+    })
+
+    test('does not require authentication', () => {
+      expect(route.config.auth).to.equal(false)
+    })
+  })
+
+  experiment('/assets/stylesheets/application.css', () => {
+    beforeEach(() => {
+      route = routes[0]
+    })
+
+    test('handles GET requests', () => {
+      expect(route.method).to.equal('GET')
     })
 
     test('does not require authentication', () => {

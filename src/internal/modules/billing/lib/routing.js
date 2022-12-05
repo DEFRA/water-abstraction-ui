85fbc0b6a5132058bd2c7eb3cc5c86823e01f79f
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2022-12-05 13:02:26 +0000

    Implement routing logic with dummy url

--- src/internal/modules/billing/lib/routing.js
+++ src/internal/modules/billing/lib/routing.js
@@ -10,12 +10,12 @@
  * @return {String} the link
  */
 const getBillingBatchRoute = (batch, opts = {}) => {
-  const { id } = batch
+  const { id, batchType, scheme } = batch
 
   const routeMap = new Map()
     .set('processing', `/billing/batch/${id}/processing?back=${opts.isBackEnabled ? 1 : 0}`)
     .set('sending', `/billing/batch/${id}/processing?back=${opts.isBackEnabled ? 1 : 0}`)
-    .set('ready', opts.invoiceId ? `/billing/batch/${id}/invoice/${opts.invoiceId}` : `/billing/batch/${id}/summary`)
+    .set('ready', _determineReadyUrl(opts, id, batchType, scheme))
     .set('sent', opts.showSuccessPage ? `/billing/batch/${id}/confirm/success` : `/billing/batch/${id}/summary`)
     .set('review', `/billing/batch/${id}/two-part-tariff-review`)
 
@@ -26,6 +26,18 @@ const getBillingBatchRoute = (batch, opts = {}) => {
   }
 
   return routeMap.get(batch.status)
+}
+
+function _determineReadyUrl (opts, id, batchType, scheme) {
+  if (opts.invoiceId) {
+    return `/billing/batch/${id}/invoice/${opts.invoiceId}`
+  }
+
+  if (batchType === 'supplementary' && scheme === 'presroc') {
+    return '/SROC/SUPPLEMENTARY'
+  }
+
+  return `/billing/batch/${id}/summary`
 }
 
 const getTwoPartTariffLicenceReviewRoute = (batch, invoiceLicenceId) =>

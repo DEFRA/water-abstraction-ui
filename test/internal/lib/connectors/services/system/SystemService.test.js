f09c905145e04cdb4aba7424fd9384c6e2365c0a
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-01-05 14:50:58 +0000

    Rework connector as service client
    We realised that the API client we'd created is more typically used to connect via `hapi-pg-rest-api` (although typically we found other instances where this isn't the case!). We therefore rework it as a service client. We also move it to the `internal` folder as we don't currently expect it to be shared.

---
+++ test/internal/lib/connectors/services/system/SystemService.test.js
@@ -0,0 +1,58 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { experiment, test, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+const sandbox = Sinon.createSandbox()
+
+// Things we need to stub
+const { serviceRequest } = require('@envage/water-abstraction-helpers')
+
+// Thing under test
+const SystemService = require('../../../../../../src/internal/lib/connectors/services/system/SystemService.js')
+
+experiment.only('services/system/SystemService', () => {
+  let service
+
+  beforeEach(async () => {
+    sandbox.stub(serviceRequest, 'post')
+    service = new SystemService('http://127.0.0.1:8013')
+  })
+
+  afterEach(async () => {
+    sandbox.restore()
+  })
+
+  experiment('.postCreateBillRun', () => {
+    beforeEach(async () => {
+      await service.postCreateBillRun(
+        'supplementary',
+        'sroc',
+        'fdf130c3-3021-408d-869c-2be1fb067e67',
+        'test@test.com'
+      )
+    })
+
+    test('passes the expected URL to the service request', async () => {
+      const url = serviceRequest.post.lastCall.args[0]
+
+      expect(url).to.equal('http://127.0.0.1:8013/bill-runs')
+    })
+
+    test('passes the query in the request body', async () => {
+      const options = serviceRequest.post.lastCall.args[1]
+
+      expect(options.body).to.equal({
+        type: 'supplementary',
+        scheme: 'sroc',
+        region: 'fdf130c3-3021-408d-869c-2be1fb067e67',
+        user: 'test@test.com',
+        previousBillRunId: undefined
+      })
+    })
+  })
+})

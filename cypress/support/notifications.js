7a6629c3c1277420aa2f027403af2ed54105f686
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-03-17 11:01:18 +0000

    Remove axios and update commands
    Also needed to tweak the tests. You are not supposed to use `let` and `const` to [hold data between tests in Cypress](https://docs.cypress.io/guides/core-concepts/variables-and-aliases#Aliases).

--- cypress/support/notifications.js
+++ cypress/support/notifications.js
@@ -1,30 +1,35 @@
-const { get } = require('lodash');
-const querystring = require('querystring');
-const axios = require('axios');
+'use strict'
 
-Cypress.Commands.add('simulateNotifyCallback', async (notificationId) => {
-  const requestBody = {
-    id: notificationId,
-    reference: notificationId,
-    status: 'delivered',
-    notification_type: 'email',
-    to: 'irrelevant',
-    created_at: new Date().toISOString(),
-    completed_at: new Date().toISOString(),
-    sent_at: new Date().toISOString()
-  };
+const querystring = require('querystring')
 
-  const url = `${Cypress.env('USER_URI')}notify/callback`;
-  await axios.post(url, requestBody, {
+Cypress.Commands.add('simulateNotifyCallback', (notificationId) => {
+  cy.request({
+    url: `${Cypress.env('USER_URI')}notify/callback`,
+    log: false,
+    method: 'POST',
     headers: {
       authorization: `Bearer ${Cypress.env('NOTIFY_CALLBACK_TOKEN')}`
-    }
-  });
-  return 'OK';
+    },
+    body: {
+      id: notificationId,
+      reference: notificationId,
+      status: 'delivered',
+      notification_type: 'email',
+      to: 'irrelevant',
+      created_at: new Date().toISOString(),
+      completed_at: new Date().toISOString(),
+      sent_at: new Date().toISOString()
+    },
+    timeout: 60000
+  })
 });
 
-Cypress.Commands.add('getLastNotifications', async (baseUrl, email) => {
-  const url = `${baseUrl}notifications/last?${querystring.encode({ email })}`;
-  const response = await axios.get(url);
-  return get((response), `data.data[0]`, {});
-});
+Cypress.Commands.add('getLastNotifications', (baseUrl, email) => {
+  cy.request({
+    url: `${baseUrl}notifications/last?${querystring.encode({ email })}`,
+    log: false,
+    method: 'GET'
+  }).then((response) => {
+    return cy.wrap(response.body.data[0])
+  })
+})

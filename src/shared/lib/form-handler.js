d56e9aee53d4b623c351ebdbc891eb3fc6a5cfec
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-03-18 16:45:54 +0000

    Refactor and add documentation
    Whilst trying to debug the issue we spotted that we were calling `formContainer.form(request)` multiple times to get the same result. So, first change was to avoid repeating the call unnecessarily.
    
    But we also had to deep dive into what on earth this was supposed to be doing. So, we have also added some documentation that hopefully will still make sense should we return to this section of the code!

--- src/shared/lib/form-handler.js
+++ src/shared/lib/form-handler.js
@@ -15,23 +15,34 @@ const isGet = method => isEqualCaseInsensitive(method, 'get')
  * @return {Object}
  */
 const handleFormRequest = (request, formContainer) => {
-  let form = formContainer.form(request)
+  // The default form is based on whatever the underlying form container, for example
+  // src/internal/modules/address-entry/forms/select-address.js generates plus what it extracts from the request object.
+  const defaultForm = formContainer.form(request)
 
-  // Use session forms for post forms
-  if (isPost(form.method) && isGet(request.method)) {
-    form = sessionForms.get(request, formContainer.form(request))
+  let processedForm
+
+  // Some forms, like the select-address form, are designated as 'post' forms. The user is expected to submit a value
+  // and the form will be POSTed to the UI. What this is checking for is we have a GET request for a 'post' form based
+  // page. If we do, there are 2 possible scenarios
+  //
+  // - this is the first GET request to the page so sessionForms() will pull nothing out of the session and default to
+  //   defaultForm. This will then be used to build the page
+  // - this is a result of a POST request being rejected because of a validation error. This means the form will have
+  //   been saved in the session and sessionForms() will retrieve it along with the error details. defaultForm will be
+  //   ignored
+  if (isPost(defaultForm.method) && isGet(request.method)) {
+    processedForm = sessionForms.get(request, defaultForm)
   }
 
-  // Handle request
-  if (isEqualCaseInsensitive(request.method, form.method)) {
-    form = forms.handleRequest(
-      formContainer.form(request),
-      request,
-      formContainer.schema(request)
-    )
+  // If we didn't fall into the previous block we next check if the type of form matches the request type. This is
+  // where a form submission will be validated, for example when the select-address form (a 'post' designated form) is
+  // POSTed to the UI the submission will be checked. forms.handleRequest() is where all the Joi validation and
+  // custom error message determination happens
+  if (isEqualCaseInsensitive(request.method, defaultForm.method)) {
+    processedForm = forms.handleRequest(defaultForm, request, formContainer.schema(request))
   }
 
-  return form
+  return processedForm
 }
 
 exports.handleFormRequest = handleFormRequest

931c5958e072ae499159fd2e06f65cebe886c679
Jason Claxton <jason.claxton@defra.gov.uk>
2023-06-08 16:35:19 +0100

    Remove cypress folder

--- cypress/integration/external/page-title.spec.js
+++
@@ -1,8 +0,0 @@
-describe('Page title verification', () => {
-  it('the login page loads', () => {
-    cy.visit(Cypress.env('USER_URI'))
-
-    //  finding the site title
-    cy.contains('Manage your water abstraction or impoundment licence')
-  })
-})

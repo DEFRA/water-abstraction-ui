3bd8ffc3e8914fadcf70f3a7f566bc4d9da33397
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-06-23 15:27:32 +0100

    Fix the bill run transactions CSV file
    Again we add a method that performs the same logic the service does, determine the latest address and use the agency company name linked to it if it exists. Else just use the name of the company directly linked to the invoice account.

--- src/internal/modules/billing/services/transactions-csv.js
+++ src/internal/modules/billing/services/transactions-csv.js
@@ -38,6 +38,18 @@ const getCSVFileName = batch => {
   return `${batch.region.displayName} ${batchType.toLowerCase()} bill run ${batch.billRunNumber}.csv`
 }
 
+function _billForName (invoiceAccount) {
+  invoiceAccount.invoiceAccountAddresses.sort((a, b) => {
+    return new Date(b.date) - new Date(a.date)
+  })
+
+  if (invoiceAccount.invoiceAccountAddresses[0]?.agentCompany?.name) {
+    return invoiceAccount.invoiceAccountAddresses[0]?.agentCompany?.name
+  }
+
+  return invoiceAccount.company.name
+}
+
 function _csvLineAlcs (invoice, invoiceLicence, transaction, chargeVersions) {
   const csvLine = {
     'Billing account number': invoice.invoiceAccount.invoiceAccountNumber,
@@ -101,7 +113,7 @@ function _csvLineAlcs (invoice, invoiceLicence, transaction, chargeVersions) {
 function _csvLineSroc (invoice, invoiceLicence, transaction, chargeVersions) {
   const csvLine = {
     'Billing account number': invoice.invoiceAccount.invoiceAccountNumber,
-    'Customer name': invoice.invoiceAccount.company.name,
+    'Customer name': _billForName(invoice.invoiceAccount),
     'Licence number': invoiceLicence.licenceRef,
     'Bill number': invoice.invoiceNumber,
     'Financial year': invoice.financialYearEnding,

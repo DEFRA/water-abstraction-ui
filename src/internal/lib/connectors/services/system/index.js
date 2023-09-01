a91ef2bcabb148a23a8a1414bb124007151ca023
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-09-01 15:15:33 +0100

    Add new BillingAccountsApiClient
    This is the convention in the legacy code we're following for sending requests to the **water-abstraction-system**.

--- src/internal/lib/connectors/services/system/index.js
+++ src/internal/lib/connectors/services/system/index.js
@@ -1,10 +1,12 @@
 'use strict'
 
 // Internal services
+const BillingAccountsApiClient = require('./BillingAccountsApiClient.js')
 const BillRunsApiClient = require('./BillRunsApiClient.js')
 
 const { logger } = require('../../../../logger')
 
 module.exports = config => ({
+  billingAccounts: new BillingAccountsApiClient(config.services.system, logger),
   billRuns: new BillRunsApiClient(config.services.system, logger)
 })

1311e322e3dca9d05f693aec206d4a0f1a74d454
Rebecca Ransome <beckyrose200@aol.com>
2023-10-11 09:22:22 +0100

    Update comment

--- src/internal/lib/connectors/services/water/NotificationsApiClient.js
+++ src/internal/lib/connectors/services/water/NotificationsApiClient.js
@@ -7,6 +7,7 @@ class NotificationsApiClient extends SharedNotificationsApiClient {
    * Send/preview notifications.  Builds de-duped contact list and renders templates
    * @param {Number} taskConfigId - the task ID in the water service task_config table
    * @param {Array} licenceNumbers - an array of licence numbers
+   * @param {String} uniqueJobId - notification job id for bullMq
    * @param {Object} params - user-entered template parameters
    * @param {String} sender - email address of sender.  If not supplied, reverts to preview mode
    * @return {Promise} resolves with an array of contacts, each with licence numbers and rendered templates attached

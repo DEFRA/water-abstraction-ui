ad03a5b0214011b01681aef48b41db9948d4d8b9
jonathangoulding <jonathangoulding21@gmail.com>
2024-06-24 16:57:57 +0100

    Add feature flag for links to the water abstraction system
    This service is slowly being replaced. There are currently links directing the pages to the legacy licence view pages, built previously in the water-abstraction-ui repo.
    
    We have rebuilt these pages (with some tweaks) in the water-abstraction-system repo with the intention of using the water-abstraction-system to view and manage licences.
    
    This change will allow us to navigate to the water-abstraction-system licence view pages instead of the 'legacy' licence view pages in a controlled manner.
    
    This change will be mirrored in water-abstraction-system.
    
    https://eaflood.atlassian.net/browse/WATER-4466

--- src/internal/config.js
+++ src/internal/config.js
@@ -173,7 +173,8 @@ module.exports = {
     showReturnRequirements: (get(process.env, 'SHOW_RETURN_REQUIREMENTS') || '').toLowerCase() === 'true' && !isProduction,
     triggerSrocAnnual: (get(process.env, 'TRIGGER_SROC_ANNUAL') || '').toLowerCase() === 'true',
     useNewBillRunSetup: (get(process.env, 'USE_NEW_BILL_RUN_SETUP') || '').toLowerCase() === 'true',
-    useWorkflowSetupLinks: (get(process.env, 'USE_WORKFLOW_SETUP_LINKS') || 'true').toLowerCase() === 'true'
+    useWorkflowSetupLinks: (get(process.env, 'USE_WORKFLOW_SETUP_LINKS') || 'true').toLowerCase() === 'true',
+    enableSystemLicenceView: process.env.ENABLE_SYSTEM_LICENCE_VIEW === 'true'
   },
   billRunsToDisplayPerPage: process.env.BILL_RUNS_TO_DISPLAY_PER_PAGE || 20
 }

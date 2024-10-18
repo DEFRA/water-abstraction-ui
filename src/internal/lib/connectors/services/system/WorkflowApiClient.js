0c5853ef2bc9068eadf1298ee3f69a747a83c851
Rebecca Ransome <beckyrose200@aol.com>
2024-10-18 09:28:45 +0100

    Update param name

--- src/internal/lib/connectors/services/system/WorkflowApiClient.js
+++ src/internal/lib/connectors/services/system/WorkflowApiClient.js
@@ -6,19 +6,19 @@ class WorkflowApiClient extends ServiceClient {
   /**
    * Flags a licence for the supplementary bill run
    *
-   * @param {String} chargeVersionWorkflowId - The UUID of the workflow record being removed that needs flagging
+   * @param {String} workflowId - The UUID of the workflow record being removed that needs flagging
    * @param {Object} cookie - Existing cookie set by this app needed for pass-through authentication
    *
    * @returns {Promise} resolves with the licence being flagged for the supplementary bill run
    */
-  supplementary (chargeVersionWorkflowId, cookie) {
+  supplementary (workflowId, cookie) {
     const url = this.joinUrl('licences/supplementary')
     const options = {
       headers: {
         cookie
       },
       body: {
-        chargeVersionWorkflowId
+        workflowId
       }
     }
     return this.serviceRequest.post(url, options)

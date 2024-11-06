6f53912d745aabe963f1135797e19cb774546f75
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2024-11-06 12:02:15 +0000

    Fix `controllers/non-chargeable.test.js`

--- test/internal/modules/charge-information/controllers/non-chargeable.test.js
+++ test/internal/modules/charge-information/controllers/non-chargeable.test.js
@@ -115,7 +115,7 @@ experiment('internal/modules/charge-information/controller', () => {
         request.query = { start: 1 }
         await controller.getNonChargeableReason(request, h)
         const { back } = h.view.lastCall.args[1]
-        expect(back).to.equal('/licences/test-licence-id#charge')
+        expect(back).to.equal('/system/licences/test-licence-id/set-up')
       })
     })
 

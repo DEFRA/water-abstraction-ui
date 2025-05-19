06e7ee3434df554e485337cc96b3658f9bc7ee23
rvsiyad <roblesiyad123@gmail.com>
2025-05-19 13:00:19 +0100

    Add tests to redirect to billing account test when enableBillingAccountView is enabled and disabled

--- test/internal/modules/internal-search/lib/redirect-to-billing-account.test.js
+++ test/internal/modules/internal-search/lib/redirect-to-billing-account.test.js
@@ -1,7 +1,10 @@
 const sinon = require('sinon')
+const sandbox = require('sinon').createSandbox()
 const { expect, fail } = require('@hapi/code')
-const { experiment, test, beforeEach } = exports.lab = require('@hapi/lab').script()
+const { experiment, test, beforeEach } = (exports.lab = require('@hapi/lab').script())
 const { redirectToBillingAccount } = require('internal/modules/internal-search/lib/redirect-to-billing-account')
+
+const config = require('../../../../../src/internal/config.js')
 
 experiment('redirectToBillingAccount', () => {
   const billingAccount = {
@@ -27,11 +30,31 @@ experiment('redirectToBillingAccount', () => {
     }
   })
 
-  test('It should redirect if the return has a path property', async () => {
-    const view = {
-      billingAccount
-    }
-    redirectToBillingAccount(billingAccount, view, h)
-    expect(h.redirect.firstCall.args[0]).to.equal(`/billing-accounts/${view.billingAccount.invoiceAccountId}`)
+  experiment('When enableBillingAccountView is not enabled', () => {
+    beforeEach(async () => {
+      sandbox.stub(config.featureToggles, 'enableBillingAccountView').value(false)
+    })
+
+    test('It should redirect if the return has a path property', async () => {
+      const view = {
+        billingAccount
+      }
+      redirectToBillingAccount(billingAccount, view, h)
+      expect(h.redirect.firstCall.args[0]).to.equal(`/billing-accounts/${view.billingAccount.invoiceAccountId}`)
+    })
+  })
+
+  experiment('When enableBillingAccountView is enabled', () => {
+    beforeEach(async () => {
+      sandbox.stub(config.featureToggles, 'enableBillingAccountView').value(true)
+    })
+
+    test('It should redirect if the return has a path property', async () => {
+      const view = {
+        billingAccount
+      }
+      redirectToBillingAccount(billingAccount, view, h)
+      expect(h.redirect.firstCall.args[0]).to.equal(`/system/billing-accounts/${view.billingAccount.invoiceAccountId}`)
+    })
   })
 })

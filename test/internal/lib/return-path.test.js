45d05ea65bb35e763f9d79c942c439d4fdca0ece
Jason Claxton <jason.claxton@defra.gov.uk>
2025-11-13 13:13:34 +0000

    Fix tests

--- test/internal/lib/return-path.test.js
+++ test/internal/lib/return-path.test.js
@@ -1,32 +1,25 @@
 'use strict'
 
 const Lab = require('@hapi/lab')
-const { experiment, test, beforeEach, afterEach } = exports.lab = Lab.script()
+const { experiment, test, beforeEach, afterEach } = (exports.lab = Lab.script())
 const { expect } = require('@hapi/code')
-const sinon = require('sinon')
-
-const sandbox = sinon.createSandbox()
-
-const InternalConfig = require('../../../src/internal/config.js')
 
 const ReturnPath = require('internal/lib/return-path')
 
 const { scope } = require('internal/lib/constants')
 
 const ret = {
+  id: 'da394471-0928-4544-8960-fed2087d1ea7',
   return_id: 'v1:123:456',
   received_date: '2018-11-08',
   status: 'completed',
   end_date: '2018-10-31'
 }
 
-const internalView = `/returns/return?id=${ret.return_id}`
 const internalEdit = `/return/internal?returnId=${ret.return_id}`
 
 const getInternalRequest = (isEditor = false) => {
-  const scopes = isEditor
-    ? [scope.internal, scope.returns]
-    : [scope.internal]
+  const scopes = isEditor ? [scope.internal, scope.returns] : [scope.internal]
   return {
     auth: {
       credentials: {
@@ -37,68 +30,13 @@ const getInternalRequest = (isEditor = false) => {
 }
 
 experiment('return-path', () => {
-  beforeEach(() => {
-    sandbox.stub(InternalConfig, 'featureToggles').value({ enableSystemReturnsView: false })
-  })
-
-  afterEach(() => {
-    sandbox.restore()
-  })
-
   experiment('#getReturnPath()', () => {
-    test('An internal user can view a return if has completed status', async () => {
+    test('An internal user can view/edit a return', async () => {
       const request = getInternalRequest()
       expect(ReturnPath.getReturnPath(ret, request)).to.equal({
-        path: internalView,
+        path: `/system/return-logs/${ret.id}`,
         isEdit: false
       })
-    })
-
-    test('An internal user can view a return if has void status', async () => {
-      const request = getInternalRequest()
-      expect(ReturnPath.getReturnPath({ ...ret, status: 'void' }, request)).to.equal({
-        path: internalView,
-        isEdit: false
-      })
-    })
-
-    test('An internal user cannot edit a return if has due status', async () => {
-      const request = getInternalRequest()
-      expect(ReturnPath.getReturnPath({ ...ret, status: 'due' }, request)).to.equal(undefined)
-    })
-
-    test('An internal returns user can edit a return if has due status', async () => {
-      const request = getInternalRequest(true)
-      expect(ReturnPath.getReturnPath({ ...ret, status: 'due' }, request)).to.equal({
-        path: internalEdit,
-        isEdit: true
-      })
-    })
-
-    test('An internal returns user can edit a return if has received status', async () => {
-      const request = getInternalRequest(true)
-      expect(ReturnPath.getReturnPath({ ...ret, status: 'received' }, request)).to.equal({
-        path: internalEdit,
-        isEdit: true
-      })
-    })
-
-    test('An internal returns user can edit a return if has completed status', async () => {
-      const request = getInternalRequest(true)
-      expect(ReturnPath.getReturnPath({ ...ret, status: 'completed' }, request)).to.equal({
-        path: internalView,
-        isEdit: false
-      })
-    })
-
-    test('An internal returns user cannot edit a return if the cycle ends in the future', async () => {
-      const request = getInternalRequest(true)
-      expect(ReturnPath.getReturnPath({ ...ret, status: 'received', end_date: '3000-01-01' }, request)).to.equal(undefined)
-    })
-
-    test('An internal returns user cannot edit a return if the cycle ends before 2018-10-31', async () => {
-      const request = getInternalRequest(true)
-      expect(ReturnPath.getReturnPath({ ...ret, status: 'received', end_date: '2018-10-30' }, request)).to.equal(undefined)
     })
   })
 
